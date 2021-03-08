39094ebaab652c3fc4dd42a4e3d45d30cc648ff5
PaperChalice <lgamma@163.com>
2021-03-08 11:23:08 +0800

    qt-declarative: new

---
+++ Formula/qt-declarative.rb
@@ -0,0 +1,61 @@
+class QtDeclarative < Formula
+  desc "Qt Quick2"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtdeclarative-everywhere-src-6.0.2.tar.xz"
+  sha256 "13d6b12d1c1eabdc6e6775e344e3479bf42ffabeadb20bee94882dc3eb003820"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  head "https://code.qt.io/qt/qtdeclarative.git", branch: "dev"
+
+  livecheck do
+    url :head
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => :build
+
+  depends_on "python"
+  depends_on "qt-base"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+
+      -DPython_ROOT_DIR=#{Formula["python"].opt_prefix}
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir["#{lib}/*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    (testpath/"hello.qml").write <<~EOS
+      import QtQuick 2.0
+      Rectangle {
+          id: page
+          width: 320; height: 480
+          color: "lightgray"
+          Text {
+              id: helloText
+              text: "Hello world!"
+              y: 30
+              anchors.horizontalCenter: page.horizontalCenter
+              font.pointSize: 24; font.bold: true
+          }
+      }
+    EOS
+    system bin/"qmlscene", "--quit", testpath/"hello.qml"
+  end
+end
