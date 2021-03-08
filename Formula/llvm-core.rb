f1f884df9ba2483f54d40f4831303bf5fb5cd9f4
PaperChalice <lgamma@163.com>
2021-03-08 09:21:07 +0800

    Create llvm-core.rb

---
+++ Formula/llvm-core.rb
@@ -0,0 +1,96 @@
+class LlvmCore < Formula
+  desc "Next-gen compiler infrastructure"
+  homepage "https://llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/llvm-11.1.0.src.tar.xz"
+  sha256 "ce8508e318a01a63d4e8b3090ab2ded3c598a50258cc49e2625b9120d4c03ea5"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  depends_on "cmake" => [:build, :test, :recommended]
+  depends_on "ninja" => :build
+  depends_on "python" => :build
+
+  depends_on "libffi"
+
+  uses_from_macos "libedit"
+  uses_from_macos "libxml2"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def install
+    ENV.libcxx if ENV.compiler == :clang
+    ENV.permit_arch_flags
+    targets = %w[
+      host
+      X86
+      AArch64
+      ARM
+      RISCV
+      WebAssembly
+    ].join ";"
+
+    #-DLLVM_BUILD_LLVM_DYLIB=ON
+    #-DLLVM_LINK_LLVM_DYLIB=ON
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_CXX_STANDARD=17
+      -DDEFAULT_SYSROOT=#{MacOS.sdk_path}
+
+      -DLLVM_BUILD_LLVM_C_DYLIB=ON
+      -DLLVM_ENABLE_EH=ON
+      -DLLVM_ENABLE_FFI=ON
+      -DLLVM_ENABLE_LIBCXX=ON
+      -DLLVM_ENABLE_MODULES=ON
+      -DLLVM_ENABLE_RTTI=ON
+      -DLLVM_INCLUDE_DOCS=OFF
+      -DLLVM_INCLUDE_TESTS=OFF
+      -DLLVM_INSTALL_UTILS=ON
+      -DLLVM_ENABLE_Z3_SOLVER=OFF
+      -DLLVM_OPTIMIZED_TABLEGEN=ON
+      -DLLVM_TARGETS_TO_BUILD=#{targets}
+      -DLLVM_USE_NEW_PM=ON
+      -DFFI_INCLUDE_DIR=#{Formula["libffi"].opt_lib}/libffi-#{Formula["libffi"].version}/include
+      -DFFI_LIBRARY_DIR=#{Formula["libffi"].opt_lib}
+      -DLLVM_CREATE_XCODE_TOOLCHAIN=OFF
+    ]
+
+    mkdir "build" do
+      system "cmake", "-G", "Ninja", "..", *(std_cmake_args + args)
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION #{Formula["cmake"].version})
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+      find_package(LLVM REQUIRED CONFIG)
+
+      include_directories(SYSTEM ${LLVM_INCLUDE_DIRS})
+      add_definitions(${LLVM_DEFINITIONS})
+
+      add_executable(test
+          main.cpp
+      )
+      llvm_map_components_to_libnames(llvm_libs Support Core IRReader)
+
+      # Link against LLVM libraries
+      target_link_libraries(test ${llvm_libs})
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <llvm/Support/Debug.h>
+      using namespace llvm;
+      int main() {
+        dbgs() << "test";
+        return 0;
+      }
+    EOS
+    system "cmake", "."
+    system "make"
+    system "./test"
+  end
+end
