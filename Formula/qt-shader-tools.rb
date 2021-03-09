3a9d4464a662cf634e4c9559afe533ae235bff85
PaperChalice <lgamma@163.com>
2021-03-09 17:44:56 +0800

    qt-shader-tools: new

---
+++ Formula/qt-shader-tools.rb
@@ -0,0 +1,56 @@
+class QtShaderTools < Formula
+  desc "Provide the producer functionality for the shader pipeline"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtshadertools-everywhere-src-6.0.2.tar.xz"
+  sha256 "fbc824cbe900a5dbae13b7534213c9eac6666aaa03cb203ef243c78aeb6b8eeb"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => :build
+
+  depends_on "qt-base"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir["#{lib}/*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    (testpath/"shader.frag").write <<~EOS
+      #version 440
+
+      layout(location = 0) in vec2 v_texcoord;
+      layout(location = 0) out vec4 fragColor;
+      layout(binding = 1) uniform sampler2D tex;
+
+      layout(std140, binding = 0) uniform buf {
+          float uAlpha;
+      };
+
+      void main()
+      {
+          vec4 c = texture(tex, v_texcoord);
+          fragColor = vec4(c.rgb, uAlpha);
+      }
+    EOS
+
+    # system bin/"qsb", "-o", "./shader.frag.qsb", "./shader.frag"
+    # assert_predicate "./shader.frag.qsb", :exist?
+  end
+end
