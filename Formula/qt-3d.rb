f76086aa48e0f19336fce9d5398829699a2e3c8f
PaperChalice <lgamma@163.com>
2021-03-09 17:43:42 +0800

    qt-3d: new

---
+++ Formula/qt-3d.rb
@@ -0,0 +1,78 @@
+class Qt3d < Formula
+  desc "3D Lib"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.2/qt3d-everywhere-src-6.0.2.tar.xz"
+  sha256 "ff6434da878062aea612a9d7323bd615c2f232c4462c26323f1a5511aac6db89"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => :build
+
+  depends_on "qt-quick3d"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+
+      -DFEATURE_qt3d_system_assimp=ON
+      -DTEST_assimp=ON
+    ]
+    system "cmake", "-G", "Ninja", ".", *args
+    system "ninja"
+    system "ninja", "install"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION #{Formula["cmake"].version})
+
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+
+      find_package(Qt6 COMPONENTS Core 3DCore REQUIRED)
+
+      add_executable(test
+          main.cpp
+      )
+
+      target_link_libraries(test PRIVATE Qt6::Core Qt6::3DCore)
+    EOS
+
+    (testpath/"test.pro").write <<~EOS
+      QT       += core 3dcore
+      QT       -= gui
+      TARGET = test
+      CONFIG   += console
+      CONFIG   -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <Qt3DCore>
+      #include <QDebug>
+
+      int main(int argc, char *argv[])
+      {
+        auto *root = new Qt3DCore::QEntity();
+        return 0;
+      }
+    EOS
+
+    system "cmake", "."
+    system "make"
+
+    ENV.delete "CPATH"
+    system "qmake", "./test.pro"
+    system "make"
+  end
+end
