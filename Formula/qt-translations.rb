c50950df50321f75f62a1f5245e6a3a97ec9a165
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-03-10 03:16:34 +0000

    qt-translations: add 6.0.2 bottle.

--- Formula/qt-translations.rb
+++ Formula/qt-translations.rb
@@ -5,6 +5,11 @@ class QtTranslations < Formula
   sha256 "6dcdcb8f03f4ca360618dd7422fb87c48b91a26b9e5e5e6f9917c047dbb8ad14"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-translations-6.0.2"
+    sha256 cellar: :any_skip_relocation, big_sur: "7ce71f4191e3389b71e86a53802e645bb8f31d42e592e1d145d2c349fab09c1e"
+  end
+
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "qt-tools" => :build
