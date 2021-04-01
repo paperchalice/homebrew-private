01f8fbd7686848c5332075ffaa831a5a56c42e88
PaperChalice <lgamma@163.com>
2021-04-01 11:18:05 +0800

    qt quick:update

--- Formula/qt-declarative.rb
+++ Formula/qt-declarative.rb
@@ -1,8 +1,8 @@
 class QtDeclarative < Formula
   desc "Qt Quick2"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/submodules/qtdeclarative-everywhere-src-6.0.2.tar.xz"
-  sha256 "13d6b12d1c1eabdc6e6775e344e3479bf42ffabeadb20bee94882dc3eb003820"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.3/submodules/qtdeclarative-everywhere-src-6.0.3.tar.xz"
+  sha256 "f2987fb4c698c5930bbb58e75f7c3de16592f2e79696ed348d77556743db30bd"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtdeclarative.git", branch: "dev"
 
