30aae7b4fd8b10aa23c15fb0edbab1caa34d015e
PaperChalice <lgamma@163.com>
2021-04-01 18:52:56 +0800

    try doc

--- Formula/qt-doc.rb
+++ Formula/qt-doc.rb
@@ -1,8 +1,8 @@
 class QtDoc < Formula
   desc "Qt Documentation"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.0/submodules/qtdoc-everywhere-src-6.0.0.tar.xz"
-  sha256 "476108d92506d93d5df227f275d653abba57b3b1694afbf2965e0c74e8c0a5a8"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.3/single/qt-everywhere-src-6.0.3.tar.xz"
+  sha256 "ca4a97439443dd0b476a47b284ba772c3b1b041a9eef733e26a789490993a0e3"
   license "GFDL-1.3-only"
   head "https://code.qt.io/qt/qtdoc.git", branch: "dev"
 
@@ -20,18 +20,61 @@ class QtDoc < Formula
   depends_on "ninja" => :build
   depends_on "qt-tools" => :build
 
-  def install
-    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
-      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
-      -DCMAKE_STAGING_PREFIX=#{prefix}
-    ]
-    system "cmake", "-G", "Ninja", ".", *args
-    system "ninja", "docs"
-    system "ninja", "install_docs"
+  resource "qtimageformats" do
+    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.3/qtimageformats-everywhere-src-6.0.3.tar.xz"
+    sha256 "327580b5a5b9a8d75e869c0eaa7ff34881bbde4e4ccc51d07a59e96054136837"
+  end
 
-    Pathname.glob("share/qt/doc/*") do |path|
-      doc.install path
-    end
+  resource "qt3d" do
+    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.3/qt3d-everywhere-src-6.0.3.tar.xz"
+    sha256 "470f95c559b68cc8faa982c1ca7ff83054d6802f7b2a0c1d960a155b92080cf9"
+  end
+
+  resource "qtnetworkauth" do
+    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.3/qtnetworkauth-everywhere-src-6.0.3.tar.xz"
+    sha256 "124bf433e2c5418e900a5947d4ceb128ee179f514eddcea33924f0b695be64ed"
+  end
+
+  def install
+    resources.each { |addition| addition.stage buildpath/addition.name }
+
+    config_args = %W[
+      -release
+
+      -prefix #{HOMEBREW_PREFIX}
+      -extprefix #{prefix}
+
+      -libexecdir share/qt/libexec
+      -plugindir share/qt/plugins
+      -qmldir share/qt/qml
+      -docdir share/doc/qt
+      -translationdir share/qt/translations
+      -examplesdir share/qt/examples
+      -testsdir share/qt/tests
+
+      -libproxy
+      -no-feature-relocatable
+      -system-sqlite
+    ]
+
+    # TODO: remove `-DFEATURE_qt3d_system_assimp=ON`
+    # and `-DTEST_assimp=ON` when Qt 6.2 is released.
+    # See https://bugreports.qt.io/browse/QTBUG-91537
+    cmake_args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"]||s["CMAKE_FIND_FRAMEWORK"] } + %W[
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+      -DCMAKE_FIND_FRAMEWORK=FIRST
+
+      -DINSTALL_MKSPECSDIR=share/qt/mkspecs
+      -DINSTALL_DESCRIPTIONSDIR=share/qt/modules
+
+      -DFEATURE_pkg_config=ON
+      -DFEATURE_qt3d_system_assimp=ON
+      -DTEST_assimp=ON
+    ]
+
+    system "./configure", *config_args, "--", *cmake_args
+    system "cmake", "--build", ".", "-t", "docs"
+    system "cmake", "--build", ".", "-t", "install_docs"
   end
 
   test do
