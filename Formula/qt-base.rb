bdf1c5e4d585b4465e548efa7b1817705bd25f99
PaperChalice <lgamma@163.com>
2021-04-02 11:00:18 +0800

    rm fdl

--- Formula/qt-base.rb
+++ Formula/qt-base.rb
@@ -3,7 +3,7 @@ class QtBase < Formula
   homepage "https://www.qt.io/"
   url "https://download.qt.io/official_releases/qt/6.0/6.0.3/submodules/qtbase-everywhere-src-6.0.3.tar.xz"
   sha256 "1a45b61c2a349964625c50e3ea40cbb309e269762dd0786397e0e18e7e10d394"
-  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  license all_of: ["GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtbase.git", branch: "dev"
 
   livecheck do
