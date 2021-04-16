46972a1e8d6b134e3182e77bfe25dabf6d42b316
PaperChalice <lgamma@163.com>
2021-04-16 21:22:04 +0800

    mlir

---
+++ Formula/mlir.rb
@@ -0,0 +1,41 @@
+class Mlir < Formula
+  desc "Multi-Level Intermediate Representation"
+  homepage "https://mlir.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/llvm-project-12.0.0.src.tar.xz"
+  sha256 "9ed1688943a4402d7c904cc4515798cdb20080066efa010fe7e1f2551b423628"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  livecheck do
+    url "https://llvm.org/"
+    regex(/LLVM (\d+\.\d+\.\d+)/i)
+  end
+
+  depends_on "cmake" => :build
+
+  depends_on "llvm-core"
+
+  patch do
+    url "https://github.com/llvm/llvm-project/commit/2aa1af9b1da0d832270d9b8afcba19a4aba2c366.patch?full_index=1"
+    sha256 "9eef2025e3e4401e705426b3bf9af7a04ab0fbb4c3cc055dded6e16addec1d79"
+  end
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_BUILD_TYPE"] } + %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_BUILD_TYPE=MinSizeRel
+      -DCMAKE_CXX_STANDARD=17
+    ]
+
+    cd "mlir" do
+      mkdir "build" do
+        system "cmake", "..", *args
+        system "cmake", "--build", "."
+        system "cmake", "--install", "."
+      end
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
