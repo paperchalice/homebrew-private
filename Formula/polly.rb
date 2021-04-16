06a4c0ff14be65042e2f08fd4cc84ffb83223f13
PaperChalice <lgamma@163.com>
2021-04-16 19:46:02 +0800

    polly

---
+++ Formula/polly.rb
@@ -0,0 +1,34 @@
+class Polly < Formula
+  desc "High-level loop and data-locality optimizer"
+  homepage "https://polly.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/polly-12.0.0.src.tar.xz"
+  sha256 "0d9afc76b262f89d0fc6cb4f155ad25be5bf0554d14f96208ec81a51a44fb4c7"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  livecheck do
+    url "https://llvm.org/"
+    regex(/LLVM (\d+\.\d+\.\d+)/i)
+  end
+
+  depends_on "cmake" => :build
+
+  depends_on "llvm-core"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_BUILD_TYPE"] } + %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_BUILD_TYPE=MinSizeRel
+      -DCMAKE_CXX_STANDARD=17
+    ]
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "cmake", "--build", "."
+      system "cmake", "--install", "."
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
