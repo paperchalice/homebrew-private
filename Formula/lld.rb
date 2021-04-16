b4194f21654ee63ab7a939928d2413ec784efb56
PaperChalice <lgamma@163.com>
2021-04-16 11:48:34 +0800

    lld

---
+++ Formula/lld.rb
@@ -0,0 +1,32 @@
+class Lld < Formula
+  desc "LLVM Linker"
+  homepage "https://lld.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/lld-12.0.0.src.tar.xz"
+  sha256 "2cb7d497f3ce33ce8a2c50ad26ec93a8c45f57268d4d96953cd0f25566f753fd"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  livecheck do
+    url :homepage
+    regex(/LLVM (\d+\.\d+\.\d+)/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  depends_on "llvm-core"
+
+  def install
+    args = %w[
+      -DBUILD_SHARED_LIBS=ON
+    ]
+    mkdir "build" do
+      system "cmake", "-G", "Ninja", "..", *(std_cmake_args + args)
+      system "cmake", "--build", "."
+      system "cmake", "--install", "."
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
