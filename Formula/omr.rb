762f0feafdb54dcddae0ae4961826be821a63dc5
PaperChalice <lgamma@163.com>
2021-04-17 12:34:38 +0800

    update sha

--- Formula/omr.rb
+++ Formula/omr.rb
@@ -2,7 +2,7 @@ class Omr < Formula
   desc "Reliable components for building language runtimes"
   homepage "https://www.eclipse.org/omr/"
   url "https://github.com/eclipse/omr/archive/refs/tags/omr-0.1.0.tar.gz"
-  sha256 "c8a2baaabf051e535ed211415a27634a2e4689f576d6d099e28ceab9ff1837c0"
+  sha256 "f956ca493e4089ce98be9651a7d3378cc43625bc8d24660195b8940e755eb797"
   license "GPL-2.0-only"
 
   depends_on "cmake" => :build
