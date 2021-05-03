6c636bc08e7f1df820ca6eac93eeb45f60a7ebf6
PaperChalice <lgamma@163.com>
2021-05-03 19:46:56 +0800

    rm comment

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -13,13 +13,6 @@ class Cmake < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "cd249c48979cc3df65cc7b1a0cc00a1d702bdf0e95ac616cee16c9daea576c59"
-    sha256 cellar: :any_skip_relocation, big_sur:       "44f432c44eef7f20474002009392ab763be05c1962a086b7c79cb9f9bf4df47c"
-    sha256 cellar: :any_skip_relocation, catalina:      "abf670453c8e7394215bbad68cf11e1bae8e598a86cc7886f4d3a9f48e9ef87a"
-    sha256 cellar: :any_skip_relocation, mojave:        "a60610090a757e36e9799be128d8404330ebd79e6d1e19fe3926dbcb95fd8e3c"
-  end
-
   depends_on "qt@5" => :build
   depends_on "sphinx-doc" => :build
 
@@ -28,12 +21,6 @@ class Cmake < Formula
   on_linux do
     depends_on "openssl@1.1"
   end
-
-  # The completions were removed because of problems with system bash
-
-  # The `with-qt` GUI option was removed due to circular dependencies if
-  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
-  # For the GUI application please instead use `brew install --cask cmake`.
 
   def install
     args = %W[
