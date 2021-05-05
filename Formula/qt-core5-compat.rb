767f2dc52d39e202ad117e02b55607679834bad8
PaperChalice <lgamma@163.com>
2021-05-05 11:25:10 +0800

    Update qt-core5-compat.rb

--- Formula/qt-core5-compat.rb
+++ Formula/qt-core5-compat.rb
@@ -1,8 +1,8 @@
 class QtCore5Compat < Formula
   desc "Qt module contains unsupported Qt 5 APIs"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.3/submodules/qt5compat-everywhere-src-6.0.3.tar.xz"
-  sha256 "b0e32cd9f3ca9028f5882dc18dafacfd50d723efbc9c0570e85600f13293b90f"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.4/submodules/qt5compat-everywhere-src-6.0.4.tar.xz"
+  sha256 "abd0b9da7bccb33a23a59f28a0a2b2debdc7e07415dd4f75de9a751984895e69"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qt5compat.git", branch: "dev"
 
