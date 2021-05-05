3b91facf4c1a18238ad7edf3bad023f762dea347
PaperChalice <lgamma@163.com>
2021-05-05 11:29:45 +0800

    timeline

--- Formula/qt-quick-timeline.rb
+++ Formula/qt-quick-timeline.rb
@@ -1,8 +1,8 @@
 class QtQuickTimeline < Formula
   desc "Module for keyframe-based timeline construction"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.3/submodules/qtquicktimeline-everywhere-src-6.0.3.tar.xz"
-  sha256 "9b224e076e1a99f7a1ef2da81c1b43835508c20f69c7d22ff66559cb499b798b"
+  url "https://download.qt.io/official_releases/qt/6.0/6.0.4/submodules/qtquicktimeline-everywhere-src-6.0.4.tar.xz"
+  sha256 "7019a2967aad0fabdcb7d5b701898274f7c87edcf7b23e56baff556094a2648e"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtquicktimeline.git", branch: "dev"
 
