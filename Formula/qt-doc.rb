3af17ad821e462460aaf5862d1491ccdcb6ecc52
PaperChalice <lgamma@163.com>
2021-05-08 18:11:30 +0800

    doc
    Closes #105.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/qt-doc.rb
+++ Formula/qt-doc.rb
@@ -1,15 +1,9 @@
 class QtDoc < Formula
   desc "Qt Documentation"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.4/single/qt-everywhere-src-6.0.4.tar.xz"
-  sha256 "677db6472420f9046b16f7c0d0aa15c4f11f344462a6374feb860625c12fc72b"
+  url "https://download.qt.io/official_releases/qt/6.1/6.1.0/single/qt-everywhere-src-6.1.0.tar.xz"
+  sha256 "326a710b08b0973bb4f5306a786548d8b8dd656db75ce9f3f85ea32680d3c88a"
   license "GFDL-1.3-only"
-  head "https://code.qt.io/qt/qtdoc.git", branch: "dev"
-
-  livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-doc-6.0.4"
@@ -24,30 +18,16 @@ class QtDoc < Formula
   depends_on "python"     => :build
   depends_on "qt-tools"   => :build
 
-  resource "qt3d" do
-    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.4/qt3d-everywhere-src-6.0.4.tar.xz"
-    sha256 "d6970593f4ab2d94c7beedf217ce4456c69f647251aa661a7de3a6ccf1b618ff"
-  end
-
-  resource "qtimageformats" do
-    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.4/qtimageformats-everywhere-src-6.0.4.tar.xz"
-    sha256 "c284b2eb413a7cf62ba7168c693b7498539fc41dfd9f29dd3ce5c5b46d8d1073"
-  end
-
-  resource "qtnetworkauth" do
-    url "https://download.qt.io/official_releases/additional_libraries/6.0/6.0.4/qtnetworkauth-everywhere-src-6.0.4.tar.xz"
-    sha256 "108c761dde84c346a292e875a24bfba7a0e1d0ea3e0238149fe9ac11a3de92e9"
-  end
-
   def install
     inreplace "qtbase/cmake/FindGSSAPI.cmake", "gssapi_krb5", ""
-    resources.each { |addition| addition.stage buildpath/addition.name }
+    inreplace "qtbase/CMakeLists.txt", "FATAL_ERROR", ""
 
     config_args = %W[
       -release
 
       -prefix #{HOMEBREW_PREFIX}
       -extprefix #{prefix}
+      -sysroot #{MacOS.sdk_path}
 
       -libexecdir share/qt/libexec
       -plugindir share/qt/plugins
