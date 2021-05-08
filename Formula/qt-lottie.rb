5a97d1346a10a991fe89087a1e4c0b505da125d9
PaperChalice <lgamma@163.com>
2021-05-08 09:42:33 +0800

    lottie
    Closes #100.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

---
+++ Formula/qt-lottie.rb
@@ -0,0 +1,38 @@
+class QtLottie < Formula
+  desc "Describing 2D vector graphics animations"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.1/6.1.0/submodules/qtlottie-everywhere-src-6.1.0.tar.xz"
+  sha256 "3c2478f1064574939cfc151d7df0743d6bb1119d8cee6ed3ca3bc1cdfb8fece8"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  head "https://code.qt.io/qt/qtlottie.git", branch: "dev"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "perl" => :build
+
+  depends_on "qt-base"
+  depends_on "qt-declarative"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", ".", *args
+    system "cmake", "--build", "."
+    system "cmake", "--install", "."
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir[lib/"*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
