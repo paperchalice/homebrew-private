3702ceb6caa980242c81713d7d72fe4b8ae23554
PaperChalice <lgamma@163.com>
2021-05-08 09:51:25 +0800

    vk
    Closes #103.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/qt-virtual-keyboard.rb
+++ Formula/qt-virtual-keyboard.rb
@@ -6,11 +6,6 @@ class QtVirtualKeyboard < Formula
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtvirtualkeyboard.git", branch: "dev"
 
-  livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
   depends_on "cmake" => [:build, :test]
   depends_on "perl" => :build
 
