923f32610db3b723bbc51cb6aa4a67e8830b1b43
PaperChalice <lgamma@163.com>
2021-05-08 09:51:04 +0800

    vk

---
+++ Formula/qt-virtual-keyboard.rb
@@ -0,0 +1,44 @@
+class QtVirtualKeyboard < Formula
+  desc "Qt Quick virtual keyboard"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.1/6.1.0/submodules/qtvirtualkeyboard-everywhere-src-6.1.0.tar.xz"
+  sha256 "595d69dbfd43a87b4d3a270afed6dd64172551990e29239281f39b727b145c01"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  head "https://code.qt.io/qt/qtvirtualkeyboard.git", branch: "dev"
+
+  livecheck do
+    url :head
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "perl" => :build
+
+  depends_on "qt-base"
+  depends_on "qt-declarative"
+  depends_on "qt-svg"
+
+  def install
+    args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"] } + %W[
+      -DCMAKE_INSTALL_PREFIX=#{HOMEBREW_PREFIX}
+      -DCMAKE_STAGING_PREFIX=#{prefix}
+    ]
+    system "cmake", ".", *args
+    system "cmake", "--build", "."
+    system "cmake", "--install", "."
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink Dir[lib/"*.framework"]
+
+    # The pkg-config files installed suggest that headers can be found in the
+    # `include` directory. Make this so by creating symlinks from `include` to
+    # the Frameworks' Headers folders.
+    Pathname.glob("#{lib}/*.framework/Headers") do |path|
+      include.install_symlink path => path.parent.basename(".framework")
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
