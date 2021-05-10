a4b81dd95cd656f83c3c750672c6daa264d27359
PaperChalice <lgamma@163.com>
2021-05-10 12:41:20 +0800

    charts test

--- Formula/qt-charts.rb
+++ Formula/qt-charts.rb
@@ -43,6 +43,69 @@ class QtCharts < Formula
   end
 
   test do
-    system "echo"
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION #{Formula["cmake"].version})
+
+      project(test_project VERSION 1.0.0 LANGUAGES CXX)
+
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+      set(CMAKE_AUTOMOC ON)
+      set(CMAKE_AUTORCC ON)
+      set(CMAKE_AUTOUIC ON)
+
+      find_package(Qt6 COMPONENTS Widgets REQUIRED)
+      find_package(Qt6Charts)
+
+      add_executable(test
+          main.cpp
+      )
+
+      target_link_libraries(test PRIVATE Qt6::Widgets Qt6::Charts)
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QtWidgets/QApplication>
+      #include <QtWidgets/QMainWindow>
+      #include <QtCharts/QChartView>
+      #include <QtCharts/QLineSeries>
+
+      QT_USE_NAMESPACE
+
+      int main(int argc, char *argv[])
+      {
+          QApplication a(argc, argv);
+
+          QLineSeries *series = new QLineSeries();
+
+          series->append(0, 6);
+          series->append(2, 4);
+          series->append(3, 8);
+          series->append(7, 4);
+          series->append(10, 5);
+          *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+          QChart *chart = new QChart();
+          chart->legend()->hide();
+          chart->addSeries(series);
+          chart->createDefaultAxes();
+          chart->setTitle("Simple line chart example");
+
+          QChartView *chartView = new QChartView(chart);
+          chartView->setRenderHint(QPainter::Antialiasing);
+
+          QMainWindow window;
+          window.setCentralWidget(chartView);
+          window.resize(400, 300);
+          window.show();
+
+          return 0;
+      }
+    EOS
+
+    system "cmake", "."
+    system "cmake", "--build", "."
+    system "./test"
   end
 end
