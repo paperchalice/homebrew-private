a17f679bc597600913d71147e2fd81c538a8fe8f
PaperChalice <lgamma@163.com>
2021-05-14 21:01:35 +0800

    nls

---
+++ Formula/clang-nls.rb
@@ -0,0 +1,83 @@
+class ClangNls < Formula
+  desc "C language family frontend for LLVM"
+  homepage "https://clang.llvm.org"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang-12.0.0.src.tar.xz"
+  sha256 "e26e452e91d4542da3ebbf404f024d3e1cbf103f4cd110c26bf0a19621cca9ed"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  depends_on "cmake"       => :build
+  depends_on "compiler-rt" => :build
+  depends_on "libc++"      => :build
+  depends_on "python"      => :build
+  depends_on "unwinder"    => :build
+
+  depends_on "boost"
+  depends_on "llvm-core"
+
+  resource "clang-tools-extra" do
+    url "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang-tools-extra-12.0.0.src.tar.xz"
+    sha256 "ad41e0b527a65ade95c1ba690a5434cefaab4a2daa1be307caaa1e8541fe6d5c"
+  end
+
+  patch :p2 do
+    url "https://github.com/paperchalice/llvm-clang/commit/3b9df7adf1f210818ddc0168455e4862b2a4eca1.patch"
+  end
+
+  patch :p2 do
+    url "https://github.com/paperchalice/llvm-clang/commit/f17bfea9d59250f93bc0e58dace46d2bac68f692.patch"
+  end
+
+  def install
+    resource("clang-tools-extra").stage buildpath/"tools/extra"
+    # avoid building libclang-cpp
+    inreplace "tools/CMakeLists.txt", "add_clang_subdirectory(clang-shlib)", ""
+    inreplace "tools/extra/clangd/quality/CompletionModel.cmake",
+      "../clang-tools-extra", "tools/extra"
+    # add `-L /usr/local/lib`
+    inreplace "lib/Driver/ToolChains/Darwin.cpp",
+      "Args.AddAllArgs(CmdArgs, options::OPT_L);",
+      (%Q{CmdArgs.push_back("-L#{HOMEBREW_PREFIX}/lib");\n} + "Args.AddAllArgs(CmdArgs, options::OPT_L);")
+
+    include_dirs = %W[
+      #{MacOS.sdk_path}/usr/include
+      #{HOMEBREW_PREFIX}/include
+      #{HOMEBREW_PREFIX}/Frameworks
+    ].join ":"
+
+    # use ld because atom based lld is work in progress
+    # -DCLANG_DEFAULT_LINKER=lld
+    config_trick = '"+std::string(std::getenv("HOME"))+"/.local/etc/clang'
+    py_ver = Language::Python.major_minor_version("python3")
+    args = std_cmake_args + %W[
+      -D BUILD_SHARED_LIBS=ON
+      -D CMAKE_CXX_STANDARD=17
+      -D CMAKE_EXE_LINKER_FLAGS=-L/usr/lib
+      -D CMAKE_SHARED_LINKER_FLAGS=-L/usr/lib
+
+      -D C_INCLUDE_DIRS=#{include_dirs}
+      -D CLANG_ENABLE_NLS=ON
+      -D CLANG_CONFIG_FILE_SYSTEM_DIR=#{etc}/clang
+      -D CLANG_CONFIG_FILE_USER_DIR=#{config_trick}
+      -D CLANG_DEFAULT_STD_C=c17
+      -D CLANG_DEFAULT_STD_CXX=cxx17
+      -D CLANG_DEFAULT_CXX_STDLIB=libc++
+      -D CLANG_DEFAULT_RTLIB=compiler-rt
+      -D CLANG_DEFAULT_UNWINDLIB=libunwind
+      -D CLANG_LINK_CLANG_DYLIB=OFF
+      -D CLANG_PYTHON_BINDINGS_VERSIONS=#{py_ver}
+
+      -D DEFAULT_SYSROOT=#{MacOS.sdk_path}
+
+      -S .
+      -B build
+    ]
+
+    system "cmake", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "echo"
+  end
+end
