8351cb92d63f151ddfbdb28e7a9ec736a8da1f1a
PaperChalice <lgamma@163.com>
2021-05-27 17:18:14 +0800

    giac

---
+++ Formula/giac.rb
@@ -0,0 +1,40 @@
+class Giac < Formula
+  desc "Free computer algebra system"
+  homepage "https://www-fourier.ujf-grenoble.fr/~parisse/giac.html"
+  url "http://www-fourier.ujf-grenoble.fr/~parisse/giac/giac-1.7.0.tar.bz2"
+  sha256 "5b79b61373ee275435076300643342b05c96979772cb898cda31f4c2ead7a193"
+
+  depends_on "pkg-config" => :build
+
+  depends_on "fltk"
+  depends_on "gettext"
+  depends_on "glpk"
+  depends_on "gmp"
+  depends_on "gsl"
+  depends_on "lapack"
+  depends_on "libao"
+  depends_on "libpng"
+  depends_on "libsamplerate"
+  depends_on "micropython"
+  depends_on "ntl"
+  depends_on "pari"
+  depends_on "quickjs"
+
+  uses_from_macos "curl"
+  uses_from_macos "libiconv"
+
+  def install
+    ENV.append_to_cflags "-framework OpenGL -lintl"
+    args = std_configure_args + %w[
+      --enable-png
+      --enable-dl
+    ]
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "echo"
+  end
+end
