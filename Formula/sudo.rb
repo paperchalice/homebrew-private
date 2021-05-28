59eea6aee024fae4ddd3af91e186560cb078082f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-05-28 08:49:08 +0000

    sudo: add 1.9.7 bottle.

--- Formula/sudo.rb
+++ Formula/sudo.rb
@@ -4,6 +4,11 @@ class Sudo < Formula
   url "https://www.sudo.ws/dist/sudo-1.9.7.tar.gz"
   sha256 "2bbe7c2d6699b84d950ef9a43f09d4d967b8bc244b73bc095c4202068ddbe549"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/sudo-1.9.7"
+    sha256 big_sur: "39b6ce47966f4d1697d372794c69d0b040429676337b6c299dde868826bcf9aa"
+  end
+
   depends_on "python" => :build
 
   depends_on "gettext"
