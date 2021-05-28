d3bb786a542c486eb559a0410f0a3a822ad2e337
PaperChalice <lgamma@163.com>
2021-05-28 16:38:09 +0800

    rm kegonly
    Closes #124.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/sudo.rb
+++ Formula/sudo.rb
@@ -4,8 +4,6 @@ class Sudo < Formula
   url "https://www.sudo.ws/dist/sudo-1.9.7.tar.gz"
   sha256 "2bbe7c2d6699b84d950ef9a43f09d4d967b8bc244b73bc095c4202068ddbe549"
 
-  keg_only :provided_by_macos
-
   depends_on "python" => :build
 
   depends_on "gettext"
