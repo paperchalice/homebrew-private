cf74c826a0b9c7e60dc05e5b5fba3174a2dc35bb
PaperChalice <lgamma@163.com>
2021-06-04 10:47:11 +0800

    add sha

--- Formula/intel-llvm.rb
+++ Formula/intel-llvm.rb
@@ -2,6 +2,7 @@ class IntelLlvm < Formula
   desc "LLVM-based projects"
   homepage "https://www.oneapi.com/"
   url "https://github.com/intel/llvm/archive/refs/tags/2020-12.tar.gz"
+  sha256 "2cb7d497f3ce33ce8a2c50ad26ec93a8c45f57268d4d96953cd0f25566f753fd"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   depends_on "cmake" => :build
