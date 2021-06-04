0f3a16224ee8ce899d04b5628d794fa4008536a8
PaperChalice <lgamma@163.com>
2021-06-04 10:36:56 +0800

    intel llvm

---
+++ Formula/intel-llvm.rb
@@ -0,0 +1,24 @@
+class IntelLlvm < Formula
+  desc "Intel LLVM-based projects"
+  homepage "https://www.oneapi.com/"
+  url "https://github.com/intel/llvm/archive/refs/tags/2020-12.tar.gz"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+
+  depends_on "cmake"  => :build
+  depends_on "python" => :build
+  depends_on "ninja" => :build
+
+  uses_from_macos "libffi"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+    system "python3", "buildbot/configure.py"
+    system "python3", "buildbot/compile.py"
+    system "cmake", "--install", "build", "--prefix", prefix
+  end
+
+  test do
+    system "echo"
+  end
+end
