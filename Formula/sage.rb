0b8ece49869faa7296e446c0a4b1d8366ab58197
PaperChalice <lgamma@163.com>
2021-06-14 10:46:24 +0800

    add more pkg

--- Formula/sage.rb
+++ Formula/sage.rb
@@ -1,12 +1,14 @@
 class Sage < Formula
   desc "Free open-source mathematics software"
   homepage "https://www.sagemath.org/"
-  url "http://mirrors.mit.edu/sage/src/sage-9.3.tar.gz"
+  url "https://mirrors.mit.edu/sage/src/sage-9.3.tar.gz"
   sha256 "ab57d72579ed8f66bf806055d2cf8d42c45c2d8d0a82469c8bacdacb2ebab31e"
   license "GPL-3.0-only"
 
   depends_on "cmake"      => :build
   depends_on "m4"         => :build
+  depends_on "ninja"      => :build
+  depends_on "pandoc"     => :build
   depends_on "perl"       => :build
   depends_on "pkg-config" => :build
   depends_on "python"     => :build
@@ -15,10 +17,11 @@ class Sage < Formula
   depends_on "arb"
   depends_on "bdw-gc"
   depends_on "boost"
+  depends_on "cddlib"
   depends_on "flint"
   depends_on "fplll"
   depends_on "freetype"
-  depends_on "gcc"
+  depends_on "gcc@9"
   depends_on "gd"
   depends_on "glpk"
   depends_on "gmp"
@@ -29,24 +32,30 @@ class Sage < Formula
   depends_on "isl"
   depends_on "libatomic_ops"
   depends_on "libmpc"
+  depends_on "libogg"
   depends_on "libpng"
   depends_on "mpfi"
   depends_on "mpfr"
   depends_on "nauty"
+  depends_on "node"
   depends_on "ntl"
   depends_on "openblas"
   depends_on "openssl"
+  depends_on "pari"
   depends_on "pcre"
   depends_on "ppl"
   depends_on "r"
+  depends_on "sphinx-doc"
   depends_on "suite-sparse"
   depends_on "tcl-tk"
+  depends_on "tdlib"
   depends_on "xz"
   depends_on "yasm"
   depends_on "zeromq"
   depends_on "zlib"
 
   uses_from_macos "libedit"
+  uses_from_macos "libiconv"
   uses_from_macos "libxml2"
   uses_from_macos "sqlite"
   uses_from_macos "zlib"
@@ -55,6 +64,9 @@ class Sage < Formula
     ENV.cxx11
     args = %W[
       --prefix=#{prefix}
+      --enable-libogg
+      --enable-nodejs
+      --enable-tdlib
     ]
     system "./configure", *args
     system "make"
