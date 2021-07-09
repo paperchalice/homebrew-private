84a5a5388114b2c52b18ec28506edbd58977aff9
PaperChalice <lgamma@163.com>
2021-07-09 13:43:12 +0800

    llvm-core: 12.0.1

--- Formula/llvm-core.rb
+++ Formula/llvm-core.rb
@@ -2,8 +2,8 @@ class LlvmCore < Formula
   desc "Next-gen compiler infrastructure"
   homepage "https://llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-12.0.0",
-    revision: "d28af7c654d8db0b68c175db5ce212d74fb5e9bc"
+    tag:      "llvmorg-12.0.1",
+    revision: "fed41342a82f5a3a9201819a82bf7a48313e296b"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   livecheck do
@@ -27,7 +27,7 @@ class LlvmCore < Formula
   uses_from_macos "zlib"
 
   def install
-    inreplace "lib/Support/Unix/Path.inc", /(?<=return )link_path/, "exe_path"
+    inreplace "llvm/lib/Support/Unix/Path.inc", /(?<=return )link_path/, "exe_path"
 
     args = std_cmake_args + %w[
       -D BUILD_SHARED_LIBS=ON
