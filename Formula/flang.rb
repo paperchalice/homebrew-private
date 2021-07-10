7d38cb2214303945425471b286b175eb12ccd930
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-07-10 03:08:04 +0000

    flang: update 12.0.1 bottle.

--- Formula/flang.rb
+++ Formula/flang.rb
@@ -8,8 +8,8 @@ class Flang < Formula
   head "https://github.com/llvm/llvm-project.git"
 
   bottle do
-    root_url "https://github.com/paperchalice/homebrew-private/releases/download/flang-12.0.0"
-    sha256 cellar: :any, big_sur: "8bdf10eae21a502c4103df0079d6824fe79232576a1845122ad4ae832b87e018"
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/flang-12.0.1"
+    sha256 cellar: :any, big_sur: "92049324d48a9dfa969496d49a27bf3ea0524b166415de9ad1e562db4adbcade"
   end
 
   depends_on "cmake" => :build
