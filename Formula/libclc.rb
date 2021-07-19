cf35ec6679e23590895a1a660d9929ec1029d8c9
PaperChalice <lgamma@163.com>
2021-07-19 10:08:35 +0800

    add clang

--- Formula/libclc.rb
+++ Formula/libclc.rb
@@ -7,6 +7,7 @@ class Libclc < Formula
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   depends_on "cmake"                 => :build
+  depends_on "clang"                 => :build
   depends_on "llvm-core"             => :build
   depends_on "spirv-llvm-translator" => :build
 
