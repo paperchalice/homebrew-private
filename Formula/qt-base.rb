6ab1a2177406910ce6a6f32497017af7e127c31d
PaperChalice <lgamma@163.com>
2021-07-24 21:08:08 +0800

    fix

--- Formula/qt-base.rb
+++ Formula/qt-base.rb
@@ -75,10 +75,9 @@ class QtBase < Formula
     system "cmake", "--install", ".", "--strip"
 
     rm bin/"qt-cmake-private-install.cmake"
-    %w[qmake qtdiag].each do |sym|
-      rm bin/sym
-      bin.install_symlink bin/"#{sym}#{version.major}" => sym
-    end
+    rm bin/"qmake"
+    bin.install_symlink bin/"qmake#{version.major}" => "qmake"
+
     inreplace lib/"cmake/Qt6/qt.toolchain.cmake", HOMEBREW_SHIMS_PATH/"mac/super", "/usr/bin"
 
     # Some config scripts will only find Qt in a "Frameworks" folder
