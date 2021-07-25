d1f8fd40899fd002339b9b58ed343880c5b42f10
PaperChalice <lgamma@163.com>
2021-07-25 12:45:15 +0800

    add more components

--- cmd/upgrade-qt.rb
+++ cmd/upgrade-qt.rb
@@ -13,9 +13,11 @@ module Homebrew
 
     url_template = "https://download.qt.io/official_releases/qt/#{new_version.major}.#{new_version.minor}/#{new_version}/submodules/qt%s-everywhere-src-#{new_version}.tar.xz"
     %w[
-      3d base charts datavis3d declarative
-      imageformats lottie networkauth quick-controls2
-      quick-timeline quick3d scxml shader-tools svg tools translations
+      3d base charts connectivity datavis3d declarative
+      imageformats location lottie multimedia networkauth quick-controls2
+      quick-timeline quick3d remote-objects scxml sensors
+      serial-bus serial-port shader-tools svg tools translations web-channel
+      web-sockets web-view
     ].each do |c|
       f = Formula["qt-#{c}"]
       old_url = f.stable.url
