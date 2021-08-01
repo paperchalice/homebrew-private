6b7d9c585f264537a3e1763732aa5a7cc651be77
PaperChalice <lgamma@163.com>
2021-08-01 21:10:41 +0800

    qmmp

---
+++ Formula/qmmp.rb
@@ -0,0 +1,71 @@
+class Qmmp < Formula
+  desc "Qt-based Multimedia Player"
+  homepage "https://qmmp.ylsoftware.com/"
+  url "https://qmmp.ylsoftware.com/files/qmmp/2.0/qmmp-2.0.0.tar.bz2"
+  sha256 "c631d69c8bfcd77746bb94e2fc4cb7186d16cd29598de08d9771a45c212c6519"
+  license "GPL-2.0-or-later"
+  head "https://svn.code.sf.net/p/qmmp-dev/code"
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/qmmp[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  depends_on "cmake"      => :build
+  depends_on "pkg-config" => :build
+
+  # TODO: on linux: pipewire
+  depends_on "faad2"
+  depends_on "ffmpeg"
+  depends_on "flac"
+  depends_on "game-music-emu"
+  depends_on "jack"
+  depends_on "libarchive"
+  depends_on "libbs2b"
+  depends_on "libcddb"
+  depends_on "libcdio"
+  depends_on "libmms"
+  depends_on "libmodplug"
+  depends_on "libogg"
+  depends_on "libsamplerate"
+  depends_on "libshout"
+  depends_on "libsndfile"
+  depends_on "libsoxr"
+  depends_on "libvorbis"
+  depends_on "libxcb"
+  depends_on "mad"
+  depends_on "mplayer"
+  depends_on "musepack"
+  depends_on "opus"
+  depends_on "opusfile"
+  depends_on "projectm"
+  depends_on "pulseaudio"
+  depends_on "qt"
+  depends_on "taglib"
+  depends_on "wavpack"
+  depends_on "wildmidi"
+
+  uses_from_macos "curl"
+  uses_from_macos "libiconv"
+
+  def install
+    inreplace "src/qmmp/CMakeLists.txt", "Qt6::Widgets", "Qt6::Widgets iconv"
+    cmake_args = std_cmake_args + %w[
+      -DUSE_SKINNED=ON
+      -DUSE_ENCA=ON
+      -DUSE_QMMP_DIALOG=ON
+      -DCMAKE_EXE_LINKER_FLAGS=-Wl,-undefined,dynamic_lookup
+      -DCMAKE_SHARED_LINKER_FLAGS=-Wl,-undefined,dynamic_lookup
+
+      -S .
+      -B build
+    ]
+    system "cmake", *cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system bin/"qmmp", "--version"
+  end
+end
