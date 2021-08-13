40fd1fb08ff16be385a90780cb900639b1dc6494
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 06:29:18 +0000

    boost-assert: add 1.77.0 bottle.

--- Formula/boost-assert.rb
+++ Formula/boost-assert.rb
@@ -5,6 +5,11 @@ class BoostAssert < Formula
     tag:      "boost-1.77.0",
     revision: "6aabfebae6d4acf996fe711de4e9b444ad88c17d"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-assert-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "fb2403bddc077f46112f8711be031784a87bb7d55c0a0adf00d81f62321b6432"
+  end
+
   def install
     prefix.install "include"
   end
