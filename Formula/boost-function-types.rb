bec6f32615ee9729cffec008f2cab7abb33499ef
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 08:33:25 +0000

    boost-function-types: add 1.77.0 bottle.

--- Formula/boost-function-types.rb
+++ Formula/boost-function-types.rb
@@ -5,6 +5,11 @@ class BoostFunctionTypes < Formula
     tag:      "boost-1.77.0",
     revision: "895335874d67987ada0d8bf6ca1725e70642ed49"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-function-types-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "edb39b8b72842fde3321b790f6727748542d123dde57561c99de89b1365bee42"
+  end
+
   def install
     prefix.install "include"
   end
