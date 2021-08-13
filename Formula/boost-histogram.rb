8aaa596df497785f09f88aeac4bb7496849651c7
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-histogram: add 1.77.0 bottle.

--- Formula/boost-histogram.rb
+++ Formula/boost-histogram.rb
@@ -5,6 +5,11 @@ class BoostHistogram < Formula
     tag:      "boost-1.77.0",
     revision: "90a58d03ee4a1eafb66ecfb642bb9344256cd17e"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-histogram-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "cb459900af8d55c9c12c5b63bf663df750c653403c0a2d98641106e2ed98e0a7"
+  end
+
   def install
     prefix.install "include"
   end
