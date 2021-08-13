70309f1069a741d8131b1dbfcc674d3c32282017
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-property-map: add 1.77.0 bottle.

--- Formula/boost-property-map.rb
+++ Formula/boost-property-map.rb
@@ -5,6 +5,11 @@ class BoostPropertyMap < Formula
     tag:      "boost-1.77.0",
     revision: "6ed5bffff77d0ca5502d581ddc557208a3f43cd7"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-property-map-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "f0420a61fc2bfb9ab6fda268e0dfa55110bbbbee93b70171c1fd70d858f65368"
+  end
+
   def install
     prefix.install "include"
   end
