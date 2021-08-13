807bef59fbf3c20a71197d2a301d5f3c1f33352d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-static-assert: add 1.77.0 bottle.

--- Formula/boost-static-assert.rb
+++ Formula/boost-static-assert.rb
@@ -5,6 +5,11 @@ class BoostStaticAssert < Formula
     tag:      "boost-1.77.0",
     revision: "392199f6b14ee64260afde27a1c3f876c4bd4843"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-static-assert-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "d9b2d26b91d423626a4eece882982a355c594406b6b1d133f0b765d61720cabc"
+  end
+
   def install
     prefix.install "include"
   end
