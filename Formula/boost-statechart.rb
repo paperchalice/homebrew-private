08fe5c6c84ec3bbf0a09ac2fe4ad84f57fb1bbd4
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:14 +0000

    boost-statechart: add 1.77.0 bottle.

--- Formula/boost-statechart.rb
+++ Formula/boost-statechart.rb
@@ -5,6 +5,11 @@ class BoostStatechart < Formula
     tag:      "boost-1.77.0",
     revision: "586445b824c5cf0e7e6ce4ff2df620fda5d0f0d7"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-statechart-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "800a20acf0dbf4b7f07f23c365d1509b08361b49c5cd4c0276dc5d7a05fd96f8"
+  end
+
   def install
     prefix.install "include"
   end
