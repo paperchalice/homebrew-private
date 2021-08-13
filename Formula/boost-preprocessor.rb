3db7bc1e76122e26f53a6165af582753724843bb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-preprocessor: add 1.77.0 bottle.

--- Formula/boost-preprocessor.rb
+++ Formula/boost-preprocessor.rb
@@ -5,6 +5,11 @@ class BoostPreprocessor < Formula
     tag:      "boost-1.77.0",
     revision: "d4e82d7d3f2e2adbe280966ac51ce8f9372f5a44"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-preprocessor-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "dc0a25d9dd2f4dc8db65f2c76096cd2b00ef191dc724e4fd55f6684e3b5ad014"
+  end
+
   def install
     prefix.install "include"
   end
