316d755842eb08a8dda571644f6de7e8caf4a641
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-throw-exception: add 1.77.0 bottle.

--- Formula/boost-throw-exception.rb
+++ Formula/boost-throw-exception.rb
@@ -5,6 +5,11 @@ class BoostThrowException < Formula
     tag:      "boost-1.77.0",
     revision: "95e02ea52b8525ecf34dbf1e7fae34af09986b8a"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-throw-exception-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "bf9a21a3766b899b5f9a044e720c57f2b911bb41d221f33815eb840671f3cac6"
+  end
+
   def install
     prefix.install "include"
   end
