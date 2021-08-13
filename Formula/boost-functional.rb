b3f6bcc85b929c1d4fc3af42d5588f5acfb0746d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 08:33:25 +0000

    boost-functional: add 1.77.0 bottle.

--- Formula/boost-functional.rb
+++ Formula/boost-functional.rb
@@ -5,6 +5,11 @@ class BoostFunctional < Formula
     tag:      "boost-1.77.0",
     revision: "0c7697fc661cc0879dd55db63b3d44725a71b400"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-functional-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "d3536643bdb8412b8861dec3be408882b95c8dddf30403b8dcfd4e5aba9fd689"
+  end
+
   def install
     prefix.install "include"
   end
