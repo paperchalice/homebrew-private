e5655175b4f2cbda4faeedd21baddc9ccef10429
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-io: add 1.77.0 bottle.

--- Formula/boost-io.rb
+++ Formula/boost-io.rb
@@ -5,6 +5,11 @@ class BoostIo < Formula
     tag:      "boost-1.77.0",
     revision: "7392d7274c3743e605bf58076566c258617833ed"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-io-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "3e289562c262ca7d591dc7845efbfa0950233aaca9f2cf830472d67cd94c3f66"
+  end
+
   def install
     prefix.install "include"
   end
