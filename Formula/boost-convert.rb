c6af36bc96c26b396426a8e4e50f849d21ac41eb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:53:32 +0000

    boost-convert: add 1.77.0 bottle.

--- Formula/boost-convert.rb
+++ Formula/boost-convert.rb
@@ -5,6 +5,11 @@ class BoostConvert < Formula
     tag:      "boost-1.77.0",
     revision: "d5f7347d309d222d10e7a204ddd086e7d6c64b21"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-convert-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "a453a3c1494efd466ec3ec3f2923befa81ff7146ec247ee1c5ab767855e6bcc5"
+  end
+
   def install
     prefix.install "include"
   end
