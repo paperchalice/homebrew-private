9f5192b3a5fad9ee5bb440ffd0ba29a9e7226fd4
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:31:32 +0000

    boost-bind: add 1.77.0 bottle.

--- Formula/boost-bind.rb
+++ Formula/boost-bind.rb
@@ -5,6 +5,11 @@ class BoostBind < Formula
     tag:      "boost-1.77.0",
     revision: "34a3ee580c88d623c6bc9fe91460ef414dabfbe6"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-bind-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "28f48a9f57fbb5129dc1a2af2baea7093a2fc69c8691fe37643fed955a729707"
+  end
+
   def install
     prefix.install "include"
   end
