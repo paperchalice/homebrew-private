c41a17e893c559ffc8989f5ed39b766b5f09d0cb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-integer: add 1.77.0 bottle.

--- Formula/boost-integer.rb
+++ Formula/boost-integer.rb
@@ -5,6 +5,11 @@ class BoostInteger < Formula
     tag:      "boost-1.77.0",
     revision: "8fd622545f3e303f467526442a8765290e735778"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-integer-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "09612746b9ea2e92e1636f2dc79c435203fceff21c5e4706235b826edbaa8beb"
+  end
+
   def install
     prefix.install "include"
   end
