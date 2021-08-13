78b8a73c0727cc7f16c8337943ed5d412b841602
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-predef: add 1.77.0 bottle.

--- Formula/boost-predef.rb
+++ Formula/boost-predef.rb
@@ -5,6 +5,11 @@ class BoostPredef < Formula
     tag:      "boost-1.77.0",
     revision: "e3a87328f415c61766838e1f5367885251b46e93"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-predef-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "457d2c068d7d5f95ec266268ef129e4000b671252bf119abf7c3f227b9c602db"
+  end
+
   def install
     prefix.install "include"
   end
