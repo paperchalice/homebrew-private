b97e5f47c87dcc0c6e97c15bc1aa82b977a4a28a
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 06:29:18 +0000

    boost-align: add 1.77.0 bottle.

--- Formula/boost-align.rb
+++ Formula/boost-align.rb
@@ -5,6 +5,11 @@ class BoostAlign < Formula
     tag:      "boost-1.77.0",
     revision: "0790cd45c8e05b1b59fffbc948b6bcb26eb6a2aa"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-align-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "2057b12d571596ddad3808afb897302f986d813984076939b233debc7557ef7a"
+  end
+
   def install
     prefix.install "include"
   end
