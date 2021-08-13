7aed2320ed62577baa88eb99029632c60b8a5ca8
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 08:33:26 +0000

    boost-flyweight: add 1.77.0 bottle.

--- Formula/boost-flyweight.rb
+++ Formula/boost-flyweight.rb
@@ -5,6 +5,11 @@ class BoostFlyweight < Formula
     tag:      "boost-1.77.0",
     revision: "af4fd3e8eb532099cec51bb6f199029b65e7998d"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-flyweight-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "283447fd3762e6eb6fe3c1bf5e60e5739e317a6c293eb4ebb17b9f206574c6bd"
+  end
+
   def install
     prefix.install "include"
   end
