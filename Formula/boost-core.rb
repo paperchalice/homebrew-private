34c64558ddaa497321a438c996eb391dfc8f5b74
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:53:32 +0000

    boost-core: add 1.77.0 bottle.

--- Formula/boost-core.rb
+++ Formula/boost-core.rb
@@ -5,6 +5,11 @@ class BoostCore < Formula
     tag:      "boost-1.77.0",
     revision: "f4b3d5dba6f86caaf96e45901655a954b2ff68b4"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-core-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "f555800aec628fd1c6cff6f66ada092202d42d8c8158b07bccf799570adf9578"
+  end
+
   def install
     prefix.install "include"
   end
