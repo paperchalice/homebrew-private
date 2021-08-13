027fe587e39b9e5eefd03a66675566c0443933e6
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-graph-parallel: add 1.77.0 bottle.

--- Formula/boost-graph-parallel.rb
+++ Formula/boost-graph-parallel.rb
@@ -5,6 +5,11 @@ class BoostGraphParallel < Formula
     tag:      "boost-1.77.0",
     revision: "476623e72b93faaf11888ad7c70a1bd301618858"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-graph-parallel-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "9e863f43b6a235d79c3385890f0b2a8cbb0a49fe6c4ad7c7eb98096eb443aade"
+  end
+
   def install
     prefix.install "include"
   end
