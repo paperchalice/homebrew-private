2c7eccec78eaabcd2a95f0c77ebdea6143c7d757
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 08:33:25 +0000

    boost-endian: add 1.77.0 bottle.

--- Formula/boost-endian.rb
+++ Formula/boost-endian.rb
@@ -5,6 +5,11 @@ class BoostEndian < Formula
     tag:      "boost-1.77.0",
     revision: "14dd63931211782a2169c8146e459afe20f92239"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-endian-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "27315ce407ee268d1a11bac836accb4a7f607c884639c66ae87f102d39a698a0"
+  end
+
   def install
     prefix.install "include"
   end
