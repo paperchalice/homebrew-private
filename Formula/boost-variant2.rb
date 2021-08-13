cf051a8f8d0205374d2b45211e30837e4e1c7262
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-variant2: add 1.77.0 bottle.

--- Formula/boost-variant2.rb
+++ Formula/boost-variant2.rb
@@ -5,6 +5,11 @@ class BoostVariant2 < Formula
     tag:      "boost-1.77.0",
     revision: "4153a535a0fa8eb4d18abc262fcf2ae834601261"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-variant2-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "6b48e76d944422c32562bfd337099b50bc3b512aedb486834e960985346d7bdf"
+  end
+
   def install
     prefix.install "include"
   end
