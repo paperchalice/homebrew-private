d2f201aa7706ee8235edb3f957c287feb15619ac
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-iterator: add 1.77.0 bottle.

--- Formula/boost-iterator.rb
+++ Formula/boost-iterator.rb
@@ -5,6 +5,11 @@ class BoostIterator < Formula
     tag:      "boost-1.77.0",
     revision: "72a7fb1b7372cf18df331e0ebbfb555f244c03fe"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-iterator-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "32df2237d8c37e34ab488854bda5fde5b62298c63a386ac80c3e59356f2b79f0"
+  end
+
   def install
     prefix.install "include"
   end
