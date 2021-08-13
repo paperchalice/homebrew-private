380c69b2504e876cbc9e143d502a769c776de97a
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-heap: add 1.77.0 bottle.

--- Formula/boost-heap.rb
+++ Formula/boost-heap.rb
@@ -5,6 +5,11 @@ class BoostHeap < Formula
     tag:      "boost-1.77.0",
     revision: "dc2f19f8815cbe0654df61bfc5f31ad8b06fc883"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-heap-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "f95d43b7e3b8f89a372a9ca3b47216f37ed884fd2577cea785024c25aaacdc09"
+  end
+
   def install
     prefix.install "include"
   end
