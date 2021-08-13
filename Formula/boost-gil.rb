5bfe4cef54959fb5020730b22696eae9596c5daf
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-gil: add 1.77.0 bottle.

--- Formula/boost-gil.rb
+++ Formula/boost-gil.rb
@@ -5,6 +5,11 @@ class BoostGil < Formula
     tag:      "boost-1.77.0",
     revision: "e3e779cc69af4905e4910c06f7e9417c2563e362"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-gil-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "6b97585572e9c111aaa9175a933eb992b6876d52b7e3da36dc79d58375c768a5"
+  end
+
   def install
     prefix.install "include"
   end
