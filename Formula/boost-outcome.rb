a6556abfb383b2ac1e15ef155e330fb89f289647
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-outcome: add 1.77.0 bottle.

--- Formula/boost-outcome.rb
+++ Formula/boost-outcome.rb
@@ -5,6 +5,11 @@ class BoostOutcome < Formula
     tag:      "boost-1.77.0",
     revision: "e9439030d49c2d70391d8a74509dfcdbcea3881e"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-outcome-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "f30bbe2cb8583585316a01ec445d69fa081571f937c3ae7ea11353ed6658a5f5"
+  end
+
   def install
     prefix.install "include"
   end
