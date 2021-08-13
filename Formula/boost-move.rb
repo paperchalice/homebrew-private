a060db6343d4035f1b3bfd7074fc880b0485bbb4
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-move: add 1.77.0 bottle.

--- Formula/boost-move.rb
+++ Formula/boost-move.rb
@@ -5,6 +5,11 @@ class BoostMove < Formula
     tag:      "boost-1.77.0",
     revision: "6ab49a8d82909ffa6675325832f3e5f45f1bbb51"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-move-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "0601f5ed2f085f109e2ae2ee47c6b8b604d10ad842c7eed67bc8d48781a48372"
+  end
+
   def install
     prefix.install "include"
   end
