dc186638e7a06187494cf16c1a9609a1709cc1c6
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-static-string: add 1.77.0 bottle.

--- Formula/boost-static-string.rb
+++ Formula/boost-static-string.rb
@@ -5,6 +5,11 @@ class BoostStaticString < Formula
     tag:      "boost-1.77.0",
     revision: "6978da552efcf9a4394c747553ea75f53e3af832"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-static-string-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "67dc02ec6fef3f899c0d271213b394269fbdc861eb8d1e5147735d9be7fcf5ab"
+  end
+
   def install
     prefix.install "include"
   end
