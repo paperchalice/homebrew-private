abd001367ce295eb7dd93b33376b0a8c78dd35f7
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-ptr-container: add 1.77.0 bottle.

--- Formula/boost-ptr-container.rb
+++ Formula/boost-ptr-container.rb
@@ -5,6 +5,11 @@ class BoostPtrContainer < Formula
     tag:      "boost-1.77.0",
     revision: "f1b0910503fb2e6329e9c852261fa18e325cb09b"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-ptr-container-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "1a9b29bab3692e30f42f0cf14a8c5ab7ffaa425b1bd3d0b3e1e2e7b78f23313d"
+  end
+
   def install
     prefix.install "include"
   end
