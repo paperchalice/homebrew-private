cfbd11aea05af7751f8ad2ec51b57d2841e3d05e
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-lexical-cast: add 1.77.0 bottle.

--- Formula/boost-lexical-cast.rb
+++ Formula/boost-lexical-cast.rb
@@ -5,6 +5,11 @@ class BoostLexicalCast < Formula
     tag:      "boost-1.77.0",
     revision: "934858fbdc86aead9831e579dc11575c579f577d"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-lexical-cast-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "54ea7e23f2d0fda260e7463e7486f652694fb9fdd609eca3e96704a6ea6068ed"
+  end
+
   def install
     prefix.install "include"
   end
