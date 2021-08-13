30decb9fac6a49c8feedca74f6c699d68bbe937d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-property-tree: add 1.77.0 bottle.

--- Formula/boost-property-tree.rb
+++ Formula/boost-property-tree.rb
@@ -5,6 +5,11 @@ class BoostPropertyTree < Formula
     tag:      "boost-1.77.0",
     revision: "d30ff9404bd6af5cc8922a177865e566f4846b19"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-property-tree-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "58d93fae4ea2da6ccbe2905024bcdd914e74e53c4e0420a239ff0e17502313ef"
+  end
+
   def install
     prefix.install "include"
   end
