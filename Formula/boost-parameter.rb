14f14e31b0a73a8191621e838889008afe422457
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-parameter: add 1.77.0 bottle.

--- Formula/boost-parameter.rb
+++ Formula/boost-parameter.rb
@@ -5,6 +5,11 @@ class BoostParameter < Formula
     tag:      "boost-1.77.0",
     revision: "94bfc0544a5c4b959d018814f25c54e796020578"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-parameter-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "52918c748bbaafdada35a957091dc9cc8f33fb97ec176c48e1397596b8422b5d"
+  end
+
   def install
     prefix.install "include"
   end
