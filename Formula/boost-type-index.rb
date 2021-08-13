e24ef1214bc939b7948584625509ce729a09c389
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-type-index: add 1.77.0 bottle.

--- Formula/boost-type-index.rb
+++ Formula/boost-type-index.rb
@@ -5,6 +5,11 @@ class BoostTypeIndex < Formula
     tag:      "boost-1.77.0",
     revision: "a3c6a957eeaf1612eb34b013ec13e67d4c279a41"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-type-index-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "8435c281b378c1910965298e62629ae73b8499e8539ad77623dc43cc8705b8c8"
+  end
+
   def install
     prefix.install "include"
   end
