eab2f5fa90f1e2d25e032c471bd6a6e86c1a1d86
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-process: add 1.77.0 bottle.

--- Formula/boost-process.rb
+++ Formula/boost-process.rb
@@ -5,6 +5,11 @@ class BoostProcess < Formula
     tag:      "boost-1.77.0",
     revision: "b2a96a3e139ae42ea2bcb80cc58f108494a995c5"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-process-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "191ec53727b95153c6e99d3330983819b4800312e055d92a5724aea0427cea4c"
+  end
+
   def install
     prefix.install "include"
   end
