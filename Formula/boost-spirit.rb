90a488a1a3c2064019e722d8d7f2ac1838619a33
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-spirit: add 1.77.0 bottle.

--- Formula/boost-spirit.rb
+++ Formula/boost-spirit.rb
@@ -5,6 +5,11 @@ class BoostSpirit < Formula
     tag:      "boost-1.77.0",
     revision: "eeb2f2052f4ae7f77bca1bef24916c558832be83"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-spirit-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "ed490f451094abe51dc8666e0899da700fb04782c2f8b2fb6425a028f6d1a575"
+  end
+
   def install
     prefix.install "include"
   end
