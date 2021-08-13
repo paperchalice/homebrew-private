28c97c10ff9616931731bfc29df812e42898b66e
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:14 +0000

    boost-yap: add 1.77.0 bottle.

--- Formula/boost-yap.rb
+++ Formula/boost-yap.rb
@@ -5,6 +5,11 @@ class BoostYap < Formula
     tag:      "boost-1.77.0",
     revision: "262624ac36652d1354e6b2ee38a79f2bc9a66cae"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-yap-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "72db677efcaefbf53c23debefc12d144af20c5b5d0b0107874fcda28807bee11"
+  end
+
   def install
     prefix.install "include"
   end
