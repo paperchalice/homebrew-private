12072aa3a9f62b3f1b8e1b80eefde3c081c21db8
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-range: add 1.77.0 bottle.

--- Formula/boost-range.rb
+++ Formula/boost-range.rb
@@ -5,6 +5,11 @@ class BoostRange < Formula
     tag:      "boost-1.77.0",
     revision: "88c6199aedf8bbb5a6a8966e534f9de99943cde2"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-range-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "cf19d70285dbfc2a5c28d8675a202a5cc14d8dcf5442bc05efe0d3ea4e7cbe75"
+  end
+
   def install
     prefix.install "include"
   end
