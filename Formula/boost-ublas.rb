93025cb32d779e00316690a3abea11a60103f04c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-ublas: add 1.77.0 bottle.

--- Formula/boost-ublas.rb
+++ Formula/boost-ublas.rb
@@ -5,6 +5,11 @@ class BoostUblas < Formula
     tag:      "boost-1.77.0",
     revision: "f0e55caf310d5e01c7e9f2190b2422e113ddeedb"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-ublas-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "8b54c7c277bbedd6f8ef15d243f38c1d73d9b6b63f8e85ac8f46179c94f6eb60"
+  end
+
   def install
     prefix.install "include"
   end
