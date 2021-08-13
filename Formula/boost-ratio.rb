48c2d4b481b5ec340fddeb769501bbb87ff324cf
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-ratio: add 1.77.0 bottle.

--- Formula/boost-ratio.rb
+++ Formula/boost-ratio.rb
@@ -5,6 +5,11 @@ class BoostRatio < Formula
     tag:      "boost-1.77.0",
     revision: "00073b7d5896603b2036a334253dc9784285355c"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-ratio-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "44b3b3c79476bd70ed21ae38e7937ff72c2ab83d17281fb6975cb87058127c66"
+  end
+
   def install
     prefix.install "include"
   end
