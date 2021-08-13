91790eabed295e57dbf383dc29a8b4938b04998c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-parameter-python: add 1.77.0 bottle.

--- Formula/boost-parameter-python.rb
+++ Formula/boost-parameter-python.rb
@@ -5,6 +5,11 @@ class BoostParameterPython < Formula
     tag:      "boost-1.77.0",
     revision: "787d8d38d9fd49c34a757b20361f8042dd5ac820"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-parameter-python-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "e49ea432fc694163728eff35bea165ce1bbb7322e749c1e26847395ee1ad322f"
+  end
+
   def install
     prefix.install "include"
   end
