3f7bbc878e2a0169e321e315a7c4d00d278dd3f2
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-hana: add 1.77.0 bottle.

--- Formula/boost-hana.rb
+++ Formula/boost-hana.rb
@@ -5,6 +5,11 @@ class BoostHana < Formula
     tag:      "boost-1.77.0",
     revision: "5c28aad03b6e157452d8623802d70dc95a7b57b6"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-hana-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "0335a596d6221c7a33d4776c454ec2d32b8ba4b3d07ef9c73180ec3228b81da6"
+  end
+
   def install
     prefix.install "include"
   end
