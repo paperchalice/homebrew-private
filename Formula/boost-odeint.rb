2c09b9d7d89e5eb5adb6875c88822d63d6d8e994
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-odeint: add 1.77.0 bottle.

--- Formula/boost-odeint.rb
+++ Formula/boost-odeint.rb
@@ -5,6 +5,11 @@ class BoostOdeint < Formula
     tag:      "boost-1.77.0",
     revision: "db8f91a51da630957d6bfa1ff87be760b0be97a6"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-odeint-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "7ff5f64d7af6313037f0c2d457df382eff9aabeda639bc8c7850e6ec8c415341"
+  end
+
   def install
     prefix.install "include"
   end
