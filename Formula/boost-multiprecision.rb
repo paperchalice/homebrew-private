3d506fd22c1fcc62fb835f6a687bf826197f1f5d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-multiprecision: add 1.77.0 bottle.

--- Formula/boost-multiprecision.rb
+++ Formula/boost-multiprecision.rb
@@ -5,6 +5,11 @@ class BoostMultiprecision < Formula
     tag:      "boost-1.77.0",
     revision: "e7c501f72462e0bf40c3ee4bbde990fc5aa0101c"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-multiprecision-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "19da5179c2fc1a621df501cc0e1a6d71a41e533a1db4e94036571503486e9637"
+  end
+
   def install
     prefix.install "include"
   end
