c1a0b3a6422db2e4e0d2fcc1001418880bbc9e40
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:31:32 +0000

    boost-beast: add 1.77.0 bottle.

--- Formula/boost-beast.rb
+++ Formula/boost-beast.rb
@@ -5,6 +5,11 @@ class BoostBeast < Formula
     tag:      "boost-1.77.0",
     revision: "710cc53331f197f6f17e8c38454c09df68e43c03"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-beast-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "e7ae27bd70c79d11b28468784607dad75734fdc576ae9a9d09afcd8474b17bb4"
+  end
+
   def install
     prefix.install "include"
   end
