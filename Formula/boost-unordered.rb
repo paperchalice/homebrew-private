43e908bbc175885eca7f92781f7cfcefbf3080f6
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-unordered: add 1.77.0 bottle.

--- Formula/boost-unordered.rb
+++ Formula/boost-unordered.rb
@@ -5,6 +5,11 @@ class BoostUnordered < Formula
     tag:      "boost-1.77.0",
     revision: "c494b3db58344aa60ac362887451776a1c5d56c8"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-unordered-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "3a260f6326a3a946a5fcb62afcd1f0db4070e4c8a6f1124821c9be47f0342ffc"
+  end
+
   def install
     prefix.install "include"
   end
