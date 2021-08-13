a594ba9200b01c63b13f76c60740c040cc399a26
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-typeof: add 1.77.0 bottle.

--- Formula/boost-typeof.rb
+++ Formula/boost-typeof.rb
@@ -5,6 +5,11 @@ class BoostTypeof < Formula
     tag:      "boost-1.77.0",
     revision: "46c7a05f826fc020ee88210ea2a5cd9278b930ab"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-typeof-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "872fe7f2c32b612e9b526ffdc766615346e4049d58df91bbf63a7aebfbeb8e00"
+  end
+
   def install
     prefix.install "include"
   end
