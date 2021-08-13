1864c70707e8cb31b5b798d592947b7a1949c4b2
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-tti: add 1.77.0 bottle.

--- Formula/boost-tti.rb
+++ Formula/boost-tti.rb
@@ -5,6 +5,11 @@ class BoostTti < Formula
     tag:      "boost-1.77.0",
     revision: "03734c54a51b6372ac3296d2fe5103b7360bcd3f"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-tti-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "c2e98e39874bc84e3b939b017673ebfc33ed66f1fa4437f05739227a073b3723"
+  end
+
   def install
     prefix.install "include"
   end
