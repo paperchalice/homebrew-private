97162acb276cbb82bc26da4d54b08121ea578049
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-multi-index: add 1.77.0 bottle.

--- Formula/boost-multi-index.rb
+++ Formula/boost-multi-index.rb
@@ -5,6 +5,11 @@ class BoostMultiIndex < Formula
     tag:      "boost-1.77.0",
     revision: "2bbf21cfc9fd8a782aa445eec178dbc15986e761"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-multi-index-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "ff07aae3320eabf8707480db1832417ca078088b178fcee8346075ca98509e39"
+  end
+
   def install
     prefix.install "include"
   end
