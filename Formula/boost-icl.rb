1b322a6f408be355b577845d90dc681485b1ab3d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-icl: add 1.77.0 bottle.

--- Formula/boost-icl.rb
+++ Formula/boost-icl.rb
@@ -5,6 +5,11 @@ class BoostIcl < Formula
     tag:      "boost-1.77.0",
     revision: "e6c06ddee1e2320f11c4ec5cd2661c4abe9bca53"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-icl-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "fffe1fc84914fe017118e8e5f3ab42697671b927fe61d38686c1f57cf8e1dd34"
+  end
+
   def install
     prefix.install "include"
   end
