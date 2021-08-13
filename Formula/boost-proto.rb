532de30c56f4760847b9c7d94b106844bf6d8656
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-proto: add 1.77.0 bottle.

--- Formula/boost-proto.rb
+++ Formula/boost-proto.rb
@@ -5,6 +5,11 @@ class BoostProto < Formula
     tag:      "boost-1.77.0",
     revision: "7f924934689b940f3a72212ab0f714ec6fd6e34b"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-proto-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "4c195b9813cab0cfb3551d5556d2670eebcd0861c82d232450eadade1a560d17"
+  end
+
   def install
     prefix.install "include"
   end
