c107d3c5ab5decbdac35731e2f2004805dea06b1
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-geometry: add 1.77.0 bottle.

--- Formula/boost-geometry.rb
+++ Formula/boost-geometry.rb
@@ -5,6 +5,11 @@ class BoostGeometry < Formula
     tag:      "boost-1.77.0",
     revision: "e2a0b14491e6af904882602ce1c47a0d74ad05a2"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-geometry-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "5ad1eaf5e27b6454b97ed38257dd26028fc0a13edb621aae0d841a0b62de03bc"
+  end
+
   def install
     prefix.install "include"
   end
