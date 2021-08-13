af55000fd286fcf4bf6cb76830151adc85ac0158
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:31:32 +0000

    boost-compute: add 1.77.0 bottle.

--- Formula/boost-compute.rb
+++ Formula/boost-compute.rb
@@ -5,6 +5,11 @@ class BoostCompute < Formula
     tag:      "boost-1.77.0",
     revision: "36c89134d4013b2e5e45bc55656a18bd6141995a"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-compute-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "4cc11a0297942513ad25ea34e1f67833ff4efcc553d8e64a24fc2ac5034345b7"
+  end
+
   def install
     prefix.install "include"
   end
