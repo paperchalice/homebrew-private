2bd4445b7ca406933d75ba5b4904d27d5aea0f25
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-optional: add 1.77.0 bottle.

--- Formula/boost-optional.rb
+++ Formula/boost-optional.rb
@@ -5,6 +5,11 @@ class BoostOptional < Formula
     tag:      "boost-1.77.0",
     revision: "5a444eb84b67a5035e3577476234189c57ca55c1"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-optional-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "a90c4a9a146ecfedbdd0944631526b6c85c92d418feedf36247a9861f40cf605"
+  end
+
   def install
     prefix.install "include"
   end
