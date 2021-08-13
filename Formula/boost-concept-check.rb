903191194ba565e019cb89c51f75039e4a5aee4e
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:31:32 +0000

    boost-concept-check: add 1.77.0 bottle.

--- Formula/boost-concept-check.rb
+++ Formula/boost-concept-check.rb
@@ -5,6 +5,11 @@ class BoostConceptCheck < Formula
     tag:      "boost-1.77.0",
     revision: "e34c735a1a6902de0d3e20ea58cfd8f101702458"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-concept-check-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "815bf1d5ce456796a9e312ef95b5a9c72ba8d5c815438956d1989bf7b359537f"
+  end
+
   def install
     prefix.install "include"
   end
