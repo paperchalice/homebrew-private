e7b8fb36d124843c036ebb74ed6c3ba3ba8ba7a3
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-mpl: add 1.77.0 bottle.

--- Formula/boost-mpl.rb
+++ Formula/boost-mpl.rb
@@ -5,6 +5,11 @@ class BoostMpl < Formula
     tag:      "boost-1.77.0",
     revision: "341748e4cb534f41fa3d0e34748f2771f5427123"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-mpl-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "04bafe7ee4edaf0195ecc08ea2eeb9fd73c35ca1fa80cd6a52eca4e4a2b6f537"
+  end
+
   def install
     prefix.install "include"
   end
