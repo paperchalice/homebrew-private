042a33be1f08e82a2b94e1985249453d38639e2d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-utility: add 1.77.0 bottle.

--- Formula/boost-utility.rb
+++ Formula/boost-utility.rb
@@ -5,6 +5,11 @@ class BoostUtility < Formula
     tag:      "boost-1.77.0",
     revision: "375382e1e6c677c7849e894675251db353ab9186"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-utility-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "786e0d4c7c504f47a88c9f7604bc6f4f7cb563f9273b6d55c4a3812f0596ea8c"
+  end
+
   def install
     prefix.install "include"
   end
