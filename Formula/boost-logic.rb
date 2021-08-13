2feee98dfa74202283d29f85e1034ba32c2e279d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-logic: add 1.77.0 bottle.

--- Formula/boost-logic.rb
+++ Formula/boost-logic.rb
@@ -5,6 +5,11 @@ class BoostLogic < Formula
     tag:      "boost-1.77.0",
     revision: "a5b56ff6fe7368e5f5ebbb374279ddc9fb49a2d5"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-logic-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "2d4280349628c7f983a94b65cca303bb8b19bd74b52ecaf8d82b620a190ae4ce"
+  end
+
   def install
     prefix.install "include"
   end
