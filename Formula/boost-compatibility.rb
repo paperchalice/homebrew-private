65bfc7a0abc0afd10c3b7255597d6def41943809
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 07:31:32 +0000

    boost-compatibility: add 1.77.0 bottle.

--- Formula/boost-compatibility.rb
+++ Formula/boost-compatibility.rb
@@ -5,6 +5,11 @@ class BoostCompatibility < Formula
     tag:      "boost-1.77.0",
     revision: "47ce71af6b018764c9ba74c0bfcb4f3151b81aa7"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-compatibility-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "29f23d7b71933638a94587c12658429586f7606af0500f273f7b52e183f3a0e9"
+  end
+
   def install
     prefix.install "include"
   end
