8f7a8b7e8936db152b9c4089f1bfc274f700e868
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:14 +0000

    boost-smart-ptr: add 1.77.0 bottle.

--- Formula/boost-smart-ptr.rb
+++ Formula/boost-smart-ptr.rb
@@ -5,6 +5,11 @@ class BoostSmartPtr < Formula
     tag:      "boost-1.77.0",
     revision: "72221d1da0ada73871ff5e0f8e60fe5ecbd7a296"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-smart-ptr-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "349b06f0e665921942b26c689bd3927b236ebad69063588002b9c52397ba313d"
+  end
+
   def install
     prefix.install "include"
   end
