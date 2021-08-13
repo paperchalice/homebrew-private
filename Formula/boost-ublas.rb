d06c65b5a23085bee42ecd6e76c0443bb2e7845e
PaperChalice <lgamma@163.com>
2021-08-13 20:34:46 +0800

    fix url

--- Formula/boost-ublas.rb
+++ Formula/boost-ublas.rb
@@ -1,6 +1,6 @@
 class BoostUblas < Formula
   desc "Awesome library from Boost"
-  homepage "https://boost.org/libs/ublas/"
+  homepage "https://boost.org/libs/numeric/ublas/"
   url "https://github.com/boostorg/ublas.git",
     tag:      "boost-1.77.0",
     revision: "f0e55caf310d5e01c7e9f2190b2422e113ddeedb"
