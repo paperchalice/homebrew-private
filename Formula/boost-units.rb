c1f8a82cc3c1e10969e1f7fa860c53d04ab1ccb3
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-units: add 1.77.0 bottle.

--- Formula/boost-units.rb
+++ Formula/boost-units.rb
@@ -5,6 +5,11 @@ class BoostUnits < Formula
     tag:      "boost-1.77.0",
     revision: "45787015dd8c11653eb988260acf05c4af9d42e5"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-units-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "11a526407bcae83e64b9658a8ce8b374787137ae7d163573d841d35ccc803b30"
+  end
+
   def install
     prefix.install "include"
   end
