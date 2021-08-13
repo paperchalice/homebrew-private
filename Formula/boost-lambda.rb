38a4ee91093368e10eb1b5d11f8f741368221b48
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-lambda: add 1.77.0 bottle.

--- Formula/boost-lambda.rb
+++ Formula/boost-lambda.rb
@@ -5,6 +5,11 @@ class BoostLambda < Formula
     tag:      "boost-1.77.0",
     revision: "4007043b545d3a834abaeaf141bb7dbd88c975cc"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-lambda-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "25e8fb0d1f3091e0fd588e18b131d8246fd2dff7e984779c5a8a962952916ad6"
+  end
+
   def install
     prefix.install "include"
   end
