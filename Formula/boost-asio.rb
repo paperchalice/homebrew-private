5980eebee60c778ea3769f93c598fdcf86a51d78
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 06:29:18 +0000

    boost-asio: add 1.77.0 bottle.

--- Formula/boost-asio.rb
+++ Formula/boost-asio.rb
@@ -5,6 +5,11 @@ class BoostAsio < Formula
     tag:      "boost-1.77.0",
     revision: "71431fcedeaf04be372c9d2c357be164c2742c43"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-asio-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "61db5a065ac54b2c6b953d05e2c4101d1cc8f390528402dc277a8cd0de955196"
+  end
+
   def install
     prefix.install "include"
   end
