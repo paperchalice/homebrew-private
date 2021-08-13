c5ca06d18ea127472f9eb90433d885bc6b0fa89d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-mpi: add 1.77.0 bottle.

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -5,6 +5,11 @@ class BoostMpi < Formula
     tag:      "boost-1.77.0",
     revision: "ace80263884ee2af3cef2ab3644ff4b41d154e49"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-mpi-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "4ef57b11a0e4ccbdc42621e8428d95ed9f0ef032e31f80a52262f48b75d544ee"
+  end
+
   def install
     prefix.install "include"
   end
