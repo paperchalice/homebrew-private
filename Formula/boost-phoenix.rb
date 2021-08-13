2d0b75bc2ed83f8feaf45548606525dc2ded6608
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 10:05:50 +0000

    boost-phoenix: add 1.77.0 bottle.

--- Formula/boost-phoenix.rb
+++ Formula/boost-phoenix.rb
@@ -5,6 +5,11 @@ class BoostPhoenix < Formula
     tag:      "boost-1.77.0",
     revision: "15500aec2187ab59e51d05addab0fdba7e788dbb"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-phoenix-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "5ec41649ffc527ffda697e9876836f1490bb5815e4575683c4861e1099e89a9a"
+  end
+
   def install
     prefix.install "include"
   end
