897576739d8b6663a0f17e6efd12914da44f055c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-tuple: add 1.77.0 bottle.

--- Formula/boost-tuple.rb
+++ Formula/boost-tuple.rb
@@ -5,6 +5,11 @@ class BoostTuple < Formula
     tag:      "boost-1.77.0",
     revision: "ec4f3b23c21581656f6241903e723e7fde761993"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-tuple-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "692670f6977f0515010a79c97bcac0e77171bc1d2c48cf55a1867d397ecdd20c"
+  end
+
   def install
     prefix.install "include"
   end
