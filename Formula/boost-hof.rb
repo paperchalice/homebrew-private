d8bcc7f6de2560630c028f80982610c2c5954e97
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-hof: add 1.77.0 bottle.

--- Formula/boost-hof.rb
+++ Formula/boost-hof.rb
@@ -5,6 +5,11 @@ class BoostHof < Formula
     tag:      "boost-1.77.0",
     revision: "0cd9c34d658fb58484002bb81fb5aff2c0be5599"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-hof-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "763a2ba5d4ba2562b46754cee6b85b97fabeae5b4d33fb375f78873557fd8619"
+  end
+
   def install
     prefix.install "include"
   end
