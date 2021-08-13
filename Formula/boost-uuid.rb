eaf3c3f81c1d44b33c2bd59439dbdf79eee40467
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-uuid: add 1.77.0 bottle.

--- Formula/boost-uuid.rb
+++ Formula/boost-uuid.rb
@@ -5,6 +5,11 @@ class BoostUuid < Formula
     tag:      "boost-1.77.0",
     revision: "eaa4be7b96c99ad56effc351aa44d0bef04da5a3"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-uuid-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "b56fdbb175ace6e3c1ce4d853713339eeedc9813d31eb1987fc7cba686ac31b3"
+  end
+
   def install
     prefix.install "include"
   end
