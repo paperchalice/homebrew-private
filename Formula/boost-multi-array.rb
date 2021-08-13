7766a313b37406479701bd9189bfb91883f29dde
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:01 +0000

    boost-multi-array: add 1.77.0 bottle.

--- Formula/boost-multi-array.rb
+++ Formula/boost-multi-array.rb
@@ -5,6 +5,11 @@ class BoostMultiArray < Formula
     tag:      "boost-1.77.0",
     revision: "0c5348bef71b890c4bd06eff1ee5ebda69e7b27a"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-multi-array-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "c9383fe2bcd7fa93f19e45627a76d699799461986125cd8aebef8970afd7770e"
+  end
+
   def install
     prefix.install "include"
   end
