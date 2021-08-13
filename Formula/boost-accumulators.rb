65b4f85db27aa869fb2327193db39581bdf682fa
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 05:46:37 +0000

    boost-accumulators: add 1.77.0 bottle.

--- Formula/boost-accumulators.rb
+++ Formula/boost-accumulators.rb
@@ -5,6 +5,11 @@ class BoostAccumulators < Formula
     tag:      "boost-1.77.0",
     revision: "14c13370602fe86d134a948943958cab0921ce9c"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-accumulators-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "42a3b13709ef85b159ba67e7cddbb4b8939ce2df42d551e46503a34146c6da08"
+  end
+
   def install
     prefix.install "include"
   end
