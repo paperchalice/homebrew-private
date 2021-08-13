3574f164b2c5461fea7757038dc69be2111acf27
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 12:33:15 +0000

    boost-vmd: add 1.77.0 bottle.

--- Formula/boost-vmd.rb
+++ Formula/boost-vmd.rb
@@ -5,6 +5,11 @@ class BoostVmd < Formula
     tag:      "boost-1.77.0",
     revision: "34cad2c1a574d445812c7c2432d3a5a5c843b412"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-vmd-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "50dcb2c2ed040b30110192fc8b1f818fbfb8100020874742aabc475969656917"
+  end
+
   def install
     prefix.install "include"
   end
