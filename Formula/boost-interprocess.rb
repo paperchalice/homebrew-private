542c2c2bb5c2864ee2363e8ca1dd80b5a2f8e8c9
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-13 09:31:00 +0000

    boost-interprocess: add 1.77.0 bottle.

--- Formula/boost-interprocess.rb
+++ Formula/boost-interprocess.rb
@@ -5,6 +5,11 @@ class BoostInterprocess < Formula
     tag:      "boost-1.77.0",
     revision: "7b2a37e614833f5fc7ab7d10df20aa11bc583bed"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-interprocess-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "77cc3093eb41cc2b2da0d0b257179dfd0a85c47985ff93c84da09710f0deb3e6"
+  end
+
   def install
     prefix.install "include"
   end
