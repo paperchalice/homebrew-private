52824a2395458e16bce3fc61af15b2a15df15c57
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-14 13:08:58 +0000

    boost-math: add 1.77.0 bottle.

--- Formula/boost-math.rb
+++ Formula/boost-math.rb
@@ -6,6 +6,12 @@ class BoostMath < Formula
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-math-1.77.0"
+    rebuild 1
+    sha256 cellar: :any, big_sur: "85c3621353ee1305dc8000faafb3529e965811d6578b5ccb1b1beeda81cc4662"
+  end
+
   depends_on "boost-config" => :build
 
   def install
