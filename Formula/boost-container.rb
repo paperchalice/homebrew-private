56ace9fb80b3cbf22d5bac724585bda85cbf5759
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-14 10:01:20 +0000

    boost-container: add 1.77.0 bottle.

--- Formula/boost-container.rb
+++ Formula/boost-container.rb
@@ -5,6 +5,11 @@ class BoostContainer < Formula
     tag:      "boost-1.77.0",
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-container-1.77.0"
+    sha256 cellar: :any, big_sur: "d39febc487033334404b8b95c5c47a1c60cee5d3158e520885c707143e2f690d"
+  end
+
   depends_on "boost-config" => :build
 
   def install
