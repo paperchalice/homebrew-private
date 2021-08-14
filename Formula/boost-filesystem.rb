779c115ac62efe39dea68b5abf2ce28c424fa1df
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-14 10:01:20 +0000

    boost-filesystem: add 1.77.0 bottle.

--- Formula/boost-filesystem.rb
+++ Formula/boost-filesystem.rb
@@ -5,6 +5,11 @@ class BoostFilesystem < Formula
     tag:      "boost-1.77.0",
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-filesystem-1.77.0"
+    sha256 cellar: :any, big_sur: "c7d79af3b484a4994bc44c4fcc39078419d28eb9214213697850983bba0189fb"
+  end
+
   depends_on "boost-config" => :build
 
   def install
