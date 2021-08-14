ed240341874915ddec39aa9a509aaf0738a4db7b
PaperChalice <lgamma@163.com>
2021-08-14 11:30:46 +0800

    more lib

---
+++ Formula/boost-context.rb
@@ -0,0 +1,21 @@
+class BoostContext < Formula
+  desc "Awesome library from Boost"
+  homepage "https://boost.org/libs/context/"
+  url "https://github.com/boostorg/boost.git",
+    tag:      "boost-1.77.0",
+    revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
+
+  def install
+    system "./bootstrap.sh"
+    system "./b2", "--with-context", "stage"
+
+    %w[boost_headers Boost].each { |d| rm_rf "stage/lib/cmake/#{d}-#{version}" }
+    rm "stage/lib/cmake/BoostDetectToolset-#{version}.cmake"
+    prefix.install "stage/lib"
+    prefix.install "libs/context/include"
+  end
+
+  test do
+    system "echo"
+  end
+end
---
+++ Formula/boost-contract.rb
@@ -0,0 +1,21 @@
+class BoostContract < Formula
+  desc "Awesome library from Boost"
+  homepage "https://boost.org/libs/contract/"
+  url "https://github.com/boostorg/boost.git",
+    tag:      "boost-1.77.0",
+    revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
+
+  def install
+    system "./bootstrap.sh"
+    system "./b2", "--with-contract", "stage"
+
+    %w[boost_headers Boost].each { |d| rm_rf "stage/lib/cmake/#{d}-#{version}" }
+    rm "stage/lib/cmake/BoostDetectToolset-#{version}.cmake"
+    prefix.install "stage/lib"
+    prefix.install "libs/contract/include"
+  end
+
+  test do
+    system "echo"
+  end
+end
