0fb2e3749b224c7d0922067772fc0be698743641
PaperChalice <lgamma@163.com>
2021-08-14 20:07:41 +0800

    nowide

--- Formula/boost-nowide.rb
+++ Formula/boost-nowide.rb
@@ -1,15 +1,16 @@
 class BoostNowide < Formula
-  desc "Awesome library from Boost"
+  desc "Make cross platform Unicode aware programming easier"
   homepage "https://boost.org/libs/nowide/"
   url "https://github.com/boostorg/boost.git",
     tag:      "boost-1.77.0",
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
+  license "BSL-1.0"
 
   depends_on "boost-config" => :build
 
   def install
     system "./bootstrap.sh"
-    system "./b2", "--with-nowide", "stage"
+    system "./b2", "--with-nowide", "cxxflags=-std=c++14", "stage"
 
     bc = Formula["boost-config"]
     Pathname.glob(bc.lib/"cmake/*").each { |c| rm_rf "stage/lib/cmake/#{c.basename}" }
