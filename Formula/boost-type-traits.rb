1cf7b6b8fbe3506613b3a71d2fb65f1bc6d0325a
PaperChalice <lgamma@163.com>
2021-08-15 15:33:33 +0800

    tt

--- Formula/boost-type-traits.rb
+++ Formula/boost-type-traits.rb
@@ -1,5 +1,5 @@
 class BoostTypeTraits < Formula
-  desc "Awesome library from Boost"
+  desc "Set of very specific traits classes"
   homepage "https://boost.org/libs/type_traits/"
   url "https://github.com/boostorg/type_traits.git",
     tag:      "boost-1.77.0",
@@ -10,11 +10,23 @@ class BoostTypeTraits < Formula
     sha256 cellar: :any_skip_relocation, big_sur: "1e7f7c1326c586fc970ae4efd7712a711d361485c6476eda81fdc6ea0a1e2b80"
   end
 
+  depends_on "boost-config"        => :test
+  depends_on "boost-static-assert" => :test
+
   def install
     prefix.install "include"
   end
 
   test do
-    system "echo"
+    (testpath/"test.cpp").write <<~EOS
+      #include <boost/type_traits.hpp>
+      int main() {
+        static_assert(boost::is_array<int[5]>::value == true);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++14", "test.cpp"
+    system "./a.out"
   end
 end
