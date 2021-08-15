7aeeda878caf1f1aed50ca085a2f420c8d581cfb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 12:49:23 +0000

    boost-container-hash: add 1.77.0 bottle.

--- Formula/boost-container-hash.rb
+++ Formula/boost-container-hash.rb
@@ -6,6 +6,11 @@ class BoostContainerHash < Formula
     revision: "e69c4c830e8999602a966a3405487814d7a50f3d"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-container-hash-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "8878429c71883531d2b4c9a193de61034e675a5121be2d640c58e2ec2aaf5d03"
+  end
+
   def install
     prefix.install "include"
   end
