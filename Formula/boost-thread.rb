70fae34058fb6fec852809077767bcae9144a445
PaperChalice <lgamma@163.com>
2021-08-15 10:42:01 +0800

    thread

--- Formula/boost-thread.rb
+++ Formula/boost-thread.rb
@@ -13,15 +13,22 @@ class BoostThread < Formula
 
   depends_on "boost-config" => :build
 
+  depends_on "boost-atomic"
+
   def install
     system "./bootstrap.sh"
     system "./b2", "--with-thread", "cxxflags=-std=c++14", "stage"
 
-    bc = Formula["boost-config"]
-    Pathname.glob(bc.lib/"cmake/*").each { |c| rm_rf "stage/lib/cmake/#{c.basename}" }
+    %w[config atomic].each do |d|
+      f = Formula["boost-#{d}"]
+      Pathname.glob(f.lib/"cmake/*").each { |c| rm_rf "stage/lib/cmake/#{c.basename}" }
+      Pathname.glob(f.lib/"lib*").each { |l| rm_rf "stage/lib/#{l.basename}" }
+    end
 
     prefix.install "stage/lib"
     prefix.install "libs/thread/include"
+
+    Pathname.glob(lib/shared_library("*")).each { |l| MachO::Tools.add_rpath(l, rpath) }
   end
 
   test do
