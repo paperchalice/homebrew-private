4827a95b45fae6f4ca75bd95c12b3a8d890cb04b
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 05:10:58 +0000

    boost-test: add 1.77.0 bottle.

--- Formula/boost-test.rb
+++ Formula/boost-test.rb
@@ -6,6 +6,11 @@ class BoostTest < Formula
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-test-1.77.0"
+    sha256 cellar: :any, big_sur: "4c037ecec021a2b3ab23414885e1a2dce932aae24443bec83ab12e00ca4226ab"
+  end
+
   depends_on "boost-config" => :build
 
   def install
