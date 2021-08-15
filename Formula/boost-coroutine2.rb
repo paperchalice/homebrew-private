a305925c5e010388c192f6715fd95022f8f8bf7f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 03:13:13 +0000

    boost-coroutine2: add 1.77.0 bottle.

--- Formula/boost-coroutine2.rb
+++ Formula/boost-coroutine2.rb
@@ -6,6 +6,11 @@ class BoostCoroutine2 < Formula
     revision: "90d319492826972571aa0a7e1a36a46f32d45b70"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-coroutine2-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "b40a469d9e9caa9696009a024b6d1f6176d56f0f1a00c72bfef39c0a774ce8f1"
+  end
+
   def install
     prefix.install "include"
   end
