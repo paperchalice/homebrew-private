4046c7a6d188b8f5b9773a1c2aa2afce1750d0f5
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 12:00:27 +0000

    boost-mp11: add 1.77.0 bottle.

--- Formula/boost-mp11.rb
+++ Formula/boost-mp11.rb
@@ -6,6 +6,11 @@ class BoostMp11 < Formula
     revision: "9d43d1f69660617266c9168e6e121ab2b0ea2287"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-mp11-1.77.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "7a5e2da642d045830648b672974527248aa375e707e43f9563853a23aacc2fa0"
+  end
+
   def install
     prefix.install "include"
   end
