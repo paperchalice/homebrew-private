f68bef73a78dc396f99c879586f349ad2c7f97b6
PaperChalice <lgamma@163.com>
2021-08-15 20:38:30 +0800

    any

--- Formula/boost-any.rb
+++ Formula/boost-any.rb
@@ -11,12 +11,28 @@ class BoostAny < Formula
     sha256 cellar: :any_skip_relocation, big_sur: "bcd02b374b86ce1741b6cbe12fff4aec089d53f23e2a0e692dd3129c175ee91c"
   end
 
+  depends_on "boost-align"           => :test
+  depends_on "boost-assert"          => :test
+  depends_on "boost-config"          => :test
+  depends_on "boost-core"            => :test
+  depends_on "boost-container-hash"  => :test
+  depends_on "boost-detail"          => :test
+  depends_on "boost-exception"       => :test
+  depends_on "boost-integer"         => :test
+  depends_on "boost-move"            => :test
+  depends_on "boost-preprocessor"    => :test
+  depends_on "boost-static-assert"   => :test
+  depends_on "boost-throw-exception" => :test
+  depends_on "boost-type-index"      => :test
+  depends_on "boost-type-traits"     => :test
+
   def install
     prefix.install "include"
   end
 
   test do
     (testpath/"test.cpp").write <<~EOS
+      #include <boost/any.hpp>
       int main() {
         return 0;
       }
