e4f86bd6fb76d3cee484126e0faddd218b594dfd
PaperChalice <lgamma@163.com>
2021-08-15 11:44:39 +0800

    timer

---
+++ Formula/boost-timer.rb
@@ -0,0 +1,34 @@
+class BoostTimer < Formula
+  desc "How long does my C++ code take to run"
+  homepage "https://boost.org/libs/timer/"
+  url "https://github.com/boostorg/boost.git",
+    tag:      "boost-1.77.0",
+    revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
+  license "BSL-1.0"
+
+  depends_on "boost-config" => :build
+
+  depends_on "boost-chrono"
+
+  def install
+    boost_name = name.delete_prefix("boost-").sub("-", "_")
+
+    system "./bootstrap.sh"
+    system "./b2", "--with-#{boost_name}", "cxxflags=-std=c++14", "stage"
+
+    %w[config chrono].each do |d|
+      f = Formula["boost-#{d}"]
+      Pathname.glob(f.lib/"cmake/*").each { |c| rm_rf "stage/lib/cmake/#{c.basename}" }
+      Pathname.glob(f.lib/"lib*").each { |l| rm_rf "stage/lib/#{l.basename}" }
+    end
+
+    prefix.install "stage/lib"
+    prefix.install "libs/#{boost_name}/include"
+
+    Pathname.glob(lib/shared_library("*")).each { |l| MachO::Tools.add_rpath(l, rpath) }
+  end
+
+  test do
+    system "echo"
+  end
+end
