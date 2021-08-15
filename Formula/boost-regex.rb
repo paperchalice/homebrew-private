eb0a9ac8d69a179566685876d5a1a43d8d599cb0
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 03:29:36 +0000

    boost-regex: add 1.77.0 bottle.

--- Formula/boost-regex.rb
+++ Formula/boost-regex.rb
@@ -5,6 +5,11 @@ class BoostRegex < Formula
     tag:      "boost-1.77.0",
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-regex-1.77.0"
+    sha256 cellar: :any, big_sur: "87c6fbdb09c37f0b4945ebe3c6d76bebccf80b22e61276bcef1cd5606cfa8562"
+  end
+
   depends_on "boost-config" => :build
   depends_on "pkg-config"   => :build
 
