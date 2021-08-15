a28401f4b3712207437f592e6199116b6c3943cb
PaperChalice <lgamma@163.com>
2021-08-15 11:59:42 +0800

    rm atomic
    Closes #237.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/boost-filesystem.rb
+++ Formula/boost-filesystem.rb
@@ -12,13 +12,15 @@ class BoostFilesystem < Formula
 
   depends_on "boost-config" => :build
 
+  depends_on "boost-atomic"
+
   def install
     boost_name = name.delete_prefix("boost-").sub("-", "_")
 
     system "./bootstrap.sh"
     system "./b2", "--with-#{boost_name}", "cxxflags=-std=c++14", "stage"
 
-    %w[config].each do |d|
+    %w[config atomic].each do |d|
       f = Formula["boost-#{d}"]
       Pathname.glob(f.lib/"cmake/*").each { |c| rm_rf "stage/lib/cmake/#{c.basename}" }
       Pathname.glob(f.lib/"lib*").each { |l| rm_rf "stage/lib/#{l.basename}" }
@@ -26,6 +28,8 @@ class BoostFilesystem < Formula
 
     prefix.install "stage/lib"
     prefix.install "libs/#{boost_name}/include"
+
+    Pathname.glob(lib/shared_library("*")).each { |l| MachO::Tools.add_rpath(l, rpath) }
   end
 
   test do
