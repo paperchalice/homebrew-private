85c0d5f4295e6002a78b6a0abcfc8cf80defa084
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-15 03:56:26 +0000

    boost-timer: add 1.77.0 bottle.

--- Formula/boost-timer.rb
+++ Formula/boost-timer.rb
@@ -6,6 +6,11 @@ class BoostTimer < Formula
     revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
   license "BSL-1.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/boost-timer-1.77.0"
+    sha256 cellar: :any, big_sur: "5d047170de20d128623d86e385133eb65f5b47e8b9f479bbc8ad309417caf44d"
+  end
+
   depends_on "boost-config" => :build
 
   depends_on "boost-chrono"
