e924208cef12f2863579ee7fe5cf59789122826e
PaperChalice <lgamma@163.com>
2021-08-16 17:30:56 +0800

    mlir

--- Formula/mlir.rb
+++ Formula/mlir.rb
@@ -2,18 +2,13 @@ class Mlir < Formula
   desc "Multi-Level Intermediate Representation"
   homepage "https://mlir.llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-12.0.1",
-    revision: "fed41342a82f5a3a9201819a82bf7a48313e296b"
+    tag:      "llvmorg-13.0.0-rc1",
+    revision: "d6974c010878cae1df5b27067230ee5dcbc63342"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   depends_on "cmake" => :build
 
   depends_on "llvm-core"
-
-  patch do
-    url "https://github.com/llvm/llvm-project/commit/2aa1af9b1da0d832270d9b8afcba19a4aba2c366.patch?full_index=1"
-    sha256 "9eef2025e3e4401e705426b3bf9af7a04ab0fbb4c3cc055dded6e16addec1d79"
-  end
 
   def install
     cd "mlir"
