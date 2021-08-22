36644ae175e961a4cf569fe6f231701bfb09ba58
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-08-22 07:00:47 +0000

    kwiml: add 2021-04-01 bottle.

--- Formula/kwiml.rb
+++ Formula/kwiml.rb
@@ -4,6 +4,11 @@ class Kwiml < Formula
   url "https://gitlab.kitware.com/utils/kwiml.git"
   version "2021-04-01"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/kwiml-2021-04-01"
+    sha256 cellar: :any_skip_relocation, big_sur: "6faf76a8a7abed49dfd50c0c6d2bb35d80add76d923caab2dc6c43c2148d896a"
+  end
+
   depends_on "cmake" => :build
 
   def install
