228991c73f1f8fb32b5c1cd618e729489c6fa5f1
PaperChalice <lgamma@163.com>
2021-09-02 20:33:43 +0800

    use major_minor

--- cmd/upgrade-qt.rb
+++ cmd/upgrade-qt.rb
@@ -15,7 +15,7 @@ module Homebrew
     qts = Pathname.glob(repo/"Formula/qt-*").map { |f| f.basename(".rb").to_s }
     qts.delete "qt-doc"
 
-    url_template = "https://download.qt.io/official_releases/qt/#{new_version.major}.#{new_version.minor}/#{new_version}/submodules/qt%s-everywhere-src-#{new_version}.tar.xz"
+    url_template = "https://download.qt.io/official_releases/qt/#{new_version.major_minor}/#{new_version}/submodules/qt%s-everywhere-src-#{new_version}.tar.xz"
     qts.each do |c|
       f = Formula[c]
       old_url = f.stable.url
@@ -30,7 +30,7 @@ module Homebrew
     # upgrade qt-doc
     f = Formula["qt-doc"]
     old_url = f.stable.url
-    new_url = "https://download.qt.io/official_releases/qt/#{new_version.major}.#{new_version.minor}/#{new_version}/single/qt-everywhere-src-#{new_version}.tar.xz"
+    new_url = "https://download.qt.io/official_releases/qt/#{new_version.major_minor}/#{new_version}/single/qt-everywhere-src-#{new_version}.tar.xz"
     Utils::Inreplace.inreplace f.path, old_url.to_s, new_url
     new_sha256 = URI.parse("#{new_url}.mirrorlist").open.read.match(/[0-9a-f]{64}/).to_s
     Utils::Inreplace.inreplace f.path, f.stable.checksum.hexdigest, new_sha256
