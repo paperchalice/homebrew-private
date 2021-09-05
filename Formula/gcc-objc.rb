909ac07be8d9d528338f911643e42386c2acf1f4
PaperChalice <lgamma@163.com>
2021-09-05 14:44:28 +0800

    oc
    Closes #267.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

---
+++ Formula/gcc-objc.rb
@@ -0,0 +1,109 @@
+class GccObjc < Formula
+  desc "GNU Objective C/C++ frontend"
+  homepage "https://gcc.gnu.org/"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz"
+  sha256 "d08edc536b54c372a1010ff6619dd274c0f1603aa49212ba20f7aa2cda36fa8b"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  head "https://gcc.gnu.org/git/gcc.git", branch: "master"
+
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  pour_bottle? only_if: :clt_installed
+
+  depends_on "python" => :build
+
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "paperchalice/private/gcc"
+  depends_on "zstd"
+
+  uses_from_macos "libiconv"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "binutils"
+  end
+
+  # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
+  cxxstdlib_check :skip
+
+  def version_suffix
+    if build.head?
+      "HEAD"
+    else
+      version.major.to_s
+    end
+  end
+
+  def install
+    # don't resolve symlinks
+    inreplace "libiberty/make-relative-prefix.c", "(progname, bin_prefix, prefix, 1)",
+      "(progname, bin_prefix, prefix, 0)"
+    # GCC will suffer build errors if forced to use a particular linker.
+    ENV.delete "LD"
+
+    languages = %w[c c++ objc obj-c++]
+
+    pkgversion = "Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-multilib
+      --enable-nls
+      --enable-host-shared
+      --enable-checking=release
+      --enable-libphobos
+      --enable-languages=#{languages.join ","}
+      --libexecdir=#{lib}
+      --with-gcc-major-version-only
+      --with-gmp=#{Formula["gmp"].opt_prefix}
+      --with-mpfr=#{Formula["mpfr"].opt_prefix}
+      --with-mpc=#{Formula["libmpc"].opt_prefix}
+      --with-isl=#{Formula["isl"].opt_prefix}
+      --with-python-dir=#{Language::Python.site_packages "python3"}
+      --with-zstd=#{Formula["zstd"].opt_prefix}
+      --with-pkgversion=#{pkgversion}
+      --with-bugurl=#{tap.issues_url}
+    ]
+
+    triple = "#{cpu}-apple-darwin#{OS.kernel_version.major}"
+    on_macos do
+      args << "--build=#{triple}"
+      args << "--with-system-zlib"
+
+      # Workaround for Xcode 12.5 bug on Intel
+      # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100340
+      args << "--without-build-config" if Hardware::CPU.intel? && DevelopmentTools.clang_build_version >= 1205
+
+      # System headers may not be in /usr/include
+      ENV["SDKROOT"] = MacOS.sdk_path
+      args << "--with-sysroot=#{MacOS.sdk_path}"
+      inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", opt_lib.to_s
+    end
+
+    mkdir "build" do
+      system "../configure", *args
+
+      # Use -headerpad_max_install_names in the build,
+      # otherwise updated load commands won't fit in the Mach-O header.
+      # This is needed because `gcc` avoids the superenv shim.
+      system "make", "BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"
+      system "make", "-C", "#{triple}/libobjc", "install"
+      %w[cc1obj cc1objplus].each { |t| (lib/"gcc/#{triple}/#{version_suffix}").install "gcc/#{t}" }
+
+      gcc = Formula["paperchalice/private/gcc"]
+      %w[objc-gnu].each do |l|
+        MachO::Tools.change_install_name lib/shared_library("lib#{l}"),
+          "#{opt_lib}/#{shared_library("libgcc_s", 1)}",
+          "#{gcc.lib}/#{shared_library("libgcc_s", 1)}"
+      end
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
