4d58a0c790e0d104bbd8bf9f7ef29057673eb05d
PaperChalice <lgamma@163.com>
2021-09-08 21:26:55 +0800

    rm finclude

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -136,11 +136,11 @@ class Gcc < Formula
       %w[fortran nat-style nat_rm nat_ugn dc].each do |i|
         rm_rf info/"g#{i}.info"
       end
-      rm_rf lib/"gcc"/triple/version_suffix/"finclude"
 
       %w[gcc gomp itm quadmath].each do |l|
         system "make", "-C", "#{triple}/lib#{l}", "install"
       end
+      rm_rf lib/"gcc"/triple/version_suffix/"finclude"
     end
 
     %w[gcc gcc-ar gcc-nm gcc-ranlib].each do |x|
