b9a8925a50dfc2b96a92684e93e2cc3b19731bff
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-09-25 12:47:38 +0000

    qt-serial-bus: add 6.2.0-rc2 bottle.

--- Formula/qt-serial-bus.rb
+++ Formula/qt-serial-bus.rb
@@ -6,6 +6,11 @@ class QtSerialBus < Formula
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtserialbus.git", branch: "dev"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-serial-bus-6.2.0-rc2"
+    sha256 cellar: :any, big_sur: "67cb58251c982ed9e6a7e54d79e37c23fa9e1a33ff729a796b8e11aeea55d68e"
+  end
+
   depends_on "cmake"      => [:build, :test]
   depends_on "perl"       => :build
   depends_on "pkgconf"    => :build
