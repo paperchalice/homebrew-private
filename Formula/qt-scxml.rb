83f34d57a3f946f20255b61d349b148779dbc356
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-09-30 23:56:08 +0000

    qt-scxml: update 6.2.0 bottle.

--- Formula/qt-scxml.rb
+++ Formula/qt-scxml.rb
@@ -12,8 +12,8 @@ class QtScxml < Formula
   end
 
   bottle do
-    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-scxml-6.1.3"
-    sha256 cellar: :any, big_sur: "7727e1ef85f15b3d169530b7c144034bc0e6dbd62e5a641469dde3006f0f7830"
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-scxml-6.2.0"
+    sha256 cellar: :any, big_sur: "1dce00f4d36e8a25c8d15ecfe3fcbb35926e1a947a6c7928a21bbb2cdf93037d"
   end
 
   depends_on "cmake"      => [:build, :test]
