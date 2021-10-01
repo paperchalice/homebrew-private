42941a616ac4c428fce39f861ab0440af3e41a0a
PaperChalice <lgamma@163.com>
2021-10-01 20:38:00 +0800

    we

--- Formula/qt-web-engine.rb
+++ Formula/qt-web-engine.rb
@@ -1,8 +1,8 @@
 class QtWebEngine < Formula
   desc "Qt Quick web support"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.1/6.1.1/submodules/qtwebengine-everywhere-src-6.1.1.tar.xz"
-  sha256 "246d1acdcd953819b09b1da22bd359335d145d8a3550d9e827dc1fd27b6bd3ff"
+  url "https://download.qt.io/official_releases/qt/6.2/6.2.0/submodules/qtwebengine-everywhere-src-6.2.0.tar.xz"
+  sha256 "c6e530a61bea2e7fbb50308a2b4e7fdb4f7c7b61a28797973270acffc020809d"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtwebengine.git", branch: "dev"
 
@@ -10,23 +10,14 @@ class QtWebEngine < Formula
   depends_on "ninja"      => :build
   depends_on "perl"       => :build
   depends_on "pkgconf"    => :build
+  depends_on "python"     => :build
 
-  depends_on "ffmpeg"
-  depends_on "icu4c"
-  depends_on "libevent"
-  depends_on "libvpx"
-  depends_on "little-cms2"
-  depends_on "minizip"
   depends_on "node"
-  depends_on "opus"
-  depends_on "protobuf"
   depends_on "qt-base"
   depends_on "qt-declarative"
   depends_on "qt-location"
   depends_on "qt-tools"
   depends_on "qt-web-channel"
-  depends_on "re2"
-  depends_on "snappy"
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex"  => :build
@@ -37,13 +28,22 @@ class QtWebEngine < Formula
   uses_from_macos "zlib"
 
   def install
+    %w[
+      cmake/Functions.cmake
+      src/CMakeLists.txt
+      cmake/Gn.cmake
+    ].each { |p| inreplace p, "REALPATH", "ABSOLUTE" }
+
     cmake_args = std_cmake_args(install_prefix: HOMEBREW_PREFIX) + %W[
+      -D CMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
       -D CMAKE_STAGING_PREFIX=#{prefix}
 
       -S .
+      -G Ninja
     ]
     system "cmake", *cmake_args
-    system "cmake", "--build", "."
+    system "cmake", "--build", ".", "-t", "run_core_GnDone"
+    system "false"
     system "cmake", "--install", ".", "--strip"
 
     lib.glob("*.framework") do |f|
