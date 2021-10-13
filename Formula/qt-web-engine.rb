338a549056e209057028b9f10e697c841e8a8139
PaperChalice <lgamma@163.com>
2021-10-13 13:22:42 +0800

    we

--- Formula/qt-web-engine.rb
+++ Formula/qt-web-engine.rb
@@ -6,12 +6,15 @@ class QtWebEngine < Formula
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtwebengine.git", branch: "dev"
 
+  keg_only "prepared bottle"
+
   depends_on "cmake"   => [:build, :test]
   depends_on "ninja"   => :build
   depends_on "node"    => :build
   depends_on "perl"    => :build
   depends_on "pkgconf" => :build
   depends_on "python"  => :build
+  depends_on "qt"      => :build
 
   depends_on "qt-base"
   depends_on "qt-declarative"
@@ -28,29 +31,42 @@ class QtWebEngine < Formula
   uses_from_macos "zlib"
 
   def install
-    inreplace "src/3rdparty/chromium/build/toolchain/mac/BUILD.gn",
-        'rebase_path("$clang_base_path/bin/", root_build_dir)', '""'
-    inreplace "src/3rdparty/gn/src/base/files/file_util_posix.cc",
-              "FilePath(full_path)", "FilePath(input)"
-    %w[
-      cmake/Gn.cmake
-      cmake/Functions.cmake
-      src/core/api/CMakeLists.txt
-      src/CMakeLists.txt
-      src/gn/CMakeLists.txt
-      src/process/CMakeLists.txt
-    ].each { |s| inreplace s, "REALPATH", "ABSOLUTE" }
+    qt = Formula["qt"]
 
-    cmake_args = std_cmake_args(install_prefix: HOMEBREW_PREFIX) + %W[
-      -D CMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
-      -D CMAKE_STAGING_PREFIX=#{prefix}
+    mkdir_p lib/"cmake/Qt#{version.major}"
+    mkdir_p lib/"metatypes"
+    mkdir_p share/"qt/modules"
+    mkdir_p share/"qt/qml"
 
-      -S .
-      -G Ninja
-    ]
-    system "cmake", *cmake_args
-    system "cmake", "--build", "."
-    system "cmake", "--install", ".", "--strip"
+    (lib/"cmake/Qt#{version.major}").install Dir["cmake/Find*"]
+    cp_r qt.lib.glob("QtWebEngine*"), lib
+    cp_r (qt.lib/"cmake").glob("qt#{version.major}webengine*"), lib/"metatypes"
+    cp_r (qt.lib/"metatypes").glob("Qt#{version.major}WebEngine*"), lib/"cmake"
+    cp_r (qt.pkgshare/"modules").glob("WebEngine*"), share/"qt/modules"
+    cp_r (qt.pkgshare/"qml").glob("QtWebEngine*"), share/"qt/qml"
+    # inreplace "src/3rdparty/chromium/build/toolchain/mac/BUILD.gn",
+    #     'rebase_path("$clang_base_path/bin/", root_build_dir)', '""'
+    # inreplace "src/3rdparty/gn/src/base/files/file_util_posix.cc",
+    #           "FilePath(full_path)", "FilePath(input)"
+    # %w[
+    #   cmake/Gn.cmake
+    #   cmake/Functions.cmake
+    #   src/core/api/CMakeLists.txt
+    #   src/CMakeLists.txt
+    #   src/gn/CMakeLists.txt
+    #   src/process/CMakeLists.txt
+    # ].each { |s| inreplace s, "REALPATH", "ABSOLUTE" }
+
+    # cmake_args = std_cmake_args(install_prefix: HOMEBREW_PREFIX) + %W[
+    #   -D CMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
+    #   -D CMAKE_STAGING_PREFIX=#{prefix}
+
+    #   -S .
+    #   -G Ninja
+    # ]
+    # TODO:system "cmake", *cmake_args
+    # TODO:system "cmake", "--build", "."
+    # TODO:system "cmake", "--install", ".", "--strip"
 
     lib.glob("*.framework") do |f|
       frameworks.install_symlink f
