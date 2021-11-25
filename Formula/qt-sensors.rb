96d46c9e22dbb9506d4f521b6657b3b3a410205f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2021-11-25 13:25:31 +0000

    qt-sensors: update 6.2.1 bottle.

--- Formula/qt-sensors.rb
+++ Formula/qt-sensors.rb
@@ -7,8 +7,8 @@ class QtSensors < Formula
   head "https://code.qt.io/qt/qtsensors.git", branch: "dev"
 
   bottle do
-    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-sensors-6.2.0"
-    sha256 cellar: :any, big_sur: "bcd0e38ef1455426e5f18eded27e645a5c14ff3629af8e734daf827d662ce037"
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-sensors-6.2.1"
+    sha256 cellar: :any, big_sur: "55319c616be69d176f1543ad9dc4560d8e3cd0228ee632faa4d15328e9a1312c"
   end
 
   depends_on "cmake"      => [:build, :test]
