6918286925126cd2dd60510c1e38139de1841d17
PaperChalice <lgamma@163.com>
2022-02-14 17:30:44 +0800

    add qt lsp

---
+++ Formula/qt-language-server.rb
@@ -0,0 +1,35 @@
+class QtLanguageServer < Formula
+  desc "Qt implementation of the Language Server Protocol"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.2/6.2.0/submodules/qtqtlanguageserver-everywhere-src-6.2.0.tar.xz"
+  sha256 "c6e530a61bea2e7fbb50308a2b4e7fdb4f7c7b61a28797973270acffc020809d"
+  license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
+  head "https://code.qt.io/qt/qtlanguageserver.git", branch: "dev"
+
+  depends_on "cmake"      => [:build, :test]
+  depends_on "perl"       => :build
+  depends_on "pkgconf"    => :build
+
+  depends_on "qt-base"
+
+  def install
+    cmake_args = std_cmake_args(install_prefix: HOMEBREW_PREFIX) + %W[
+      -D CMAKE_STAGING_PREFIX=#{prefix}
+
+      -S .
+    ]
+    system "cmake", *cmake_args
+    system "cmake", "--build", "."
+    system "cmake", "--install", ".", "--strip"
+
+    lib.glob("*.framework") do |f|
+      frameworks.install_symlink f
+      include.install_symlink f/"Headers" => f.stem
+      lib.install_symlink f/f.stem => shared_library("lib#{f.stem}")
+    end
+  end
+
+  test do
+    system "echo"
+  end
+end
