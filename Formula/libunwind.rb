4b43bca19a97338bd471f2663fecae3f171c7b34
PaperChalice <lgamma@163.com>
2022-03-25 15:50:16 +0800

    unwind

--- Formula/libunwind.rb
+++ Formula/libunwind.rb
@@ -2,25 +2,22 @@ class Libunwind < Formula
   desc "LLVM unwinding library"
   homepage "https://libcxx.llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-13.0.0",
-    revision: "d7b669b3a30345cfcdb2fde2af6f48aa4b94845d"
+    tag:      "llvmorg-14.0.0",
+    revision: "329fda39c507e8740978d10458451dcdb21563be"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   depends_on "cmake" => :build
 
-  depends_on "compiler-rt"
-
   def install
     args = std_cmake_args+ %w[
-      -D LIBUNWIND_USE_COMPILER_RT=ON
+      -D LLVM_ENABLE_RUNTIMES=libunwind
 
-      -S libunwind
+      -S runtimes
       -B build
     ]
 
     system "cmake", *args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build", "--strip"
+    system "cmake", "--build", "build", "--target", "install-unwind"
   end
 
   test do
