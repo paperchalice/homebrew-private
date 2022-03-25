f7aef53c3846f15d8f15fc29c791b60e3a83f9f9
PaperChalice <lgamma@163.com>
2022-03-25 19:06:31 +0800

    mlir 14.0.0
    mlir
    
    Closes #355.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/mlir.rb
+++ Formula/mlir.rb
@@ -2,8 +2,8 @@ class Mlir < Formula
   desc "Multi-Level Intermediate Representation"
   homepage "https://mlir.llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-13.0.0",
-    revision: "d7b669b3a30345cfcdb2fde2af6f48aa4b94845d"
+    tag:      "llvmorg-14.0.0",
+    revision: "329fda39c507e8740978d10458451dcdb21563be"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
@@ -15,30 +15,26 @@ class Mlir < Formula
   depends_on "cmake" => :build
 
   depends_on "llvm-core"
-  # TODO: depends_on "numpy"
-  # TODO: depends_on "pybind11"
-  # TODO: depends_on "python"
+  depends_on "numpy"
+  depends_on "pybind11"
+  depends_on "python"
 
   def install
-    cd "mlir"
-
     cmake_args = std_cmake_args+ %w[
-      -D BUILD_SHARED_LIBS=OFF
+      -D BUILD_SHARED_LIBS=ON
       -D CMAKE_CXX_STANDARD=17
 
       -D LLVM_BUILD_TOOLS=ON
+      -D LLVM_BUILD_UTILS=ON
       -D MLIR_ENABLE_BINDINGS_PYTHON=OFF
-      -D MLIR_BINDINGS_PYTHON_LOCK_VERSION=OFF
 
-      -S .
+      -S mlir
       -B build
     ]
 
     system "cmake", *cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-
-    bin.install "build/bin/mlir-tblgen"
   end
 
   test do
