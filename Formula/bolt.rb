c15d98b7a6c4300c5f506d6e4eb0a332f1d03ede
PaperChalice <lgamma@163.com>
2022-03-26 14:44:48 +0800

    bolt 14.0.0
    bolt
    
    Closes #357.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/bolt.rb
+++ Formula/bolt.rb
@@ -2,26 +2,42 @@ class Bolt < Formula
   desc "Binary Optimization and Layout Tool"
   homepage "https://llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-12.0.1",
-    revision: "fed41342a82f5a3a9201819a82bf7a48313e296b"
+    tag:      "llvmorg-14.0.0",
+    revision: "329fda39c507e8740978d10458451dcdb21563be"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
-  depends_on "cmake" => :build
+  depends_on "cmake"      => :build
+  depends_on "sphinx-doc" => :build
 
   depends_on "llvm-core"
 
   def install
-    cmake_args = std_cmake_args + %w[
+    cmake_args = std_cmake_args + %W[
       -D BUILD_SHARED_LIBS=ON
       -D CMAKE_CXX_STANDARD=17
+      -D CMAKE_INSTALL_RPATH=#{rpath};#{Formula["llvm-core"].lib}
 
-      -S bolt
+      -D LLVM_ENABLE_EH=ON
+      -D LLVM_ENABLE_FFI=ON
+      -D LLVM_ENABLE_RTTI=ON
+      -D LLVM_INSTALL_UTILS=ON
+      -D LLVM_ENABLE_Z3_SOLVER=ON
+
+      -D LLVM_BUILD_DOCS=ON
+      -D LLVM_INCLUDE_DOCS=ON
+      -D SPHINX_WARNINGS_AS_ERRORS=OFF
+      -D SPHINX_OUTPUT_HTML=OFF
+      -D SPHINX_OUTPUT_MAN=ON
+      -D LLVM_ENABLE_PROJECTS=bolt
+
+      -S llvm
       -B build
     ]
 
     system "cmake", *cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build", "--strip"
+    system "cmake", "--build", "build", "-t", "bolt", "install-merge-fdata"
+    system "cmake", "--install", "build", "--strip", "--component", "bolt"
+    lib.install Dir["build/lib/#{shared_library "libLLVMBOLT*"}"]
   end
 
   test do
