4f108dc2a5b7097bf53e0ec9fd31993c2f543cc0
PaperChalice <lgamma@163.com>
2022-03-27 17:38:32 +0800

    lldb

--- Formula/lldb.rb
+++ Formula/lldb.rb
@@ -2,8 +2,8 @@ class Lldb < Formula
   desc "Next generation, high-performance debugger"
   homepage "https://lldb.llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-13.0.0",
-    revision: "d7b669b3a30345cfcdb2fde2af6f48aa4b94845d"
+    tag:      "llvmorg-14.0.0",
+    revision: "329fda39c507e8740978d10458451dcdb21563be"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
@@ -12,8 +12,9 @@ class Lldb < Formula
     sha256 cellar: :any, big_sur: "c3336c570df3b196ad108abaa08b9b0732b24e9cb6d978e4c3b4e2c54ead1503"
   end
 
-  depends_on "cmake" => :build
-  depends_on "swig"  => :build
+  depends_on "cmake"       => :build
+  depends_on "sphinx-doc"  => :build
+  depends_on "swig"        => :build
 
   depends_on "clang"
   depends_on "llvm-core"
@@ -27,9 +28,12 @@ class Lldb < Formula
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
-  patch :DATA
-
   def install
+    %w[lldb-vscode driver].each do |t|
+      inreplace "lldb/tools/#{t}/CMakeLists.txt", '"@', "#"
+      inreplace "lldb/tools/#{t}/CMakeLists.txt", "INSTALL_RPATH", "INSTALL_RPATH \"#{rpath}\""
+    end
+
     cmake_args = std_cmake_args + %W[
       -D BUILD_SHARED_LIBS=OFF
       -D CMAKE_CXX_STANDARD=17
@@ -37,8 +41,15 @@ class Lldb < Formula
       -D Clang_DIR=#{Formula["clang"].lib}/cmake/clang
       -D LLDB_BUILD_FRAMEWORK=ON
       -D LLDB_SKIP_DSYM=ON
-      -D LLDB_FRAMEWORK_INSTALL_DIR=Frameworks
+      -D LLDB_FRAMEWORK_INSTALL_DIR=lib
       -D LLDB_USE_SYSTEM_SIX=ON
+
+      -D LLVM_BUILD_DOCS=ON
+      -D LLVM_INCLUDE_DOCS=ON
+      -D LLVM_ENABLE_SPHINX=ON
+      -D SPHINX_WARNINGS_AS_ERRORS=OFF
+      -D SPHINX_OUTPUT_HTML=OFF
+      -D SPHINX_OUTPUT_MAN=ON
 
       -S lldb
       -B build
@@ -49,7 +60,8 @@ class Lldb < Formula
     system "cmake", "--install", "build"
 
     site_package = Language::Python.site_packages("python3")
-    lldb_framework = frameworks/"LLDB.framework"
+    lldb_framework = lib/"LLDB.framework"
+    frameworks.install_symlink lib/"LLDB.framework"
     clangf = Formula["clang"]
     (prefix/site_package).install_symlink lldb_framework/"Resources/Python/lldb"
     %w[darwin-debug lldb-argdumper debugserver].each { |x| bin.install_symlink lldb_framework/"Resources"/x }
@@ -62,41 +74,3 @@ class Lldb < Formula
     system bin/"lldb", "--version"
   end
 end
-
-__END__
-diff --git a/lldb/bindings/python/CMakeLists.txt b/lldb/bindings/python/CMakeLists.txt
-index 9422ee0..f115121 100644
---- a/lldb/bindings/python/CMakeLists.txt
-+++ b/lldb/bindings/python/CMakeLists.txt
-@@ -163,7 +163,7 @@ function(finish_swig_python swig_target lldb_python_bindings_dir lldb_python_tar
- 
-   # Install the LLDB python module
-   if(LLDB_BUILD_FRAMEWORK)
--    set(LLDB_PYTHON_INSTALL_PATH ${LLDB_FRAMEWORK_INSTALL_DIR}/LLDB.framework/Resources/Python)
-+    set(LLDB_PYTHON_INSTALL_PATH ${LLDB_FRAMEWORK_INSTALL_DIR}/LLDB.framework/Versions/${LLDB_FRAMEWORK_VERSION}/Resources/Python)
-   else()
-     set(LLDB_PYTHON_INSTALL_PATH ${LLDB_PYTHON_RELATIVE_PATH})
-   endif()
-diff --git a/lldb/tools/driver/CMakeLists.txt b/lldb/tools/driver/CMakeLists.txt
-index c31863b..167b840 100644
---- a/lldb/tools/driver/CMakeLists.txt
-+++ b/lldb/tools/driver/CMakeLists.txt
-@@ -44,5 +44,6 @@ if(LLDB_BUILD_FRAMEWORK)
-       "@loader_path/../../../SharedFrameworks"
-       "@loader_path/../../System/Library/PrivateFrameworks"
-       "@loader_path/../../Library/PrivateFrameworks"
-+      "@loader_path/../${LLDB_FRAMEWORK_INSTALL_DIR}"
-   )
- endif()
-diff --git a/lldb/tools/lldb-vscode/CMakeLists.txt b/lldb/tools/lldb-vscode/CMakeLists.txt
-index 41c1f10..7dc3765 100644
---- a/lldb/tools/lldb-vscode/CMakeLists.txt
-+++ b/lldb/tools/lldb-vscode/CMakeLists.txt
-@@ -58,5 +58,6 @@ if(LLDB_BUILD_FRAMEWORK)
-       "@loader_path/../../../SharedFrameworks"
-       "@loader_path/../../System/Library/PrivateFrameworks"
-       "@loader_path/../../Library/PrivateFrameworks"
-+      "@loader_path/../${LLDB_FRAMEWORK_INSTALL_DIR}"
-   )
- endif()
-
