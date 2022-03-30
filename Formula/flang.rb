40e2acbce2e703f05ad545c648fa9da47a3a6288
PaperChalice <lgamma@163.com>
2022-03-30 15:16:48 +0800

    flang 14.0.0
    flang
    
    Closes #359.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/flang.rb
+++ Formula/flang.rb
@@ -1,9 +1,9 @@
 class Flang < Formula
   desc "Fortran front end for LLVM"
-  homepage "http://flang.llvm.org"
+  homepage "https://flang.llvm.org"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-13.0.0",
-    revision: "d7b669b3a30345cfcdb2fde2af6f48aa4b94845d"
+    tag:      "llvmorg-14.0.0",
+    revision: "329fda39c507e8740978d10458451dcdb21563be"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/llvm/llvm-project.git"
 
@@ -12,8 +12,10 @@ class Flang < Formula
     sha256 cellar: :any, big_sur: "7753fde04702b1afcb2a1f87f5e25450ebe84093423c3e33c16b969164eba037"
   end
 
-  depends_on "cmake" => :build
+  depends_on "cmake"       => :build
+  depends_on "sphinx-doc"  => :build
 
+  depends_on "clang"
   depends_on "llvm-core"
   depends_on "mlir"
 
@@ -21,18 +23,24 @@ class Flang < Formula
   uses_from_macos "zlib"
 
   def install
-    args = std_cmake_args + %W[
+    cmake_args = std_cmake_args + %W[
       -D BUILD_SHARED_LIBS=ON
       -D CMAKE_CXX_STANDARD=17
 
+      -D CLANG_DIR=#{Formula["clang"].lib}/cmake/clang
+      -D LLVM_BUILD_DOCS=OFF
+      -D LLVM_INCLUDE_DOCS=ON
+      -D LLVM_ENABLE_SPHINX=ON
       -D MLIR_TABLEGEN_EXE=#{Formula["mlir"].bin}/mlir-tblgen
-      -D FLANG_BUILD_NEW_DRIVER=OFF
-      -D FLANG_INCLUDE_TESTS=OFF
+      -D SPHINX_WARNINGS_AS_ERRORS=OFF
+      -D SPHINX_OUTPUT_HTML=OFF
+      -D SPHINX_OUTPUT_MAN=ON
+
       -S flang
       -B build
     ]
 
-    system "cmake", *args
+    system "cmake", *cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build", "--strip"
   end
