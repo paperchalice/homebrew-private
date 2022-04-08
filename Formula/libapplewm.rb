34cf1eb1b5f3a37b908a7008be448dc439fa407f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-04-08 05:06:14 +0000

    libapplewm: add 1.4.1 bottle.

--- Formula/libapplewm.rb
+++ Formula/libapplewm.rb
@@ -5,6 +5,11 @@ class Libapplewm < Formula
   sha256 "5e5c85bcd81152b7bd33083135bfe2287636e707bba25f43ea09e1422c121d65"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/libapplewm-1.4.1"
+    sha256 cellar: :any, monterey: "8afd01d8d28ead06f69881605fef8bdd83b90d0c50c09d936a90cb80a4ca5fa2"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libx11"
