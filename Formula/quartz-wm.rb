46a843bed1d09923b9f5375561a5b2f916a4162e
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-04-08 05:56:15 +0000

    quartz-wm: add 1.3.2 bottle.

--- Formula/quartz-wm.rb
+++ Formula/quartz-wm.rb
@@ -6,6 +6,11 @@ class QuartzWm < Formula
     revision: "6f0fc2cfa5b81a64a6db5317aec5850458c5065d"
   license "APSL-2.0"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/quartz-wm-1.3.2"
+    sha256 cellar: :any, monterey: "4138972b8a60dd9c420c62f6bc9a9088d5735de7cafdcd98d45f96e42b7d5733"
+  end
+
   depends_on "autoconf"    => :build
   depends_on "automake"    => :build
   depends_on "libtool"     => :build
