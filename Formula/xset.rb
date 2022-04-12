295a05441fe03cda79220a0f791fcb5f37eb2cb0
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-04-12 23:13:48 +0000

    xset: add 1.2.4 bottle.

--- Formula/xset.rb
+++ Formula/xset.rb
@@ -5,6 +5,11 @@ class Xset < Formula
   sha256 "e4fd95280df52a88e9b0abc1fee11dcf0f34fc24041b9f45a247e52df941c957"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xset-1.2.4"
+    sha256 cellar: :any, monterey: "c7743b6d3e5c98e9b8c44f6a379b44e8590c9b893937e50af41c9f413c0ec4eb"
+  end
+
   depends_on "pkgconf"   => :build
   depends_on "xorgproto" => :build
 
