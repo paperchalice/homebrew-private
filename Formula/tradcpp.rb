2680d4f2cccdd2d48f77fec25527c03ad05593cb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-04-14 14:09:07 +0000

    tradcpp: add 0.5.3 bottle.

--- Formula/tradcpp.rb
+++ Formula/tradcpp.rb
@@ -5,6 +5,11 @@ class Tradcpp < Formula
   sha256 "e17b9f42cf74b360d5691bc59fb53f37e41581c45b75fcd64bb965e5e2fe4c5e"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/tradcpp-0.5.3"
+    sha256 cellar: :any_skip_relocation, monterey: "0f36a7c793079b155824dde2acfb5545598b1ee57c23244860a6e9076bd40e17"
+  end
+
   depends_on "bmake" => :build
 
   def install
