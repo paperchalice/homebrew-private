bdf6eb4a3c629464b6153e2cbebc500c36fdb02c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-04-15 04:19:27 +0000

    xman: add 1.1.5 bottle.

--- Formula/xman.rb
+++ Formula/xman.rb
@@ -5,6 +5,11 @@ class Xman < Formula
   sha256 "4e3c2c7497e9734a6d3c8e1b6a364612892bb31e9f33076c9fdae7177ab60978"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xman-1.1.5"
+    sha256 monterey: "f785b98e4df63899d9e7fd7c63a989a68376a9ae57a2c04224db9c94714bb919"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
