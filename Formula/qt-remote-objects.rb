7bea56e297206fa12781bcdf8b7692da5f83ecb4
PaperChalice <lgamma@163.com>
2022-04-16 21:53:51 +0800

    qt-remote-objects 6.3.0
    ro

--- Formula/qt-remote-objects.rb
+++ Formula/qt-remote-objects.rb
@@ -1,8 +1,8 @@
 class QtRemoteObjects < Formula
   desc "Support for CAN and potentially other serial buses"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.2/6.2.1/submodules/qtremoteobjects-everywhere-src-6.2.1.tar.xz"
-  sha256 "76681b03bb63e1cafa38a1bfde23c194f232aaff4b010d5f58c065fdcc0b379f"
+  url "https://download.qt.io/official_releases/qt/6.3/6.3.0/submodules/qtremoteobjects-everywhere-src-6.3.0.tar.xz"
+  sha256 "75b23f4cf0aff959e7c5b5c94ca97209c2d6bbdb964e8b5bf8236cebbbe94a6c"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtremoteobjects.git", branch: "dev"
 
