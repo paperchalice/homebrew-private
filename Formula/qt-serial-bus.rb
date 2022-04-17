acf0be714fc81ca06eac28f17dd9fc0e7fb16e9e
PaperChalice <lgamma@163.com>
2022-04-17 09:18:09 +0800

    bus

--- Formula/qt-serial-bus.rb
+++ Formula/qt-serial-bus.rb
@@ -1,8 +1,8 @@
 class QtSerialBus < Formula
   desc "Support for CAN and potentially other serial buses"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.2/6.2.0/submodules/qtserialbus-everywhere-src-6.2.0.tar.xz"
-  sha256 "b8cfe2a5b49f8e06c76072021015c107fb35a678d1d28beaba9e629be18aac41"
+  url "https://download.qt.io/official_releases/qt/6.3/6.3.0/submodules/qtserialbus-everywhere-src-6.3.0.tar.xz"
+  sha256 "d2f6c988cb36e76956b8828cdc17513c0677b5523f635fc30a920701bb3627d6"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qtserialbus.git", branch: "dev"
 
