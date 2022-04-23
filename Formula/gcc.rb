6b366bd79a06f6eb9a9cf1de89b7ae7314b17987
PaperChalice <lgamma@163.com>
2022-04-23 20:05:56 +0800

    gcc 11.3.0
    gcc
    
    Closes #390.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -1,8 +1,8 @@
 class Gcc < Formula
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org/"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz"
-  sha256 "d08edc536b54c372a1010ff6619dd274c0f1603aa49212ba20f7aa2cda36fa8b"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-11.3.0/gcc-11.3.0.tar.xz"
+  sha256 "b47cf2818691f5b1e21df2bb38c795fac2cfbd640ede2d0a5e1c89e338a3ac39"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
   head "https://gcc.gnu.org/git/gcc.git", branch: "master"
 
@@ -75,11 +75,14 @@ class Gcc < Formula
     languages = %w[ada c c++ d objc obj-c++ fortran]
 
     pkgversion = "Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip
+    triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}#{OS.kernel_version.major}"
 
     args = %W[
+      --build=#{triple}
       --prefix=#{prefix}
       --disable-multilib
       --enable-checking=release
+      --enable-libphobos
       --enable-host-shared
       --enable-install-libiberty
       --enable-languages=#{languages.join(",")}
@@ -93,31 +96,16 @@ class Gcc < Formula
       --with-mpc=#{Formula["libmpc"].opt_prefix}
       --with-isl=#{Formula["isl"].opt_prefix}
       --with-python-dir=#{Language::Python.site_packages "python3"}
+      --with-system-zlib
       --with-zstd=#{Formula["zstd"].opt_prefix}
       --with-pkgversion=#{pkgversion}
       --with-bugurl=#{tap.issues_url}
+      --with-sysroot=#{MacOS.sdk_path}
     ]
-    # libphobos is part of gdc
-    args << "--enable-libphobos" if Hardware::CPU.intel?
-
-    triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}#{OS.kernel_version.major}"
-    if OS.mac?
-      args << "--build=#{triple}"
-      args << "--with-system-zlib"
-
-      # System headers may not be in /usr/include
-      ENV["SDKROOT"] = MacOS.sdk_path
-      args << "--with-sysroot=#{MacOS.sdk_path}"
-      inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", opt_lib.to_s
-    end
 
     mkdir "build" do
       system "../configure", *args
-
-      # Use -headerpad_max_install_names in the build,
-      # otherwise updated load commands won't fit in the Mach-O header.
-      # This is needed because `gcc` avoids the superenv shim.
-      system "make", "BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"
+      system "make"
 
       %w[
         cpp driver gcc-ar mkheaders
@@ -155,13 +143,13 @@ class Gcc < Formula
   test do
     (testpath/"hello-c.c").write <<~EOS
       #include <stdio.h>
-      int main()
+      int main(void)
       {
         puts("Hello, world!");
         return 0;
       }
     EOS
-    system HOMEBREW_PREFIX/"bin/gcc", "hello-c.c"
+    system bin/"gcc", "hello-c.c"
     system "./a.out"
   end
 end
