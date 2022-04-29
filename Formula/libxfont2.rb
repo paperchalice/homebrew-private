61c6fe0b3ac8f6984582725c65ec19b841c8747f
PaperChalice <lgamma@163.com>
2022-04-29 20:25:26 +0800

    modify

--- Formula/libxfont2.rb
+++ Formula/libxfont2.rb
@@ -5,11 +5,6 @@ class Libxfont2 < Formula
   sha256 "aa7c6f211cf7215c0ab4819ed893dc98034363d7b930b844bb43603c2e10b53e"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/paperchalice/homebrew-private/releases/download/libxfont2-2.0.5"
-    sha256 cellar: :any, monterey: "8b7baa768fa4ccfc22198b0e2fe819bfc2bcfec0dca071e3f7a1fc9224eb01ee"
-  end
-
   depends_on "pkgconf"     => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto"   => [:build, :test]
@@ -23,8 +18,13 @@ class Libxfont2 < Formula
 
   def install
     configure_args = std_configure_args + %w[
-      --disable-silent-rules
-      --disable-static
+      --with-bzip2
+      --enable-devel-docs=no
+      --enable-snfformat
+      --enable-unix-transport
+      --enable-tcp-transport
+      --enable-ipv6
+      --enable-local-transport
     ]
 
     system "./configure", *configure_args
@@ -43,8 +43,11 @@ class Libxfont2 < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lXfont2"
-    system "./a.out"
+
+    system ENV.cc, "test.c", "-o", "test",
+      "-I#{include}", "-I#{Formula["xorgproto"].include}",
+      "-L#{lib}", "-lXfont2"
+    system "./test"
     assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
