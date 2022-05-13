63b983e1dfa060e433590ddfa662b3a3fd076f5e
PaperChalice <lgamma@163.com>
2022-05-13 20:32:43 +0800

    short

--- Formula/xinit.rb
+++ Formula/xinit.rb
@@ -3,7 +3,7 @@ class Xinit < Formula
   homepage "https://www.x.org/"
   url "https://www.x.org/releases/individual/app/xinit-1.4.1.tar.bz2"
   sha256 "de9b8f617b68a70f6caf87da01fcf0ebd2b75690cdcba9c921d0ef54fa54abb9"
-  license "MIT"
+  license all_of: ["MIT", "APSL-2.0"]
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/xinit-1.4.1"
@@ -18,18 +18,12 @@ class Xinit < Formula
   depends_on "libx11"
   depends_on "mkfontscale"
   depends_on "xauth"
-  depends_on "xorg-server"
   depends_on "xrdb"
   depends_on "xterm"
 
   resource "font_cache" do
     url "https://raw.githubusercontent.com/XQuartz/XQuartz/XQuartz-2.8.1/base/opt/X11/bin/font_cache"
     sha256 "8f27cf55e5053686350fce6ea1078e314bd5b2d752e9da1b9051541e643e79d6"
-  end
-
-  resource "10-fontdir.sh" do
-    url "https://raw.githubusercontent.com/XQuartz/XQuartz/XQuartz-2.8.1/base/opt/X11/etc/X11/xinit/xinitrc.d/10-fontdir.sh"
-    sha256 "b44d51e582c129320942c0a6d17d18cbefb0612ca7fed5c266a7b337e4b00b55"
   end
 
   resource "98-user.sh" do
@@ -50,26 +44,37 @@ class Xinit < Formula
     system "make", "install"
 
     inreplace bin/"startx", prefix, HOMEBREW_PREFIX
-    replace bin/"startx", HOMEBREW_PREFIX/"libexec", opt_libexec
+    inreplace bin/"startx", HOMEBREW_PREFIX/"libexec", opt_libexec
     inreplace prefix/"etc/X11/xinit/xinitrc", prefix, opt_prefix
     # install scripts
     if OS.mac?
       bin.install resource("font_cache") if OS.mac?
       inreplace bin/"font_cache" do |s|
         s.gsub! "/opt/X11", HOMEBREW_PREFIX
-        s.gsub! "/usr/bin/lockfile", Formula["procmail"].opt_bin/"lockfile"
+        s.gsub! "/usr/bin/lockfile", HOMEBREW_PREFIX/"lockfile"
       end
 
-      %w[10-fontdir 98-user].each do |x|
-        (prefix/"etc/X11/xinit/xinitrc.d").install resource("#{x}.sh")
-        chmod "+x", prefix/"etc/X11/xinit/xinitrc.d/#{x}.sh"
-      end
+      (prefix/"etc/X11/xinit/xinitrc.d").install resource("98-user.sh")
+      chmod "+x", prefix/"etc/X11/xinit/xinitrc.d/98-user.sh"
 
-      inreplace prefix/"etc/X11/xinit/xinitrc.d/10-fontdir.sh" do |s|
-        s.gsub! "/opt/X11/bin", Formula["xset"].opt_bin
-        s.gsub! "/opt/X11/share", HOMEBREW_PREFIX/"share/X11"
-        s.gsub! 'fp= "$fontpath"', 'fp= "built-ins,$fontpath"'
+      font_paths = %w[misc TTF OTF Type1 75dpi 100dpi system_fonts].map do |f|
+        HOMEBREW_PREFIX/"share/fonts/X11"/f
       end
+      font_paths += %w[$HOME/.fonts $HOME/Library/Fonts /Library/Fonts]
+      font_paths.map! do |p|
+        %Q([ -e #{p}/fonts.dir ] && fontpath="$fontpath,#{p}#{",#{p}/:unscaled" if /\d+dpi/.match? p}")
+      end
+      (prefix/"etc/X11/xinit/xinitrc.d/10-fontdir.sh").write <<~SH
+        #!/bin/sh
+        if [ -x #{HOMEBREW_PREFIX}/xset ] ; then
+          fontpath="built-ins"
+          #{font_paths.join "\n  "}
+
+          #{HOMEBREW_PREFIX}/xset fp= "$fontpath"
+          unset fontpath
+        fi
+      SH
+      chmod "+x", prefix/"etc/X11/xinit/xinitrc.d/10-fontdir.sh"
     end
   end
 
@@ -78,6 +83,7 @@ class Xinit < Formula
       # generate fonts dir
       mkdir_p share/"X11/system_fonts"
       system "lndir", "/System/Library/Fonts", share/"X11/system_fonts"
+      system Formula["mkfontscale"].opt_bin/"mkfontdir", share/"X11/system_fonts"
     end
   end
 
