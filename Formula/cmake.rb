a22166200afe337a0da0f9e2c19863f803afd34e
PaperChalice <lgamma@163.com>
2022-05-26 17:47:29 +0800

    cmake 3.23.2
    cmake
    
    Closes #409.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,11 +1,10 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.22.0/cmake-3.22.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.21.4.tar.gz"
-  sha256 "998c7ba34778d2dfdb3df8a695469e24b11e2bfa21fbe41b361a3f45e1c9345e"
+  url "https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.23.2.tar.gz"
+  sha256 "f316b40053466f9a416adf981efda41b160ca859e97f6a484b447ea299ff26aa"
   license "BSD-3-Clause"
-  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/cmake-3.22.0"
@@ -33,37 +32,27 @@ class Cmake < Formula
     depends_on "openssl@1.1"
   end
 
-  resource "bootstrap-cmake" do
-    url "https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-macos10.10-universal.tar.gz"
-    mirror "http://fresh-center.net/linux/misc/cmake-3.21.3.tar.gz"
-    sha256 "20dbede1d80c1ac80be2966172f8838c3d899951ac4467372f806b386d42ad3c"
-  end
-
   def install
-    resource("bootstrap-cmake").stage buildpath/"bootstrap-cmake"
-    ENV.append_path "PATH", buildpath/"bootstrap-cmake/CMake.app/Contents/bin"
+    bootstrap_args = %W[
+      --prefix=#{prefix}
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
 
-    cmake_args = std_cmake_args(install_prefix: libexec) + %W[
-      -D BUILD_QtDialog=ON
+      --system-libs
+      --qt-gui
 
-      -D CMAKE_DATA_DIR=/share/cmake
-      -D CMAKE_DOC_DIR=/share/doc/cmake
-      -D CMAKE_MAN_DIR=/share/man
-
-      -D CMAKE_USE_SYSTEM_LIBRARIES=ON
-
-      -D LibArchive_ROOT=#{Formula["libarchive"].prefix}
-
-      -D CMake_INSTALL_EMACS_DIR=#{elisp.to_s.delete_prefix "#{prefix}/"}
+      --
+      -D CMake_INSTALL_EMACS_DIR=#{elisp.relative_path_from prefix}
       -D CMake_BUILD_LTO=ON
-
-      -S .
     ]
 
-    system "cmake", *cmake_args
-    system "cmake", "--build", "."
-    system "cmake", "--install", "."
+    system "./bootstrap", *bootstrap_args
+    system "make"
+    system "make", "install"
 
+    libexec.install prefix/"CMake.app"
     %w[bin share].each do |p|
       prefix.install libexec/"CMake.app/contents"/p
       (libexec/"CMake.app/Contents/").install_symlink prefix/p
@@ -73,20 +62,23 @@ class Cmake < Formula
     bin.install prefix/"CMake" => "cmake-gui"
     (bin/"cmake-app").write <<~SH
       #! /bin/sh
-      open #{libexec}/CMake.app
+      open #{opt_libexec}/CMake.app
     SH
   end
 
-  def post_install
-    qt_tools = Formula["qt-tools"]
-    if qt_tools.latest_version_installed?
-      share.glob("doc/qt/*.qch") { |qch| system qt_tools.bin/"Assistant", "-register", qch }
-    end
-    ln_sf libexec/"CMake.app", "/Applications/CMake.app"
-  end
-
   test do
-    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.23)
+
+      project(Template
+              VERSION      0.0.0.0
+              DESCRIPTION  "CMake template project"
+              HOMEPAGE_URL https://www.example.com/
+              LANGUAGES    C CXX)
+
+      set(CMAKE_C_STANDARD 17)
+      set(CMAKE_CXX_STANDARD 17)
+    CMAKE
     system bin/"cmake", "."
   end
 end
