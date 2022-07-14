da31b56969a764ea633f78c0b733f3d471d13b8a
PaperChalice <lgamma@163.com>
2022-07-14 13:10:41 +0800

    lld 14.0.6
    lld
    
    Closes #416.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/lld.rb
+++ Formula/lld.rb
@@ -2,8 +2,8 @@ class Lld < Formula
   desc "LLVM Linker"
   homepage "https://lld.llvm.org/"
   url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-14.0.0",
-    revision: "329fda39c507e8740978d10458451dcdb21563be"
+    tag:      "llvmorg-14.0.6",
+    revision: "f28c006a5895fc0e329fe15fead81e37457cb1d1"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
@@ -14,6 +14,11 @@ class Lld < Formula
   depends_on "cmake" => :build
 
   depends_on "llvm-core"
+
+  patch do
+    url "https://github.com/llvm/llvm-project/commit/94e0f8e.patch?full_index=1"
+    sha256 "4d1c0ad421e060f3557aadc5be94e8b1dcd80ebf62973bd5664e1f5b2de84ef4"
+  end
 
   def install
     args = std_cmake_args+ %w[
@@ -28,6 +33,7 @@ class Lld < Formula
     system "cmake", "--build", "build"
     system "cmake", "--install", "build", "--strip"
     man1.install "lld/docs/ld.lld.1"
+    system "gzip", man1/"ld.lld.1"
   end
 
   test do
