69cf182aaff0814f8367e4b5034f38e0850dace5
PaperChalice <lgamma@163.com>
2022-09-07 14:59:28 +0800

    gcc-base 12.2.0 (new formula)
    gcc base
    
    Closes #425.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

---
+++ Formula/gcc-base.rb
@@ -0,0 +1,137 @@
+class GccBase < Formula
+  desc "GNU compiler collection"
+  homepage "https://gcc.gnu.org/"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+  sha256 "e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  head "https://gcc.gnu.org/git/gcc.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(%r{href=.*?gcc[._-]v?(\d+(?:\.\d+)+)(?:/?["' >]|\.t)}i)
+  end
+
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  pour_bottle? only_if: :clt_installed
+
+  depends_on "gcc-strap" => :build
+  depends_on "python"    => :build
+
+  depends_on "gettext"
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "zstd"
+
+  uses_from_macos "gzip" => :build
+  uses_from_macos "libiconv"
+  uses_from_macos "zlib"
+
+  # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
+  cxxstdlib_check :skip
+
+  patch do
+    url "https://github.com/paperchalice/homebrew-private/raw/main/Patch/gcc.diff"
+    sha256 "691af73554281887a941ea145ed2ddb89be1e352020949c0c3d2ca3a30fc75a1"
+  end
+
+  def version_suffix
+    if build.head?
+      "HEAD"
+    else
+      version.major.to_s
+    end
+  end
+
+  def install
+    ENV.prepend_path "PATH", Formula["gcc-strap"].bin
+    ENV.delete "CC"
+    ENV.delete "CXX"
+    ENV.delete "LD"
+    ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include"
+
+    languages = %w[ada c c++ d objc obj-c++ fortran]
+
+    pkgversion = "Homebrew GCC #{pkg_version}"
+    triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}#{OS.kernel_version.major}"
+
+    args = %W[
+      --build=#{triple}
+      --prefix=#{HOMEBREW_PREFIX}
+      --disable-multilib
+      --disable-bootstrap
+      --enable-checking=release
+      --enable-libphobos
+      --enable-host-shared
+      --enable-install-libiberty
+      --enable-languages=#{languages.join(",")}
+      --enable-nls
+      --enable-shared
+      --libexecdir=#{HOMEBREW_PREFIX}/lib
+      --with-gcc-major-version-only
+      --with-gettext=#{Formula["gettext"].opt_prefix}
+      --with-gmp=#{Formula["gmp"].opt_prefix}
+      --with-mpfr=#{Formula["mpfr"].opt_prefix}
+      --with-mpc=#{Formula["libmpc"].opt_prefix}
+      --with-isl=#{Formula["isl"].opt_prefix}
+      --with-python-dir=#{Language::Python.site_packages "python3"}
+      --with-system-zlib
+      --with-zstd=#{Formula["zstd"].opt_prefix}
+      --with-pkgversion=#{pkgversion}
+      --with-bugurl=#{tap.issues_url}
+      --with-sysroot=#{MacOS.sdk_path}
+    ]
+
+    mkdir "build" do
+      destdir = "#{Pathname.pwd}/../instdir"
+      system "../configure", *args
+      system "make"
+
+      %w[
+        cpp driver gcc-ar mkheaders
+        headers plugin lto-wrapper
+        man info po
+      ].each do |t|
+        system "make", "-C", "gcc", "DESTDIR=#{destdir}", "install-#{t}"
+      end
+      system "make", "DESTDIR=#{destdir}", "install-fixincludes"
+      system "make", "DESTDIR=#{destdir}", "install-libcc1"
+      %w[atomic gcc gomp itm quadmath ssp].each do |l|
+        system "make", "DESTDIR=#{destdir}", "-C", "#{triple}/lib#{l}", "install"
+      end
+      prefix.install Dir[Pathname.pwd/"../instdir/#{HOMEBREW_PREFIX}/*"]
+      %w[gcov gcov-dump gcov-tool].each { |x| bin.install "gcc/#{x}" }
+      %w[cc1 collect2 lto1].each do |t|
+        (lib/"gcc"/triple/version_suffix).install "gcc/#{t}"
+      end
+      %w[fortran dc ++].each { |m| rm_rf man1/"g#{m}.1" }
+      %w[fortran nat-style nat_rm nat_ugn dc].each do |i|
+        rm_rf info/"g#{i}.info"
+      end
+      rm_rf lib/"gcc"/triple/version_suffix/"finclude"
+    end
+
+    %w[gcc gcc-ar gcc-nm gcc-ranlib].each do |x|
+      rm bin/x
+      bin.install_symlink bin/"#{triple}-#{x}" => x
+    end
+    rm bin/"#{triple}-gcc"
+    bin.install_symlink bin/"#{triple}-gcc-#{version.major}" => "#{triple}-gcc"
+    [man1, man7, info].each { |d| system "gzip", *Dir[d/"*"] }
+  end
+
+  test do
+    (testpath/"hello-c.c").write <<~EOS
+      #include <stdio.h>
+      int main(void)
+      {
+        puts("Hello, world!");
+        return 0;
+      }
+    EOS
+    system bin/"gcc", "hello-c.c"
+    system "./a.out"
+  end
+end
