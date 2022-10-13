7c51c16151bbadf07d2548af2e17ee4115f7c426
paperchalice <29250197+paperchalice@users.noreply.github.com>
2022-10-13 05:32:19 +0000

    qt-opcua: add 6.4.0 bottle.

--- Formula/qt-opcua.rb
+++ Formula/qt-opcua.rb
@@ -5,6 +5,11 @@ class QtOpcua < Formula
   sha256 "f68eb8a2582f1d07405974814d43284a90b65f959c7a69e4c4e1727925e223b5"
   license "GPL-3.0-only" => { with: "Qt-GPL-exception-1.0" }
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/qt-opcua-6.4.0"
+    sha256 cellar: :any, monterey: "4ddda37c27330d1dc1d5edc7f85f4671f1f7c8a623fab632a81353ab8076a885"
+  end
+
   depends_on "cmake" => [:build, :test]
 
   depends_on "open62541"
