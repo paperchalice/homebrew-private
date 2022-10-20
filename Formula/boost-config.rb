dfdb96bf8d56ca2b9ff435ba23a9413fd46e48c6
PaperChalice <lgamma@163.com>
2022-10-20 19:02:36 +0800

    boost config

--- Formula/boost-config.rb
+++ Formula/boost-config.rb
@@ -1,9 +1,8 @@
 class BoostConfig < Formula
   desc "Awesome library from Boost"
   homepage "https://boost.org/libs/config/"
-  url "https://github.com/boostorg/boost.git",
-    tag:      "boost-1.77.0",
-    revision: "9d3f9bcd7d416880d4631d7d39cceeb4e8f25da0"
+  url "https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2"
+  sha256 "1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0"
   license "BSL-1.0"
 
   bottle do
@@ -12,12 +11,23 @@ class BoostConfig < Formula
     sha256 cellar: :any_skip_relocation, big_sur: "e2c1c97d60a0109f8a7b89f0d4c2bc0b759569d65d9ab8a74b21f84af1104c8a"
   end
 
+  resource "config" do
+    url "https://github.com/boostorg/config.git",
+      tag:      "boost-1.80.0",
+      revision: "1cff5e37bb69f42ffab73438de61e34c71258f19"
+  end
+
   def install
     system "./bootstrap.sh"
     system "./b2", "--with-headers", "stage"
 
     prefix.install "stage/lib"
-    prefix.install "libs/config/include"
+    (lib.glob "**/boost_headers-config.cmake").each do |f|
+      inreplace f, buildpath/"stage", HOMEBREW_PREFIX
+    end
+    resource("config").stage do
+      prefix.install "include"
+    end
   end
 
   test do
