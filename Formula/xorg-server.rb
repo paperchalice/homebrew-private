c37c5c5216706ea87e9d2032b3ee217e586c3968
PaperChalice <example@example.com>
2022-12-02 20:14:34 +0800

    bump xorg

--- Formula/xorg-server.rb
+++ Formula/xorg-server.rb
@@ -1,16 +1,9 @@
 class XorgServer < Formula
   desc "X Window System display server"
   homepage "https://www.x.org"
-  url "https://gitlab.freedesktop.org/xorg/xserver.git",
-    tag:      "xorg-server-21.1.3",
-    revision: "85397cc2efe8fa73461cd21afe700829b2eca768"
-  license "MIT"
-
-  bottle do
-    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xorg-server-21.1.3"
-    rebuild 1
-    sha256 monterey: "ab91d95c8ead23fe8ee61f5fff244714ae6c5ceb1f3ecc04d45c4c46efa72c19"
-  end
+  url "https://www.x.org/releases/individual/xserver/xorg-server-21.1.4.tar.xz"
+  sha256 "5cc4be8ee47edb58d4a90e603a59d56b40291ad38371b0bd2471fc3cbee1c587"
+  license all_of: ["MIT", "APSL-2.0"]
 
   depends_on "font-util"   => :build
   depends_on "libxkbfile"  => :build
@@ -21,11 +14,11 @@ class XorgServer < Formula
   depends_on "xorgproto"   => :build
   depends_on "xtrans"      => :build
 
-  depends_on "libapplewm"
   depends_on "libxfixes"
   depends_on "libxfont2"
   depends_on "mesa"
   depends_on "pixman"
+  depends_on "xauth"
   depends_on "xcb-util"
   depends_on "xcb-util-image"
   depends_on "xcb-util-keysyms"
@@ -34,57 +27,77 @@ class XorgServer < Formula
   depends_on "xkbcomp"
   depends_on "xkeyboardconfig"
 
+  on_macos do
+    depends_on "libapplewm"
+  end
+
   on_linux do
+    depends_on "dbus"
     depends_on "libdrm"
     depends_on "libepoxy"
     depends_on "libxcvt"
     depends_on "libxshmfence"
+    depends_on "nettle"
     depends_on "systemd"
+
+    resource "xvfb-run" do
+      url "https://salsa.debian.org/xorg-team/xserver/xorg-server/-/raw/xorg-server-2_21.1.4-1/debian/local/xvfb-run"
+      sha256 "fd05e0f8e6207c3984b980a0f037381c9c4a6f22a6dd94fdcfa995318db2a0a4"
+    end
+
+    resource "xvfb-run.1" do
+      url "https://salsa.debian.org/xorg-team/xserver/xorg-server/-/raw/xorg-server-2_21.1.4-1/debian/local/xvfb-run.1"
+      sha256 "08f14f55e14e52e5d98713c4d8f25ae68d67e2ee188dc0247770c6ada6e27c05"
+    end
   end
 
   def install
-    font_path = %w[misc TTF OTF Type1 100dpi 75dpi].map do |p|
-      HOMEBREW_PREFIX/"share/fonts/X11"/p
-    end
-    if OS.mac?
-      font_path += %W[
-        #{HOMEBREW_PREFIX}/share/system_fonts
-        /Library/Fonts
-        /System/Library/Fonts
-      ]
-    end
-    meson_args = std_meson_args + %W[
+    # ChangeLog contains some non relocatable strings
+    rm "ChangeLog"
+    meson_args = std_meson_args.reject { |s| s["prefix"] } + %W[
       -Dxephyr=true
       -Dxf86bigfont=true
       -Dxcsecurity=true
 
-      -Dmodule_dir=#{HOMEBREW_PREFIX}/xorg/modules
-      -Ddefault_font_path=#{font_path.join ","}
-      -Dxkb_dir=#{HOMEBREW_PREFIX}/share/X11/xkb
-      -Dxkb_bin_dir=#{Formula["xkbcomp"].opt_bin}
-      -Dxkb_output_dir=#{HOMEBREW_PREFIX}/X11/xkb/compiled
-
-      -Dbundle-id-prefix=homebrew.mxcl
+      -Dbundle-id-prefix=#{Formula["xinit"].plist_name.chomp ".startx"}
       -Dbuilder_addr=#{tap.remote}
       -Dbuilder_string=#{tap.name}
     ]
     # macOS doesn't provide `authdes_cred` so `secure-rpc=false`
-    # glamor needs GLX enabled `libepoxy` on macOS
+    # glamor needs GLX with `libepoxy` on macOS
     if OS.mac?
-      meson_args += %W[
+      meson_args += %w[
         -Dsecure-rpc=false
-        -Dapple-applications-dir=#{libexec}
+        -Dapple-applications-dir=libexec
       ]
     end
 
+    # X11.app need startx etc. in the same directory
+    destdir = buildpath/"dest"
     system "meson", "build", *meson_args
     system "meson", "compile", "-C", "build"
-    system "meson", "install", "-C", "build"
+    system "meson", "install", "-C", "build", "--destdir", destdir
+    prefix.install Dir["#{destdir}#{HOMEBREW_PREFIX}/*"]
+    # follow https://github.com/XQuartz/XQuartz/blob/main/compile.sh#L955
     bin.install_symlink bin/"Xquartz" => "X" if OS.mac?
+
+    if OS.linux?
+      bin.install resource("xvfb-run")
+      man1.install resource("xvfb-run.1")
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To launch X server, it is recommend to install xinit,
+      especially on macOS, otherwise X11.app will not work:
+        brew install xinit
+      If cask xquartz is installed, this link may be helpful:
+        https://www.xquartz.org/FAQs.html#want-another-x11app-server
+    EOS
   end
 
   test do
-    mkdir_p "Library/Logs/X11"
     (testpath/"test.c").write <<~EOS
       #include <assert.h>
       #include <xcb/xcb.h>
@@ -93,17 +106,20 @@ class XorgServer < Formula
         xcb_connection_t *connection = xcb_connect(NULL, NULL);
         int has_err = xcb_connection_has_error(connection);
         assert(has_err == 0);
+        xcb_disconnect(connection);
         return 0;
       }
     EOS
     xcb = Formula["libxcb"]
     system ENV.cc, "./test.c", "-o", "test", "-I#{xcb.include}", "-L#{xcb.lib}", "-lxcb"
 
-    ENV["DISPLAY"] = ":1"
     fork do
-      exec bin/"X", ":1"
+      exec bin/"Xvfb", ":1"
     end
+    ENV["DISPLAY"] = ":1"
     sleep 10
     system "./test"
+
+    system bin/"xvfb-run", "./test" if OS.linux?
   end
 end
