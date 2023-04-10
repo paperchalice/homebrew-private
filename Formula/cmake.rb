ee7e697d8a69b5ae5e06ba66cf8b3ee6b1b29bea
PaperChalice <example@example.com>
2023-04-10 21:21:33 +0800

    cmake

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,11 +1,11 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.0.tar.gz"
-  sha256 "306463f541555da0942e6f5a0736560f70c487178b9d94a5ae7f34d0538cdd48"
+  url "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.25.2.tar.gz"
+  sha256 "c026f22cb931dd532f648f087d587f07a1843c6e66a3dfca4fb0ea21944ed33c"
   license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/cmake-3.25.0"
