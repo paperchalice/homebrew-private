371e977282d0b6d71223565ffd2e258fb0da460c
PaperChalice <example@example.com>
2023-04-27 18:39:29 +0800

    gcc13
    Closes #483.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/gcc-boot.rb
+++ Formula/gcc-boot.rb
@@ -1,8 +1,8 @@
 class GccBoot < Formula
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org/"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
-  sha256 "e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-13.1.0/gcc-13.1.0.tar.xz"
+  sha256 "61d684f0aa5e76ac6585ad8898a2427aade8979ed5e7f85492286c4dfc13ee86"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   bottle do
@@ -13,7 +13,11 @@ class GccBoot < Formula
 
   keg_only "bootstrap formula"
 
+  depends_on "doxygen"   => :build
   depends_on "gcc-strap" => :build
+  depends_on "gettext"   => :build
+  depends_on "python"    => :build
+  depends_on "texinfo"   => :build
 
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
@@ -29,26 +33,36 @@ class GccBoot < Formula
     ENV.delete "LD"
 
     triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}"
-    languages = %w[ada c c++ d objc obj-c++ fortran]
+    languages = %w[ada c c++ d objc obj-c++ fortran jit lto m2]
     sr = MacOS.sdk_path.to_str.tr "0-9", ""
 
     configure_args = %W[
       --build=#{triple}
       --prefix=#{prefix}
+      --disable-bootstrap
       --disable-multilib
       --enable-checking=release
       --enable-libphobos
       --enable-languages=#{languages.join(",")}
       --with-gcc-major-version-only
       --with-sysroot=#{sr}
+      --with-arch=x86-64
+      --with-tune=generic
     ]
 
     system "./contrib/download_prerequisites"
     mkdir "build" do
       system "../configure", *configure_args
       system "make"
-      system "make", "install"
+      system "make", "install", "-j", "1"
+      system "make", "-C", "gcc", "install-man", "install-info", "-j", "1"
+
+      # make libstdc++ documentation
+      system "make", "-C", "#{triple}/libstdc++-v3/doc", "prefix=#{prefix}", "doc-man-doxygen"
+      system "make", "-C", "#{triple}/libstdc++-v3/doc", "prefix=#{prefix}", "doc-install-man"
+      system "make", "-C", "#{triple}/libstdc++-v3/po", "prefix=#{prefix}", "install"
     end
+    [info, man1, man3, man7].each { |d| Utils::Gzip.compress(*Dir[d/"*"]) }
     MachO::Tools.add_rpath "#{lib}/gcc/#{triple}/#{version.major}/adalib/#{shared_library "libgnarl"}",
                            "@loader_path"
   end
