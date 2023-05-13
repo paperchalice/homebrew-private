dcce9b851c24f1d3d1bf93c43bba1a1350aaf90c
paperchalice <lgamma@163.com>
2023-05-13 08:15:54 +0800

    Update qt-web-engine.rb

--- Formula/qt-web-engine.rb
+++ Formula/qt-web-engine.rb
@@ -131,6 +131,7 @@ class QtWebEngine < Formula
         openh264
         woff2
       ]
+      inreplace "./brotli.gn", "/usr/bin", HOMEBREW_PREFIX
       system python, "./replace_gn_files.py", "--system-libraries", *syslibs
     end
 
