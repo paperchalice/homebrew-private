a4d4fafec9714c5ca552583bacad1ca1300645cc
PaperChalice <example@example.com>
2023-06-14 19:04:54 +0800

    gcc base

--- Formula/gcc-base.rb
+++ Formula/gcc-base.rb
@@ -1,8 +1,8 @@
 class GccBase < Formula
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org/"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
-  sha256 "e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-13.1.0/gcc-13.1.0.tar.xz"
+  sha256 "61d684f0aa5e76ac6585ad8898a2427aade8979ed5e7f85492286c4dfc13ee86"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
   head "https://gcc.gnu.org/git/gcc.git", branch: "master"
 
@@ -21,6 +21,7 @@ class GccBase < Formula
   pour_bottle? only_if: :clt_installed
 
   depends_on "gcc-strap" => :build
+  depends_on "make"      => :build
   depends_on "python"    => :build
 
   depends_on "gettext"
@@ -39,7 +40,7 @@ class GccBase < Formula
 
   patch do
     url "https://github.com/paperchalice/homebrew-private/raw/main/Patch/gcc.diff"
-    sha256 "691af73554281887a941ea145ed2ddb89be1e352020949c0c3d2ca3a30fc75a1"
+    sha256 "187c0cff0975d675adeff130ee25d73c09006fce6edd9836d511fdf82cf90230"
   end
 
   def version_suffix
@@ -52,12 +53,13 @@ class GccBase < Formula
 
   def install
     ENV.prepend_path "PATH", Formula["gcc-strap"].bin
+    ENV.prepend_path "PATH", Formula["make"].libexec/"gnubin"
     ENV.delete "CC"
     ENV.delete "CXX"
     ENV.delete "LD"
     ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include"
 
-    languages = %w[ada c c++ d objc obj-c++ fortran]
+    languages = %w[ada c c++ d objc obj-c++ fortran jit lto m2]
 
     pkgversion = "Homebrew GCC #{pkg_version}"
     triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}"
@@ -88,8 +90,11 @@ class GccBase < Formula
       --with-pkgversion=#{pkgversion}
       --with-bugurl=#{tap.issues_url}
       --with-sysroot=#{default_sysroot}
+      --with-arch=native
+      --with-tune=native
     ]
 
+    instdir = Pathname.pwd/"instdir"
     mkdir "build" do
       system "../configure", *args
       system "make"
@@ -99,13 +104,14 @@ class GccBase < Formula
         headers plugin lto-wrapper
         man info po
       ].each do |t|
-        system "make", "-C", "gcc", "prefix=#{prefix}", "install-#{t}"
+        system "make", "-C", "gcc", "DESTDIR=#{instdir}", "install-#{t}"
       end
-      system "make", "prefix=#{prefix}", "install-fixincludes"
-      system "make", "prefix=#{prefix}", "install-libcc1"
+      system "make", "DESTDIR=#{instdir}", "install-fixincludes"
+      system "make", "DESTDIR=#{instdir}", "install-libcc1"
       %w[atomic gcc gomp itm quadmath ssp].each do |l|
-        system "make", "prefix=#{prefix}", "-C", "#{triple}/lib#{l}", "install"
+        system "make", "DESTDIR=#{instdir}", "-C", "#{triple}/lib#{l}", "install"
       end
+      mv Dir["#{instdir}#{HOMEBREW_PREFIX}/*"], prefix
       %w[gcov gcov-dump gcov-tool].each { |x| bin.install "gcc/#{x}" }
       %w[cc1 collect2 lto1].each do |t|
         (lib/"gcc"/triple/version_suffix).install "gcc/#{t}"
@@ -123,7 +129,7 @@ class GccBase < Formula
     end
     rm bin/"#{triple}-gcc"
     bin.install_symlink bin/"#{triple}-gcc-#{version.major}" => "#{triple}-gcc"
-    [man1, man7, info].each { |d| system "gzip", *Dir[d/"*"] }
+    [man1, man7, info].each { |d| Utils::Gzip.compress(*Dir[d/"*"]) }
   end
 
   test do
