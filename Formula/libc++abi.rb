0c7e94db8d6651cbaa88e299a28492738912ec37
PaperChalice <example@example.com>
2023-09-23 16:17:16 +0800

    lca

--- Formula/libc++abi.rb
+++ Formula/libc++abi.rb
@@ -1,9 +1,8 @@
 class Libcxxabi < Formula
   desc "C++ Standard Library Support"
   homepage "https://libcxxabi.llvm.org/"
-  url "https://github.com/llvm/llvm-project.git",
-    tag:      "llvmorg-14.0.0",
-    revision: "329fda39c507e8740978d10458451dcdb21563be"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.1/llvm-project-17.0.1.src.tar.xz"
+  sha256 "b0e42aafc01ece2ca2b42e3526f54bebc4b1f1dc8de6e34f46a0446a13e882b9"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
