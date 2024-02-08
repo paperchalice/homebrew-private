d77aae8678d816f0099f28121adcb26ebec65c6c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-08 12:46:32 +0000

    xlsatoms: add 1.1.4 bottle.

--- Formula/xorg-app/xlsatoms.rb
+++ Formula/xorg-app/xlsatoms.rb
@@ -5,6 +5,11 @@ class Xlsatoms < Formula
   sha256 "f4bfa15f56c066d326a5d5b292646708f25b9247506840b9047cd2687dcc71b7"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xlsatoms-1.1.4"
+    sha256 cellar: :any, ventura: "9384b0ee5daa6e2348b79812c07924f36b76784e6a0274ffd219d039dfc40654"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorg-server" => :test
