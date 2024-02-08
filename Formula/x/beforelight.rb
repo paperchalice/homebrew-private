83a9e371b6661a8e9c6aa30f18f2c6c0ba55913c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-08 12:15:52 +0000

    beforelight: add 1.0.6 bottle.

--- Formula/x/beforelight.rb
+++ Formula/x/beforelight.rb
@@ -4,6 +4,11 @@ class Beforelight < Formula
   url "https://xorg.freedesktop.org/releases/individual/app/beforelight-1.0.6.tar.xz"
   sha256 "53f0bf085b7272ebbf626d1b9b0ad320d1a07bc1fe7f3ae260c86a71a857ad28"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/beforelight-1.0.6"
+    sha256 cellar: :any, ventura: "3b3e715a7fd1aa9f90ef11e2c63d9e1f66354cd2f35efa85ed7d21a235a160c3"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xorg-server" => :test
