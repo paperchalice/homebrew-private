fd80ec260952871ff9f93545fc0e34464b165ade
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-08 11:26:47 +0000

    appres: add 1.0.6 bottle.

--- Formula/x/appres.rb
+++ Formula/x/appres.rb
@@ -5,6 +5,11 @@ class Appres < Formula
   sha256 "8b2257e2a0a1ad8330323aec23f07c333075d7fe4e6efd88e0c18fba8223590b"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/appres-1.0.6"
+    sha256 cellar: :any, ventura: "90e0d2798d43aa1f0d272e70b12a749745648931e1cda218cbd2969e1d143bf8"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xorg-server" => :test
