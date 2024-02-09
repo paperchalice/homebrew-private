df8dd63fc54699f22fab84b4247ed93ecefa4251
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 08:39:03 +0000

    xcmsdb: add 1.0.6 bottle.

--- Formula/xorg-app/xcmsdb.rb
+++ Formula/xorg-app/xcmsdb.rb
@@ -5,6 +5,11 @@ class Xcmsdb < Formula
   sha256 "3c77eec4537d5942bb0966973b787bfdaf7121f3125ffa81bb1c9708d4cf4f55"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xcmsdb-1.0.6"
+    sha256 cellar: :any, ventura: "85b0517eefc3940f68d291d42db3e02515387c6826b92e97645fd6c97e17501f"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libx11"
