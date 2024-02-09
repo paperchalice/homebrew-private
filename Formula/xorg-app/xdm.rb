fa82420f5c3add294b4acc47832a3308a48d8280
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 11:25:40 +0000

    xdm: add 1.1.14 bottle.

--- Formula/xorg-app/xdm.rb
+++ Formula/xorg-app/xdm.rb
@@ -5,6 +5,11 @@ class Xdm < Formula
   sha256 "3e9bf25636797ec9e595286dd6820ecc33901439f07705eaf608ecda012c3d5f"
   license "X11-distribute-modifications-variant"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xdm-1.1.14"
+    sha256 ventura: "5ca3c23559d7773a1f479ce576ecb79e54702ad24a0f2784666d2cb0dd25e07b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
