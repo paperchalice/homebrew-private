7017de84b2fb2e76b9b4d9feae532542e7569fbe
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 02:38:20 +0000

    fonttosfnt: add 1.2.3 bottle.

--- Formula/xorg-app/fonttosfnt.rb
+++ Formula/xorg-app/fonttosfnt.rb
@@ -5,6 +5,11 @@ class Fonttosfnt < Formula
   sha256 "aa7a93f240cbd0f5cdfe6be7c1b934b4f74d23de6257883a9f1b4bf21d7d61af"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/fonttosfnt-1.2.3"
+    sha256 cellar: :any, ventura: "dcf3f60456449c16eef74e9bf09d6de042bb1ab0412342ae7049c0456a2308ca"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
