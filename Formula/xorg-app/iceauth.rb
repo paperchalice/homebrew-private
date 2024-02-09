f0f1f3a71a8ea9d30bd21f205984163fb35a5c40
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 03:30:34 +0000

    iceauth: add 1.0.9 bottle.

--- Formula/xorg-app/iceauth.rb
+++ Formula/xorg-app/iceauth.rb
@@ -4,6 +4,11 @@ class Iceauth < Formula
   url "https://xorg.freedesktop.org/releases/individual/app/iceauth-1.0.9.tar.xz"
   sha256 "2cb9dfcb545683af77fb1029bea3fc52dcc8a0666f7b8b2d7373b6ed4c408c05"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/iceauth-1.0.9"
+    sha256 cellar: :any, ventura: "21653b951c86456c348c63d9a87d939d361f9475e1fed66a4d56339bd83653b5"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
