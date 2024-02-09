d76c644a52a7d5269f903d333d023717a646d995
paperchalice <liujunchang97@outlook.com>
2024-02-09 11:33:02 +0800

    Update iceauth.rb

--- Formula/xorg-app/iceauth.rb
+++ Formula/xorg-app/iceauth.rb
@@ -3,6 +3,7 @@ class Iceauth < Formula
   homepage "https://gitlab.freedesktop.org/xorg/app/iceauth"
   url "https://xorg.freedesktop.org/releases/individual/app/iceauth-1.0.9.tar.xz"
   sha256 "2cb9dfcb545683af77fb1029bea3fc52dcc8a0666f7b8b2d7373b6ed4c408c05"
+  license "MIT-open-group"
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/iceauth-1.0.9"
