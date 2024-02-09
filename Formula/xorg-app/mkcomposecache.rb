d871f9457444447c05774ecb77d363187e0a4cee
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 04:10:33 +0000

    mkcomposecache: add 1.2.2 bottle.

--- Formula/xorg-app/mkcomposecache.rb
+++ Formula/xorg-app/mkcomposecache.rb
@@ -5,6 +5,11 @@ class Mkcomposecache < Formula
   sha256 "1a58d8504ae52964d83904c49f5bcd47c160572b4e65bc445083ddc750a70055"
   license "HPND"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/mkcomposecache-1.2.2"
+    sha256 cellar: :any, ventura: "d0c34fd9046fc6db5d2d754dd36d6245ced2f262786e191b555c2f7502adea1a"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
 
