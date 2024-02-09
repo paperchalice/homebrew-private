e7b3bdd271254507ec9379093cb325dd4102a524
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 06:55:11 +0000

    setxkbmap: add 1.3.4 bottle.

--- Formula/xorg-app/setxkbmap.rb
+++ Formula/xorg-app/setxkbmap.rb
@@ -5,6 +5,11 @@ class Setxkbmap < Formula
   sha256 "be8d8554d40e981d1b93b5ff82497c9ad2259f59f675b38f1b5e84624c07fade"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/setxkbmap-1.3.4"
+    sha256 cellar: :any, ventura: "4628b7c4d13cf61fb656e78606e74159ce7561facd99c63fe440613a56b8edd7"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
