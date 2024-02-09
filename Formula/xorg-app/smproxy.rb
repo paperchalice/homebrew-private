4eab7283d33cee36e5c74ba1592ff647b8b4ba6f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:05:00 +0000

    smproxy: add 1.0.7 bottle.

--- Formula/xorg-app/smproxy.rb
+++ Formula/xorg-app/smproxy.rb
@@ -5,6 +5,11 @@ class Smproxy < Formula
   sha256 "4aa99237cc9dab7d87ce9bc7cca4116674a853b5f08dfe3f9db1bb2b2cf9f305"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/smproxy-1.0.7"
+    sha256 cellar: :any, ventura: "ff1833d42772518b72db21e80a0cb2daefb3cffd88b8cf63af44b6fa0bd5312b"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxmu"
