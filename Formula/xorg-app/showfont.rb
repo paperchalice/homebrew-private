d563e8f3b91be8cd9f60037cbd121ad9dcc0e98a
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 06:59:52 +0000

    showfont: add 1.0.6 bottle.

--- Formula/xorg-app/showfont.rb
+++ Formula/xorg-app/showfont.rb
@@ -5,6 +5,11 @@ class Showfont < Formula
   sha256 "2b9b9f06e65e095ed76ce560b701b9fc47fa63310ee706b54c8787af061d0e56"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/showfont-1.0.6"
+    sha256 cellar: :any, ventura: "9b81334b8078139fbb69cb5d7a56001f3aa41048559fa7b9be68a4125fa46865"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libfs"
