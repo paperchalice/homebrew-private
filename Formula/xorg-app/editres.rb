ce2d1428ca79e6b30db86e77a77619bcda324a03
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 02:27:06 +0000

    editres: add 1.0.8 bottle.

--- Formula/xorg-app/editres.rb
+++ Formula/xorg-app/editres.rb
@@ -5,6 +5,11 @@ class Editres < Formula
   sha256 "83cf5dffb1883635fd9c6a8dc48ff9e560f6c6d8ce1a0e929d5f409cba18b6f9"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/editres-1.0.8"
+    sha256 cellar: :any, ventura: "e9c6d48a1a67c6fa016ddeee74017d05be23d8ef53bfd7f12dbe936f525d7f02"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xorg-server" => :test
