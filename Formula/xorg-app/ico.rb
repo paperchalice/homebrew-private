58c0d7236a7ed693c022185715de852c9528f253
paperchalice <liujunchang97@outlook.com>
2024-02-09 11:34:10 +0800

    Update ico.rb
    Closes #530.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/xorg-app/ico.rb
+++ Formula/xorg-app/ico.rb
@@ -2,7 +2,8 @@ class Ico < Formula
   desc "Animation program for testing X11"
   homepage "https://gitlab.freedesktop.org/xorg/app/ico"
   url "https://xorg.freedesktop.org/releases/individual/app/ico-1.0.6.tar.xz"
-  sha256 "2cb9dfcb545683af77fb1029bea3fc52dcc8a0666f7b8b2d7373b6ed4c408c05"
+  sha256 "38f369d431e753280fde70fa489cc94ce204f9f8eabd2f49fc7d32afa69f4405"
+  license "MIT-open-group"
 
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
