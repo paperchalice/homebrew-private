312a382f70c2eae7f1cbb3d654283d609d80d55e
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 06:48:26 +0000

    sessreg: add 1.1.3 bottle.

--- Formula/xorg-app/sessreg.rb
+++ Formula/xorg-app/sessreg.rb
@@ -5,6 +5,11 @@ class Sessreg < Formula
   sha256 "022acd5de8077dddc4f919961f79e102ecd5f3228a333681af5cd0e7344facc2"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/sessreg-1.1.3"
+    sha256 cellar: :any_skip_relocation, ventura: "5097a27a9266c09418afdb758449a40beae1dad7a53eaee47313e07e45bf5623"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
