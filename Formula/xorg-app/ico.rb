f9be2c36c0cc3adfe9033760f2894c387ea808b4
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 03:38:38 +0000

    ico: add 1.0.6 bottle.

--- Formula/xorg-app/ico.rb
+++ Formula/xorg-app/ico.rb
@@ -5,6 +5,11 @@ class Ico < Formula
   sha256 "38f369d431e753280fde70fa489cc94ce204f9f8eabd2f49fc7d32afa69f4405"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/ico-1.0.6"
+    sha256 cellar: :any, ventura: "891ef1ecf96e05ee9e585908e84d2869aa4e88d4edc8c3faaacbd26add1e4705"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
