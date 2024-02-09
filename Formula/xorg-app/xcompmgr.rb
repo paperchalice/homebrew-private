0d045586b59355fb552e7f922fef0ac00b5ae2a5
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 09:56:22 +0000

    xcompmgr: add 1.1.9 bottle.

--- Formula/xorg-app/xcompmgr.rb
+++ Formula/xorg-app/xcompmgr.rb
@@ -5,6 +5,11 @@ class Xcompmgr < Formula
   sha256 "4875b6698672d01eb3a5080bde6eac9a989d486a82226a2d5e23624f1527a6f0"
   license "MIT-CMU"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xcompmgr-1.1.9"
+    sha256 cellar: :any, ventura: "a3523504cb891be899a72e3e7a14521f1d87df506449c33c3862aa4ed8753895"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
