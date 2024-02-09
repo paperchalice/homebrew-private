5ea9e5aa5e9f138f170c7bef7ef4b06d567cd2a7
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 03:19:27 +0000

    fstobdf: add 1.0.7 bottle.

--- Formula/xorg-app/fstobdf.rb
+++ Formula/xorg-app/fstobdf.rb
@@ -5,6 +5,11 @@ class Fstobdf < Formula
   sha256 "2624cbf071ccca89c2a6dadd65004784f478b2ba1c62b8209e03909954f36b50"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/fstobdf-1.0.7"
+    sha256 cellar: :any, ventura: "aa89f1beabc87eb5155ed78cd11d8c6cbe04182f34896a0c6eb0457ae1ad1ec7"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
