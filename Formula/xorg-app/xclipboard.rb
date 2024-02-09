1ae6ce43cd83e18189f1361d88513504bcbe6459
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:34:01 +0000

    xclipboard: add 1.1.4 bottle.

--- Formula/xorg-app/xclipboard.rb
+++ Formula/xorg-app/xclipboard.rb
@@ -5,6 +5,11 @@ class Xclipboard < Formula
   sha256 "f43d4560d1464e8ff58e850212f6bdb703989b66d599fb61f351b5f9e077f253"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xclipboard-1.1.4"
+    sha256 cellar: :any, ventura: "60566480095cfaa9dc4c247056b80fbae7a7c74c216080b41e596bf4e5d8074b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
