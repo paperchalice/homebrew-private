81a139e158052f6063c306d226d5c5fdb7789aa1
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:27:48 +0000

    xcalc: add 1.1.2 bottle.

--- Formula/xorg-app/xcalc.rb
+++ Formula/xorg-app/xcalc.rb
@@ -5,6 +5,11 @@ class Xcalc < Formula
   sha256 "8578dfa1457e94289f6d6ed6146714307d8a73a1b54d2f42af1321b625fc1cd4"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xcalc-1.1.2"
+    sha256 cellar: :any, ventura: "80e5031cdd88866402e37230da1e83aeaa2174330b8e3a92498dd55cda8d9316"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
