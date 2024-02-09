5e46b6a3e4ba0c73268afb01685de6dcae8624cc
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 12:18:43 +0000

    xfontsel: add 1.1.0 bottle.

--- Formula/xorg-app/xfontsel.rb
+++ Formula/xorg-app/xfontsel.rb
@@ -5,6 +5,11 @@ class Xfontsel < Formula
   sha256 "17052c3357bbfe44b8468675ae3d099c2427ba9fcac10540aef524ae4d77d1b4"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xfontsel-1.1.0"
+    sha256 cellar: :any, ventura: "d2148538f19403e8eed52902c9346792d7a9b0d8ac0c4dfb00426c57c9d0b23e"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
