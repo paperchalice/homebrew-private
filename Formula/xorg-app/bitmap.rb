64d09a6cb58a78059e02a3cf7c7a1d0b9f27cb05
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 02:04:32 +0000

    bitmap: add 1.1.1 bottle.

--- Formula/xorg-app/bitmap.rb
+++ Formula/xorg-app/bitmap.rb
@@ -5,6 +5,11 @@ class Bitmap < Formula
   sha256 "63d42eb63fe48198b39344af49949e5e424cc62ce8d722781fdad4a4fa3426e6"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/bitmap-1.1.1"
+    sha256 cellar: :any, ventura: "2eb6e26396d519c1b3fab3579eba8d44e424e949895862e1782a7047f9733bc3"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xbitmaps" => :build
