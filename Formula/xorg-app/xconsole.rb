b77ccab061d0090bdfe5b24ed022521a3a6ce31c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 10:05:02 +0000

    xconsole: add 1.0.8 bottle.

--- Formula/xorg-app/xconsole.rb
+++ Formula/xorg-app/xconsole.rb
@@ -5,6 +5,11 @@ class Xconsole < Formula
   sha256 "7b4a6af068e40e2e6a4521d6f35c9253ec152c287d025fff9cc4c99f2586bba4"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xconsole-1.0.8"
+    sha256 cellar: :any, ventura: "33f11d7d8c04969454bec7e6228d5bd1c8a7a44d55fa0354bc0df9cfa1abff01"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
