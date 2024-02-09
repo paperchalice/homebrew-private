8461a33eb58a98678f21d3162d0228d96da7abef
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 11:58:06 +0000

    xedit: add 1.2.3 bottle.

--- Formula/xorg-app/xedit.rb
+++ Formula/xorg-app/xedit.rb
@@ -5,6 +5,11 @@ class Xedit < Formula
   sha256 "bdd33afeeca881622e55d2cf28f07b4a98f083d0a2573c0b9048f25bdd62db2f"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xedit-1.2.3"
+    sha256 ventura: "0915d312d970ab39dce028e2d626b9c169016ff635777b4ab176e5369b04d4f3"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
