060861c2dc99ebc8a72df15594a06581d00d3656
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 11:57:58 +0000

    xdriinfo: add 1.0.7 bottle.

--- Formula/xorg-app/xdriinfo.rb
+++ Formula/xorg-app/xdriinfo.rb
@@ -5,6 +5,11 @@ class Xdriinfo < Formula
   sha256 "dd838bae9d2b19ddd71fe6d30ed33abc7c85e19d223e79d35600db3fa44bf734"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xdriinfo-1.0.7"
+    sha256 cellar: :any, ventura: "1d869743172c944ba8828ab42fc192134095516837b6bf169f452d869f88c73f"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
