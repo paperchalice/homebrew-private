4586ec839f3ffbc9efbedd22ea31651bb3ad1f83
paperchalice <liujunchang97@outlook.com>
2024-02-09 11:47:12 +0800

    Update listres.rb

--- Formula/xorg-app/listres.rb
+++ Formula/xorg-app/listres.rb
@@ -2,6 +2,7 @@ class Listres < Formula
   desc "Utility to list X resources"
   homepage "https://gitlab.freedesktop.org/xorg/app/listres"
   url "https://xorg.freedesktop.org/releases/individual/app/listres-1.0.5.tar.xz"
+  sha256 "ce2a00bbe7d2eb8d75177006f343c80443a22d52570c48a43c6fe70ea074dc9d"
   license "X11"
 
   depends_on "pkgconf" => :build
