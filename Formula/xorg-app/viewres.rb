54b700b2a17f44ef374d7b51d643a7a8ff51c96c
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:13:52 +0000

    viewres: add 1.0.7 bottle.

--- Formula/xorg-app/viewres.rb
+++ Formula/xorg-app/viewres.rb
@@ -5,6 +5,11 @@ class Viewres < Formula
   sha256 "b15a62085b1a10f55ae1cf17b7ded75b72b21be240c68071685db377c4afc628"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/viewres-1.0.7"
+    sha256 cellar: :any, ventura: "b8a478135716d873aa7ee20009719afba7f4bb2ad46c2f1f0da43101dee7cf44"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
