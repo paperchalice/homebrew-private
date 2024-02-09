db13e7cc9cf046b1ab2b7a81e7a39f176a087c63
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 10:10:53 +0000

    xcursorgen: add 1.0.8 bottle.

--- Formula/xorg-app/xcursorgen.rb
+++ Formula/xorg-app/xcursorgen.rb
@@ -5,6 +5,11 @@ class Xcursorgen < Formula
   sha256 "32b33ce27b4e285e64ff375731806bb7988cc626ff10915c65f1dc4da640cc9b"
   license "HPND"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xcursorgen-1.0.8"
+    sha256 cellar: :any, ventura: "8668eeb9aae102e3731b44e602d632795e82514d222c7b5bbc1ff3fc380fa638"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
