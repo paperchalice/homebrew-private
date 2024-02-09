cbe88eaa98d0e7e1c471744dc9d5e91e967bf251
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:23:46 +0000

    xbacklight: add 1.2.3 bottle.

--- Formula/xorg-app/xbacklight.rb
+++ Formula/xorg-app/xbacklight.rb
@@ -5,6 +5,11 @@ class Xbacklight < Formula
   sha256 "3a27f324777ae99fee476cfb2f064576fb8cba4eb77f97cda37adda1c1d39ade"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xbacklight-1.2.3"
+    sha256 cellar: :any, ventura: "75ad21e7023f3c330d40389d8974efb29a89de42f353a824fa7762d2eb2d69fc"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxcb"
