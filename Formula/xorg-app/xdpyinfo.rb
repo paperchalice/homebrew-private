99c74feff3d9d5fe189f0e81a793d318275af047
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 11:37:39 +0000

    xdpyinfo: add 1.3.4 bottle.

--- Formula/xorg-app/xdpyinfo.rb
+++ Formula/xorg-app/xdpyinfo.rb
@@ -5,6 +5,11 @@ class Xdpyinfo < Formula
   sha256 "a8ada581dbd7266440d7c3794fa89edf6b99b8857fc2e8c31042684f3af4822b"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xdpyinfo-1.3.4"
+    sha256 cellar: :any, ventura: "6fd709cc2f9c40b54a41bd9253eb96eff575e8397add337d60d6d89651ae9011"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
