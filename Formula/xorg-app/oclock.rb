bb5f74ef261cb43b3ba11f27a9c306179681218f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 04:22:38 +0000

    oclock: add 1.0.5 bottle.

--- Formula/xorg-app/oclock.rb
+++ Formula/xorg-app/oclock.rb
@@ -5,6 +5,11 @@ class Oclock < Formula
   sha256 "8f09979655e889d056b7a1e50b57f38b32529453e756b53ec659070fb3c3965c"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/oclock-1.0.5"
+    sha256 cellar: :any, ventura: "1a1b8210247562e817b1018ef4039510d1fcc2c50096544aa278b8f8381cc696"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
