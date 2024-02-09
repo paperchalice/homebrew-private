1d39ce5447e8b6fdeb3933fe8628fb835b986353
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:31:03 +0000

    xbiff: add 1.0.5 bottle.

--- Formula/xorg-app/xbiff.rb
+++ Formula/xorg-app/xbiff.rb
@@ -5,6 +5,11 @@ class Xbiff < Formula
   sha256 "cffb10e2488b09695da8377f395a4fed6d33f5eb9691322ebaa969e8ead7b0c2"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xbiff-1.0.5"
+    sha256 cellar: :any, ventura: "e6f43318bf6c23ddd614af7b59cb22f21d140d073a245582252038e36d3936c6"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
