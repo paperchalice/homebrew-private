e97f321e6b45b5101ac3b240295614128f5a52bf
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 04:25:37 +0000

    rgb: add 1.1.0 bottle.

--- Formula/xorg-app/rgb.rb
+++ Formula/xorg-app/rgb.rb
@@ -5,6 +5,11 @@ class Rgb < Formula
   sha256 "fc03d7f56e5b2a617668167f8927948cce54f93097e7ccd9f056077f479ed37b"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/rgb-1.1.0"
+    sha256 cellar: :any_skip_relocation, ventura: "62986c47d9654b91c3fca906960ea744fbfc392aa4895b4215e9d1eabb069c7e"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "util-macros" => :build
   depends_on "xorgproto" => :build
