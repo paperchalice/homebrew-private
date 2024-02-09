fae24a549e5853a6f3ee178ed2767c7699163db9
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 07:08:46 +0000

    transset: add 1.0.3 bottle.

--- Formula/xorg-app/transset.rb
+++ Formula/xorg-app/transset.rb
@@ -5,6 +5,11 @@ class Transset < Formula
   sha256 "1fe38f30dddd0dd88b5f05f121e5c6632315915468cd6a49d65fc0b17aa3e6fe"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/transset-1.0.3"
+    sha256 cellar: :any, ventura: "0eb98678797689609457e1d221266b0596ede2976cc56623814edfc30888d4d2"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
