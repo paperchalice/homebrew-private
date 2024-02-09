b8159c365ccc921d805aa4c4d227d8330eb9d703
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 11:58:15 +0000

    xev: add 1.2.5 bottle.

--- Formula/xorg-app/xev.rb
+++ Formula/xorg-app/xev.rb
@@ -5,6 +5,11 @@ class Xev < Formula
   sha256 "c9461a4389714e0f33974f9e75934bdc38d836a0f059b8dc089c7cbf2ce36ec1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xev-1.2.5"
+    sha256 cellar: :any, ventura: "a2497654f9e2004f47354f6ce82ce830a650038194114d265332ec1ad36b2a02"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
