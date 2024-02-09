09f7de82b9d78d5f617db11d03b5d76564b0ac85
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 12:25:08 +0000

    xfs: add 1.2.1 bottle.

--- Formula/xorg-app/xfs.rb
+++ Formula/xorg-app/xfs.rb
@@ -5,6 +5,11 @@ class Xfs < Formula
   sha256 "3d8ac3e8add7eeb1dc199dff6c59f90a2dbe7aa4332c1e12192350f2cda1c1e6"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xfs-1.2.1"
+    sha256 ventura: "5f471e167bb86bb4d37b35a79ffe6b3d35901bc54fef93cf941d0a212104c55f"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xtrans" => :build
