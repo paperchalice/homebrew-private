b3fe145697854537a5a73704cb7894e6b697d224
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 12:30:27 +0000

    xfsinfo: add 1.0.7 bottle.

--- Formula/xorg-app/xfsinfo.rb
+++ Formula/xorg-app/xfsinfo.rb
@@ -5,6 +5,11 @@ class Xfsinfo < Formula
   sha256 "92f3ca451cba7717eed15f80de3c123aa56c82a1ee7d6e9116ba513021b98874"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xfsinfo-1.0.7"
+    sha256 cellar: :any, ventura: "56b25df0be35bcffe88a527f7942300c97550d8243063837fc4bab58b5854281"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
