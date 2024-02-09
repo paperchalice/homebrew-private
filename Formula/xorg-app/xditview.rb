cefbc65e5af9c8935d53599e0afc238334aab1f5
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-09 10:11:05 +0000

    xditview: add 1.0.7 bottle.

--- Formula/xorg-app/xditview.rb
+++ Formula/xorg-app/xditview.rb
@@ -5,6 +5,11 @@ class Xditview < Formula
   sha256 "039e2d447fa409d4bb25c4e87cf09b4d4b3eff2d3af5aee8e7e52165a3259fd7"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xditview-1.0.7"
+    sha256 cellar: :any, ventura: "83328f3ef1975450b461133b307527339588301f7a80b43b2ce8de1bf26082c8"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
