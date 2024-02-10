0350b857ffe6ac78707da8bd345d6d36972f4a85
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:54:46 +0000

    xrefresh: add 1.0.7 bottle.

--- Formula/xorg-app/xrefresh.rb
+++ Formula/xorg-app/xrefresh.rb
@@ -5,6 +5,11 @@ class Xrefresh < Formula
   sha256 "a9f1d635f2f42283d0174e94d09ab69190c227faa5ab542bfe15ed9607771b1e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xrefresh-1.0.7"
+    sha256 cellar: :any, ventura: "59d514e281002ff2bd851b483121681bf01d0858dff988a2568336dd93d5a68d"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
