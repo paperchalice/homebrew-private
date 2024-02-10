af0b0c2efe55483463d750e4eef39d8a03b4a2f9
PaperChalice <liujunchang97@outlook.com>
2024-02-10 12:53:48 +0800

    sc

---
+++ Formula/xorg-app/xscope.rb
@@ -0,0 +1,22 @@
+class Xscope < Formula
+  desc "Program to monitor X11 Server/Client conversations"
+  homepage "https://gitlab.freedesktop.org/xorg/app/xscope"
+  url "https://xorg.freedesktop.org/releases/individual/app/xscope-1.4.4.tar.xz"
+  sha256 "820d6708fce16e449a3cb8e62c2a0f49551e589d6595633deab82643e6a90c83"
+  license "MIT"
+
+  depends_on "pkgconf" => :build
+  depends_on "xorgproto" => :build
+  depends_on "xtrans" => :build
+
+  depends_on "libxres"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    system "echo"
+  end
+end
