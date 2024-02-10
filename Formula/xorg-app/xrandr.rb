ca1af6532dda5e91416a089cf9028dfa0283c0cc
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:48:29 +0000

    xrandr: add 1.5.2 bottle.

--- Formula/xorg-app/xrandr.rb
+++ Formula/xorg-app/xrandr.rb
@@ -4,6 +4,11 @@ class Xrandr < Formula
   url "https://xorg.freedesktop.org/releases/individual/app/xrandr-1.5.2.tar.xz"
   sha256 "c8bee4790d9058bacc4b6246456c58021db58a87ddda1a9d0139bf5f18f1f240"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xrandr-1.5.2"
+    sha256 cellar: :any, ventura: "3c1cd9f5fd29dbd640c97d2f59d490eed4efdb71cfdd0167c50fc1aeed3dc5b5"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
