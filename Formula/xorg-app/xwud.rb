49afd0f04b7fdcdd623d8a646e676a03e6a7f245
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 07:07:54 +0000

    xwud: add 1.0.6 bottle.

--- Formula/xorg-app/xwud.rb
+++ Formula/xorg-app/xwud.rb
@@ -5,6 +5,11 @@ class Xwud < Formula
   sha256 "64048cd15eba3cd9a3d2e3280650391259ebf6b529f2101d1a20f441038c1afe"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xwud-1.0.6"
+    sha256 cellar: :any, ventura: "d3f6d8131d3539ff53fc1590db0a79376f685014f93fad0d35253fc98bec9549"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
