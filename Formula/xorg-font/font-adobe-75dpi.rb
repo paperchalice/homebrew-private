05beded57e20768d31849e528e93abc017bc6e22
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 12:25:48 +0000

    font-adobe-75dpi: add 1.0.4 bottle.

--- Formula/xorg-font/font-adobe-75dpi.rb
+++ Formula/xorg-font/font-adobe-75dpi.rb
@@ -4,6 +4,11 @@ class FontAdobe75dpi < Formula
   url "https://xorg.freedesktop.org/releases/individual/font/font-adobe-75dpi-1.0.4.tar.xz"
   sha256 "1281a62dbeded169e495cae1a5b487e1f336f2b4d971d92911c59c103999b911"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-adobe-75dpi-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "78dedb9c256886e0f0714a536a6444be88ff427d4d7c69a94120119c311d1c57"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
