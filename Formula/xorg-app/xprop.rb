97f0b26385ddfdef7b9ba6eabf15f13003d02432
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:48:26 +0000

    xprop: add 1.2.6 bottle.

--- Formula/xorg-app/xprop.rb
+++ Formula/xorg-app/xprop.rb
@@ -5,6 +5,11 @@ class Xprop < Formula
   sha256 "580b8525b12ecc0144aa16c88b0aafa76d2e799b44c8c6c50f9ce92788b5586e"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xprop-1.2.6"
+    sha256 cellar: :any, ventura: "051de65d682be8c4e288463e3a902a3a918d2ef367a6807e9538d1e003498179"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xorg-server" => :test
