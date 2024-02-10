6b8de9d5f4a28d120abc04ee23e39d3c3c3fdb01
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 06:09:00 +0000

    xsetroot: add 1.1.3 bottle.

--- Formula/xorg-app/xsetroot.rb
+++ Formula/xorg-app/xsetroot.rb
@@ -5,6 +5,11 @@ class Xsetroot < Formula
   sha256 "6081b45a9eb4426e045d259d1e144b32417fb635e5b96aa90647365ac96638d1"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xsetroot-1.1.3"
+    sha256 cellar: :any, ventura: "f999d041458e1424e82754fed50a755bfeaf73bb141983ffd427992871a275a2"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xbitmaps" => :build
   depends_on "xorgproto" => :build
