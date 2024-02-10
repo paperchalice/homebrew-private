2fe6616d30179435053510bf80829d06d23d684d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:29:53 +0000

    xmh: add 1.0.4 bottle.

--- Formula/xorg-app/xmh.rb
+++ Formula/xorg-app/xmh.rb
@@ -5,6 +5,11 @@ class Xmh < Formula
   sha256  "e82c425a4c4156eee2e344d2e952f3fd816e03973005cd656ec1e9acf6f329db"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xmh-1.0.4"
+    sha256 cellar: :any, ventura: "b97bee52513616234d6695d32eae8ea6232cc3bf86f57f789083c4200d45570a"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xbitmaps" => :build
   depends_on "xorgproto" => :build
