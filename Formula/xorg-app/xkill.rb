7f856ab10c60abeace6987ba5159ad384f7f2ba2
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:05:38 +0000

    xkill: add 1.0.6 bottle.

--- Formula/xorg-app/xkill.rb
+++ Formula/xorg-app/xkill.rb
@@ -5,6 +5,11 @@ class Xkill < Formula
   sha256 "e5a8aa78c475677b11504646da8d93dacc30744258076a2ca418a24438aeb907"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xkill-1.0.6"
+    sha256 cellar: :any, ventura: "60cc69b07e7912cb420f2833a34ef8c2635977983f3d7582b892d6c14fc0d1f8"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
