05808aef830054436a63c3e587d0b8179fafe1f8
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:13:11 +0000

    xlsfonts: add 1.0.7 bottle.

--- Formula/xorg-app/xlsfonts.rb
+++ Formula/xorg-app/xlsfonts.rb
@@ -5,6 +5,11 @@ class Xlsfonts < Formula
   sha256 "7b726945a967b44c35cddee5edd74802907a239ce2e2e515730b8a32c8e50465"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xlsfonts-1.0.7"
+    sha256 cellar: :any, ventura: "da6657b231562bc990171a90f811b55dcc3d066e03f08f47d050e2d61a15ad66"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
