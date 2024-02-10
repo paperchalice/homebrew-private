1dcd2fcc9f53f4c94702793ac5762f0fddad01bb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 03:45:04 +0000

    xgc: add 1.0.6 bottle.

--- Formula/xorg-app/xgc.rb
+++ Formula/xorg-app/xgc.rb
@@ -5,6 +5,11 @@ class Xgc < Formula
   sha256 "7b87bbdbce4ec858738a3b81b8a3943aff01036001e2a1d00bb5384392a8b240"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xgc-1.0.6"
+    sha256 cellar: :any, ventura: "be961217423dbe0cada086a9d11a9a807b514cc9153ca213dbe5fe96c6af6916"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
