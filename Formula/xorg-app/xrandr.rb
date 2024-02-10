0230ba2db6807de2465c258c07cfc8be6d01eec0
PaperChalice <liujunchang97@outlook.com>
2024-02-10 14:07:00 +0800

    mit

--- Formula/xorg-app/xrandr.rb
+++ Formula/xorg-app/xrandr.rb
@@ -3,6 +3,7 @@ class Xrandr < Formula
   homepage "https://gitlab.freedesktop.org/xorg/app/xrandr"
   url "https://xorg.freedesktop.org/releases/individual/app/xrandr-1.5.2.tar.xz"
   sha256 "c8bee4790d9058bacc4b6246456c58021db58a87ddda1a9d0139bf5f18f1f240"
+  license "MIT"
 
   bottle do
     root_url "https://github.com/paperchalice/homebrew-private/releases/download/xrandr-1.5.2"
