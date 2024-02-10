64a2379b6fd9721ada9db7151a18a3e13757d11b
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 03:59:20 +0000

    xkbutils: add 1.0.6 bottle.

--- Formula/xorg-app/xkbutils.rb
+++ Formula/xorg-app/xkbutils.rb
@@ -5,6 +5,11 @@ class Xkbutils < Formula
   sha256 "31a2bbee1e09ccba01de92897b8f540b545de812f318d31de07bd3a5a75ee25e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xkbutils-1.0.6"
+    sha256 cellar: :any, ventura: "73d09288e6a63ba670e7e27e6484ec701064154ecc76792e940726d47b31cd6e"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
