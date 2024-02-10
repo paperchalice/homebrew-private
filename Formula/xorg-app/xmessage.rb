9850fbdd3f90bffc6801815b27fee2e06b1b381b
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:27:01 +0000

    xmessage: add 1.0.6 bottle.

--- Formula/xorg-app/xmessage.rb
+++ Formula/xorg-app/xmessage.rb
@@ -5,6 +5,11 @@ class Xmessage < Formula
   sha256 "d2eac545f137156b960877e052fcc8e29795ed735c02f7690fd7b439e6846a12"
   license "X11"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xmessage-1.0.6"
+    sha256 cellar: :any, ventura: "a1d40efb195d8160ccd63070f2a594eb2fd5b0c89b5597732af6daac8335c5c5"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxaw"
