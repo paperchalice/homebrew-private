fd75f4b889f5acad8fde5ef6f900a155d0da4ec8
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:14:02 +0000

    xlsclients: add 1.1.4 bottle.

--- Formula/xorg-app/xlsclients.rb
+++ Formula/xorg-app/xlsclients.rb
@@ -5,6 +5,11 @@ class Xlsclients < Formula
   sha256 "0b46e8289413c3e7c437a95ecd6494f99d27406d3a0b724ef995a98cbd6c33e8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xlsclients-1.1.4"
+    sha256 cellar: :any, ventura: "f08d224dc228a7c8334f7cd57921ece71dabfa76609ab8dae32780f6d394c7fb"
+  end
+
   depends_on "pkgconf" => :build
 
   depends_on "libxcb"
