eea369e75e75bfb88d9248e9de408a2dbde9e44a
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 06:22:41 +0000

    xwd: add 1.0.9 bottle.

--- Formula/xorg-app/xwd.rb
+++ Formula/xorg-app/xwd.rb
@@ -5,6 +5,11 @@ class Xwd < Formula
   sha256 "dc121b84947eb4a3d1131bff1e9844cfa2124d95b47b35f9932340fa931fbd3f"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xwd-1.0.9"
+    sha256 cellar: :any, ventura: "889725013f8284afc556556a5dcf14363b3850cee4c86b8d3ad24f2c0a698cf4"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
