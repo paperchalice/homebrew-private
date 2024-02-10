ab5cab8f6caea1e2e0d4dbf30c016216acfffb24
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 06:10:38 +0000

    xsm: add 1.0.5 bottle.

--- Formula/xorg-app/xsm.rb
+++ Formula/xorg-app/xsm.rb
@@ -5,6 +5,11 @@ class Xsm < Formula
   sha256 "9c30fdaa3fc132e4ff201cfc478669056e6e15502e77df88df11fb94e4e6fb2d"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xsm-1.0.5"
+    sha256 ventura: "e5003eab0bd2affe34539a8dc29b8418f62b1bef19e6bc7a0e56c7405a77dfcc"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
