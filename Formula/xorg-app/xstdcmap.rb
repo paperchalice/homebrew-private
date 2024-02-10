819b2129e3d39f92ed4447dd8c254b0d30775bae
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 06:14:59 +0000

    xstdcmap: add 1.0.5 bottle.

--- Formula/xorg-app/xstdcmap.rb
+++ Formula/xorg-app/xstdcmap.rb
@@ -5,6 +5,11 @@ class Xstdcmap < Formula
   sha256 "365847e379398499ec9ad9a299cc47a0d6e7feba9546dfd4e5b422204b5ac180"
   license "MIT-open-group"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xstdcmap-1.0.5"
+    sha256 cellar: :any, ventura: "9aaf43a9b6216ecb2a5932f2dbc4756befd8c1c380d19f5751a137dcd64e3b4f"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
