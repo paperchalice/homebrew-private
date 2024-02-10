19fc5cba7e49c1cb9b4c788c03661a21d12fb2c3
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:33:17 +0000

    xmore: add 1.0.4 bottle.

--- Formula/xorg-app/xmore.rb
+++ Formula/xorg-app/xmore.rb
@@ -5,6 +5,11 @@ class Xmore < Formula
   sha256 "7eb560dbc1de4e43c64fe491ad73907a29d734cca82a9ad82c7d3feb9cdb0a9a"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xmore-1.0.4"
+    sha256 cellar: :any, ventura: "d7d881efa22fc738ff0d35ce3336477a8023bcddeb2499fa50d9a1698649b120"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xorg-server" => :test
