5833407659bbb334ea2017ba331516c90c7c2d02
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 06:17:33 +0000

    xvidtune: add 1.0.4 bottle.

--- Formula/xorg-app/xvidtune.rb
+++ Formula/xorg-app/xvidtune.rb
@@ -5,6 +5,11 @@ class Xvidtune < Formula
   sha256 "0d4eecd54e440cc11f1bdaaa23180fcf890f003444343f533f639086b05b2cc5"
   license "X11-distribute-modifications-variant"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xvidtune-1.0.4"
+    sha256 cellar: :any, ventura: "28c8fa6941524280b7d7a0a390628028befc41924e32f85436e0608c287a0a51"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
