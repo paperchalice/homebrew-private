9fdac0df42f163b6adadc1f76f2df5639d6bd712
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 03:41:19 +0000

    xgamma: add 1.0.7 bottle.

--- Formula/xorg-app/xgamma.rb
+++ Formula/xorg-app/xgamma.rb
@@ -5,6 +5,11 @@ class Xgamma < Formula
   sha256 "1c79dae85a8953a15f4fe5c2895a033307b43b8613456c87ec47b374b113bc8f"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xgamma-1.0.7"
+    sha256 cellar: :any, ventura: "5146c59e8d0ab8988894c580e5b440f73e22d7ae77a722791efe759b9ee5e937"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
