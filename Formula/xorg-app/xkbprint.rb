4abcfd15df1f4db724d45f46a82a75eedcb9f6ce
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 04:02:50 +0000

    xkbprint: add 1.0.6 bottle.

--- Formula/xorg-app/xkbprint.rb
+++ Formula/xorg-app/xkbprint.rb
@@ -5,6 +5,11 @@ class Xkbprint < Formula
   sha256 "99cc9404f7b90289ae04944c0d98a208cc8b158492ad6481386e31d4d09aa7b0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xkbprint-1.0.6"
+    sha256 cellar: :any, ventura: "be4c0726a574f8bf68882ea4f1f66b05b776d5d18f61094e10ddd1736400b493"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
