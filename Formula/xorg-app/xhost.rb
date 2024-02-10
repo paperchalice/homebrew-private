4f904cc7a53e522035604d9a46e5b7b455f31592
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-10 03:55:18 +0000

    xhost: add 1.0.9 bottle.

--- Formula/xorg-app/xhost.rb
+++ Formula/xorg-app/xhost.rb
@@ -5,6 +5,11 @@ class Xhost < Formula
   sha256 "ea86b531462035b19a2e5e01ef3d9a35cca7d984086645e2fc844d8f0e346645"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xhost-1.0.9"
+    sha256 cellar: :any, ventura: "29e8737d2c15f70fa5bfb3b1a307d7ea08bee8a24f6713d3e8c943f62947e507"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
 
