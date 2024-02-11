c770e29ba374aa327200e7a1a51a9bb9ed4b2948
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:47 +0000

    font-mutt-misc: add 1.0.4 bottle.

--- Formula/xorg-font/font-mutt-misc.rb
+++ Formula/xorg-font/font-mutt-misc.rb
@@ -5,6 +5,11 @@ class FontMuttMisc < Formula
   sha256 "b12359f4e12c23bcfcb448b918297e975fa91bef5293d88d3c25343cc768bb24"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-mutt-misc-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "f8d082aeb6f1186a54c37830cde687f90b67c079a0d6bebb0f503fc843b25d51"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
