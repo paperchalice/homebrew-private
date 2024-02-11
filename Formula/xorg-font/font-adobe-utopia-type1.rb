72637da0d4d2f2e682cd85979406da1f1df5f8f7
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:46 +0000

    font-adobe-utopia-type1: add 1.0.5 bottle.

--- Formula/xorg-font/font-adobe-utopia-type1.rb
+++ Formula/xorg-font/font-adobe-utopia-type1.rb
@@ -5,6 +5,11 @@ class FontAdobeUtopiaType1 < Formula
   sha256 "4cb280bc47693b07c5e00fd0e5ad5721aabebc0548c3f06774e5cc3cbcf75697"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-adobe-utopia-type1-1.0.5"
+    sha256 cellar: :any_skip_relocation, ventura: "e3fec65843520238abd854d867140d664e1ea1905dde5d0cf462d2728b25f314"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
