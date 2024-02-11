4211f327f5ae8bbd633c1e84d3c6193d8d717013
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:47 +0000

    font-misc-cyrillic: add 1.0.4 bottle.

--- Formula/xorg-font/font-misc-cyrillic.rb
+++ Formula/xorg-font/font-misc-cyrillic.rb
@@ -5,6 +5,11 @@ class FontMiscCyrillic < Formula
   sha256 "76021a7f53064001914a57fd08efae57f76b68f0a24dca8ab1b245474ee8e993"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-misc-cyrillic-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "961cf4cb1e606c686d1849f8ae7434d2d1e1ae8303a6decc1cfc6db09a09a947"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
