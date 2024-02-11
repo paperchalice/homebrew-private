ab12a13333020cce8871284a7354fbed708b79a1
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:48 +0000

    font-winitzki-cyrillic: add 1.0.4 bottle.

--- Formula/xorg-font/font-winitzki-cyrillic.rb
+++ Formula/xorg-font/font-winitzki-cyrillic.rb
@@ -5,6 +5,11 @@ class FontWinitzkiCyrillic < Formula
   sha256 "3b6d82122dc14776e3afcd877833a7834e1f900c53fc1c7bb2d67c781cfa97a8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-winitzki-cyrillic-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "699f0d2da8bbcfbb2490738c0ddceabd77810c324c6d2db90cc857afc9b1c60d"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
