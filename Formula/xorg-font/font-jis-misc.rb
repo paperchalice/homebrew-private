549ef4e457fba905210a7fc96f2c31a5c17c3895
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:47 +0000

    font-jis-misc: add 1.0.4 bottle.

--- Formula/xorg-font/font-jis-misc.rb
+++ Formula/xorg-font/font-jis-misc.rb
@@ -5,6 +5,11 @@ class FontJisMisc < Formula
   sha256 "78d1eff6c471f7aa6802a26d62cccf51d8e5185586406d9b6e1ee691b0bffad0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-jis-misc-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "5ae66f5656f47d1bcabc7cc07cfed626dbccaf7f4731cac473669985aac86590"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
