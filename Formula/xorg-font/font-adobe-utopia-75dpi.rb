fc6b8d5ce11b08a81893828d847d7ca9fa5fb465
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:46 +0000

    font-adobe-utopia-75dpi: add 1.0.5 bottle.

--- Formula/xorg-font/font-adobe-utopia-75dpi.rb
+++ Formula/xorg-font/font-adobe-utopia-75dpi.rb
@@ -5,6 +5,11 @@ class FontAdobeUtopia75dpi < Formula
   sha256 "a726245932d0724fa0c538c992811d63d597e5f53928f4048e9caf5623797760"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-adobe-utopia-75dpi-1.0.5"
+    sha256 cellar: :any_skip_relocation, ventura: "74555e9ad7ea7bcec885e7d05e0a1d686647a9f39237f0e77385091e1891d863"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
