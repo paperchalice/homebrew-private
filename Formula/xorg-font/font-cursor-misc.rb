16a934b747348e7e2b601616def22a3b1c895cdd
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:47 +0000

    font-cursor-misc: add 1.0.4 bottle.

--- Formula/xorg-font/font-cursor-misc.rb
+++ Formula/xorg-font/font-cursor-misc.rb
@@ -5,6 +5,11 @@ class FontCursorMisc < Formula
   sha256 "25d9c9595013cb8ca08420509993a6434c917e53ca1fec3f63acd45a19d4f982"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-cursor-misc-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "f3e47f430bb8f9f9ab8267a22516de9d8d723d9edbd2946ed0a584093730e8ea"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
