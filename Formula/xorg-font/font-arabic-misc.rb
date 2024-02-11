1a54fdd2aa9a8f7ac14173855e0ed141e8c1a420
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-11 12:34:46 +0000

    font-arabic-misc: add 1.0.4 bottle.

--- Formula/xorg-font/font-arabic-misc.rb
+++ Formula/xorg-font/font-arabic-misc.rb
@@ -5,6 +5,11 @@ class FontArabicMisc < Formula
   sha256 "46ffe61b52c78a1d2dca70ff20a9f2d84d69744639cab9a085c7a7ee17663467"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-arabic-misc-1.0.4"
+    sha256 cellar: :any_skip_relocation, ventura: "cec4bcbaad67efb0d756158f971ed29e0003844c7e6a68b9b585b4fd0bd01c5b"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
