aabab922c2de06479c0be4b4d74c98aad1a6a704
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-12 01:24:56 +0000

    font-schumacher-misc: add 1.1.3 bottle.

--- Formula/xorg-font/font-schumacher-misc.rb
+++ Formula/xorg-font/font-schumacher-misc.rb
@@ -5,6 +5,11 @@ class FontSchumacherMisc < Formula
   sha256 "8b849f0cdb1e55a34cc3dd8b0fb37443fabbc224d5ba44085569581244a68070"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-schumacher-misc-1.1.3"
+    sha256 cellar: :any_skip_relocation, ventura: "6ca9e9c3acb3c663af583df290ffdf8e2b89cff90f9a6cc644115251b4885bc2"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
