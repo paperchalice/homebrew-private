2d5f571cb11e1a6d9975af0af6ace7d0e8201eeb
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-12 01:56:33 +0000

    xscope: add 1.4.4 bottle.

--- Formula/xorg-app/xscope.rb
+++ Formula/xorg-app/xscope.rb
@@ -5,6 +5,11 @@ class Xscope < Formula
   sha256 "820d6708fce16e449a3cb8e62c2a0f49551e589d6595633deab82643e6a90c83"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/xscope-1.4.4"
+    sha256 cellar: :any_skip_relocation, ventura: "dcc3aee6972ccf1811e84c23d20076ef9a2c2725f8998f231525056a81bf4844"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "xorgproto" => :build
   depends_on "xtrans" => :build
