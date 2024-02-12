36f3be23bca70c9fcf294fa73b1612cb67bdacee
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-02-12 01:24:56 +0000

    font-misc-ethiopic: add 1.0.5 bottle.

--- Formula/xorg-font/font-misc-ethiopic.rb
+++ Formula/xorg-font/font-misc-ethiopic.rb
@@ -5,6 +5,12 @@ class FontMiscEthiopic < Formula
   sha256 "4749a7e6e1a1eef6c91fcc9a04e8b1c0ed027d40c1599e5a6c93270d8469b612"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/font-misc-ethiopic-1.0.5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura: "58196eb1cbdcc09c7f3fe0f21ceb9b0957da15ed74ada8c9370b672d28394662"
+  end
+
   depends_on "bdftopcf" => :build
   depends_on "font-util" => :build
   depends_on "fontconfig" => :build
