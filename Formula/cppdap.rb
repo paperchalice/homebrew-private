5343dde34c3ce5d4b87f9f23dc9b7c50ddee906d
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-03-24 05:17:02 +0000

    cppdap: add 1.58.0 bottle.

--- Formula/cppdap.rb
+++ Formula/cppdap.rb
@@ -4,6 +4,11 @@ class Cppdap < Formula
   url "https://github.com/google/cppdap/archive/refs/tags/dap-1.58.0-a.tar.gz"
   sha256 "5d35ca5db78570b6bef698e3365f79bd82a4f78e8393546387f78d7bdb2a2a08"
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/cppdap-1.58.0"
+    sha256 cellar: :any, ventura: "841301c9212f99e00b7e1072ad93b3315142ed97dfb929180f1019b4e47de344"
+  end
+
   depends_on "cmake" => :build
   depends_on "nlohmann-json" => :build
 
