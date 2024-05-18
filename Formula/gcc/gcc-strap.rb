34f25771ec4e932e1394d137128393a7e7d9348a
PaperChalice <liujunchang97@outlook.com>
2024-05-18 20:36:42 +0800

    strap
    Closes #616.
    
    Signed-off-by: paperchalice <29250197+paperchalice@users.noreply.github.com>

--- Formula/gcc/gcc-strap.rb
+++ Formula/gcc/gcc-strap.rb
@@ -1,8 +1,8 @@
 class GccStrap < Formula
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org/"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz"
-  sha256 "e275e76442a6067341a27f04c5c6b83d8613144004c0413528863dc6b5c743da"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.xz"
+  sha256 "e283c654987afe3de9d8080bc0bd79534b5ca0d681a73a11ff2b5d3767426840"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   bottle do
@@ -17,6 +17,7 @@ class GccStrap < Formula
   depends_on "gettext"  => :build
   depends_on "make"     => :build
   depends_on "python"   => :build
+  depends_on "rust"     => :build
   depends_on "texinfo"  => :build
 
   # The bottles are built on systems with the CLT installed, and do not work
@@ -32,15 +33,17 @@ class GccStrap < Formula
     ENV.delete "CXX"
     ENV.delete "LD"
 
-    triple = "#{Hardware::CPU.arch}-apple-#{OS.kernel_name.downcase}"
-    languages = %w[ada c c++ d objc obj-c++ fortran jit lto m2]
+    inreplace "libgcc/config/t-darwin-min-5", "10.5", "11.7"
+
+    triple = "#{Hardware::CPU.arch}-apple-darwin#{OS.kernel_version.major}"
+    languages = %w[ada c c++ d objc obj-c++ fortran jit lto m2 rust]
     sr = MacOS.sdk_path.to_str.tr "0-9", ""
 
     configure_args = %W[
       --build=#{triple}
       --prefix=#{prefix}
-      --disable-bootstrap
       --disable-multilib
+      --disable-bootstrap
       --enable-checking=release
       --enable-libphobos
       --enable-languages=#{languages.join(",")}
@@ -48,6 +51,7 @@ class GccStrap < Formula
       --with-sysroot=#{sr}
       --with-arch=x86-64
       --with-tune=generic
+      --without-zstd
     ]
 
     system "./contrib/download_prerequisites"
@@ -64,8 +68,15 @@ class GccStrap < Formula
     end
     rm_rf man3/"stdheader.dSYM"
     [info, man1, man3, man7].each { |d| Utils::Gzip.compress(*Dir[d/"*"]) }
-    MachO::Tools.add_rpath "#{lib}/gcc/#{triple}/#{version.major}/adalib/#{shared_library "libgnarl"}",
-                           "@loader_path"
+
+    gcc_s = (Formula["gcc-boot"].lib/shared_library("libgcc_s", "1.1")).to_s
+    gcc_ss = (lib/shared_library("libgcc_s", "1.1")).to_s
+    MachO::Tools.add_rpath "#{lib}/#{shared_library "libgcc_s", 1}", "@loader_path"
+    (Pathname.glob(lib/shared_library("*")) +
+      Pathname.glob(lib/"gcc/#{triple}/#{version.major}/adalib"/shared_library("*"))).each do |l|
+      d = MachO::Tools.dylibs l
+      MachO::Tools.change_install_name l.to_s, gcc_s, gcc_ss if d.include? gcc_s
+    end
   end
 
   test do
