50fd6369df8474c191224b687f9b92e5489f217f
paperchalice <29250197+paperchalice@users.noreply.github.com>
2024-12-15 05:57:50 +0000

    tzdb: add 2024b bottle.

--- Formula/tzdb.rb
+++ Formula/tzdb.rb
@@ -5,6 +5,11 @@ class Tzdb < Formula
   sha256 "22674a67786d3ec1b0547305904011cb2b9126166e72abbbea39425de5595233"
   license all_of: ["BSD-3-Clause", :public_domain]
 
+  bottle do
+    root_url "https://github.com/paperchalice/homebrew-private/releases/download/tzdb-2024b"
+    sha256 ventura: "3d54a1fb4bf592008c765ffb6ad8b86e0915744de79f620588fdc4ab8c0a2073"
+  end
+
   depends_on "gettext"
 
   def install
